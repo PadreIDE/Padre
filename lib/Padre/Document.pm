52663be134f5189c93c501c22bff6e6a6eb1e169
Jurre <jurrestender+github@gmail.com>
2008-12-19 14:03:13 +0000

    Made autocompletion more like vim and easier for the lazy programmer.
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@2103 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- lib/Padre/Document.pm
+++ lib/Padre/Document.pm
@@ -610,14 +610,20 @@ sub autocomplete {
 	   $prefix =~ s{^.*?((\w+::)*\w+)$}{$1};
 	my $last   = $editor->GetLength();
 	my $text   = $editor->GetTextRange(0, $last);
+	my $pre_text  = $editor->GetTextRange(0, $first+length($prefix)); 
+	my $post_text = $editor->GetTextRange($first, $last); 
 
 	my $regex;
-	eval { $regex = qr{\b($prefix\w*(?:::\w+)*)\b} };
+	eval { $regex = qr{\b($prefix\w+(?:::\w+)*)\b} };
 	if ($@) {
 		return ("Cannot build regex for '$prefix'");
 	}
+
 	my %seen;
-	my @words = grep { ! $seen{$_}++ } sort ($text =~ /$regex/g);
+	my @words;
+	push @words ,grep { ! $seen{$_}++ } reverse ($pre_text =~ /$regex/g);
+	push @words , grep { ! $seen{$_}++ } ($post_text =~ /$regex/g);
+
 	if (@words > 20) {
 		@words = @words[0..19];
 	}
