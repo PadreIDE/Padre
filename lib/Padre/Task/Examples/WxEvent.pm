b8c3741c06fa2df598b001545fb021bd5a3ae5e7
Steffen Müller <smueller@cpan.org>
2008-12-21 18:23:57 +0000

    Add simple Wx event example
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@2139 bc93166f-e056-4eb9-8c59-0a0840647e1b

---
+++ lib/Padre/Task/Examples/WxEvent.pm
@@ -0,0 +1,49 @@
+
+package Padre::Task::Examples::WxEvent;
+use strict;
+use warnings;
+
+our $VERSION = '0.21';
+
+use base 'Padre::Task';
+
+# set up a new event type
+our $SAY_HELLO_EVENT : shared = Wx::NewEventType();
+
+sub prepare {
+	my $self = shift;
+
+	# Set up the event handler
+	my $main = Padre->ide->wx->main_window;
+	Wx::Event::EVT_COMMAND($main, -1, $SAY_HELLO_EVENT, \&on_say_hello);
+	return();
+}
+
+# the event handler
+sub on_say_hello {
+	my ($main, $event) = @_; @_=(); # hack to avoid "Scalars leaked"
+	
+	# write a message to the beginning of the document
+	my $cur = Padre::Documents->current->editor;
+	return if not defined $cur;
+	$cur->InsertText(0, $event->GetData());
+}
+
+sub run {
+	my $self = shift;
+	
+	# post two events for fun
+	$self->post_event($SAY_HELLO_EVENT, "Hello from thread!\n");
+	sleep 1;
+	$self->post_event($SAY_HELLO_EVENT, "Hello again!\n");
+	return 1;
+}
+
+1;
+
+__END__
+
+# Copyright 2008 Gabor Szabo.
+# LICENSE
+# This program is free software; you can redistribute it and/or
+# modify it under the same terms as Perl 5 itself.
