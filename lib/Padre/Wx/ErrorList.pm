aa6e4d04a666cf07a8deab6b55ca7feff30b2eb6
szabgab <szabgab@gmail.com>
2008-12-31 21:29:09 +0000

    move the Padre::Task::ErrorParser package into its own file
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@2325 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- lib/Padre/Wx/ErrorList.pm
+++ lib/Padre/Wx/ErrorList.pm
@@ -7,6 +7,7 @@ our $VERSION = '0.22';
 
 require Padre;
 use Padre::Wx;
+use Padre::Task::ErrorParser;
 use Parse::ErrorString::Perl;
 use Wx::Locale qw(:default);
 use Encode qw(encode);
@@ -157,74 +158,10 @@ sub clear {
 	$self->DeleteChildren($root);
 }
 
-package Padre::Task::ErrorParser;
-
-use base 'Padre::Task';
-
-use Class::XSAccessor
-	getters => {
-		parser       => 'parser',
-		old_lang     => 'old_lang',
-		cur_lang     => 'cur_lang',
-		data         => 'data',
-	};
-
-sub run {
-	my $self = shift;
-	unless ($self->parser and ( (!$self->cur_lang and !$self->old_lang) or ($self->cur_lang eq $self->old_lang) )) {
-		if ($self->cur_lang) {
-			$self->{parser} = Parse::ErrorString::Perl->new(lang => $self->cur_lang);
-		} else {
-			$self->{parser} = Parse::ErrorString::Perl->new;
-		}
-	}
-    return 1;
-}
-
-sub finish {
-	my $self = shift;
-    my $mw = shift;
-
-	my $errorlist = $mw->errorlist;
-	
-	my $data = $self->data;
-	my $parser = $self->parser;
-	$errorlist->{parser} = $parser;
-
-	my @errors = defined $data && $data ne '' ? $parser->parse_string($data) : ();
-	
-	foreach my $err (@errors) {
-		my $message = $err->message . " at " . $err->file . " line " . $err->line;
-		#$message = encode('utf8', $message);
-		if ($err->near) {
-			my $near = $err->near;
-			# some day when we have unicode in wx ...
-			#$near =~ s/\n/\x{c2b6}/g;
-			$near =~ s/\n/\\n/g;
-			$near =~ s/\r//g;
-			$message .= ", near \"$near\"";
-		} elsif ($err->at) {
-			my $at = $err->at;
-			$message .= ", at $at";
-		}
-		my $err_tree_item = $errorlist->AppendItem( $errorlist->root, $message, -1, -1, Wx::TreeItemData->new( $err ) );
-		
-		if ($err->stack) {
-			foreach my $stack_item ($err->stack) {
-				my $stack_message = $stack_item->sub . 
-					" called at " . $stack_item->file . 
-					" line " . $stack_item->line;
-				$errorlist->AppendItem( $err_tree_item, $stack_message, -1, -1, Wx::TreeItemData->new( $stack_item ) );
-			}
-		}
-	}
-	
-	return 1;
-}
-
 1;
 
 # Copyright 2008 Gabor Szabo.
 # LICENSE
 # This program is free software; you can redistribute it and/or
 # modify it under the same terms as Perl 5 itself.
+
