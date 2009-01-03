09c084c4daa2358f41afef139af93e39420eaaf0
adamk <adamk@cpan.org>
2009-01-03 06:07:56 +0000

    Adding the null project
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@2388 bc93166f-e056-4eb9-8c59-0a0840647e1b

---
+++ lib/Padre/Project/Null.pm
@@ -0,0 +1,29 @@
+package Padre::Project::Null;
+
+use strict;
+use warnings;
+use Padre::Project ();
+
+our $VERSION = '0.22';
+our @ISA     = 'Padre::Project';
+
+sub new {
+	my $class = shift;
+	my $self  = bless { @_ }, $class;
+
+	# Check the root directory
+	unless ( defined $self->root ) {
+		croak("Did not provide a root directory");
+	}
+	unless ( -d $self->root ) {
+		croak("Root directory " . $self->root . " does not exist");
+	}
+
+	return $self;
+}
+
+sub padre_yml {
+	return undef;
+}
+
+1;
