b709276148cee71bb1c713fea8f0d832f88bc191
adamk <adamk@cpan.org>
2009-01-07 09:44:46 +0000

    Adding a menu layout for various install tasks, based on Padre::Plugin::CPAN. Implementation to follow...
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@2572 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- lib/Padre/Wx/Menu/Perl.pm
+++ lib/Padre/Wx/Menu/Perl.pm
@@ -5,8 +5,8 @@ package Padre::Wx::Menu::Perl;
 use 5.008;
 use strict;
 use warnings;
-use Params::Util       ();
-use Padre::Wx          ();
+use Params::Util    ();
+use Padre::Wx       ();
 use Padre::Wx::Menu ();
 
 our $VERSION = '0.24';
@@ -26,9 +26,71 @@ sub new {
 	# Create the empty menu as normal
 	my $self = $class->SUPER::new(@_);
 
+	# Module-Related Functions
+	$self->{module} = Wx::Menu->new;
+	$self->Append( -1,
+		Wx::gettext("Install Module..."),
+		$self->{module}
+	);
+
+	# Install modules from CPAN
+	$self->{module_install_cpan} = $self->{module}->Append( -1,
+		Wx::gettext("Install CPAN Module"),
+	);
+	Wx::Event::EVT_MENU( $main,
+		$self->{module_install_cpan},
+		sub {
+			$self->install_cpan($_[0]);
+		},
+	);
+
+	$self->{module}->AppendSeparator;
+
+	# Install from other places
+	$self->{module_install_file} = $self->{module}->Append( -1,
+		Wx::gettext("Install Local Distribution"),
+	);
+	Wx::Event::EVT_MENU( $main,
+		$self->{module_install_file},
+		sub {
+			$self->install_file($_[0]);
+		},
+	);
+
+	$self->{module_install_url} = $self->{module}->Append( -1,
+		Wx::gettext("Install Remote Distribution"),
+	);
+	Wx::Event::EVT_MENU( $main,
+		$self->{module_install_url},
+		sub {
+			$self->install_url($_[0]);
+		},
+	);
+
+	$self->{module}->AppendSeparator;
+
+	# Utility Operations
+	$self->{module_edit_cpan} = $self->{module}->Append( -1,
+		Wx::gettext("Open CPAN::Config"),
+	);
+	Wx::Event::EVT_MENU( $main,
+		$self->{module_edit_cpan},
+		sub {
+			$self->edit_config($_[0]);
+		},
+	);
+
+	$self->AppendSeparator;
+
+
+
+
+
 	# Perl-Specific Searches
 	Wx::Event::EVT_MENU( $main,
-		$self->Append( -1, Wx::gettext("Find Unmatched Brace") ),
+		$self->Append( -1,
+			Wx::gettext("Find Unmatched Brace")
+		),
 		sub {
 			my $doc = $_[0]->current->document;
 			return unless Params::Util::_INSTANCE($doc, 'Padre::Document::Perl');
@@ -37,7 +99,7 @@ sub new {
 	);
 
 	Wx::Event::EVT_MENU( $main,
-		$self->Append( -1, Wx::gettext("Find variable declaration") ),
+		$self->Append( -1, Wx::gettext("Find Variable Declaration") ),
 		sub {
 			my $doc = $_[0]->current->document;
 			return unless Params::Util::_INSTANCE($doc, 'Padre::Document::Perl');
@@ -45,8 +107,15 @@ sub new {
 		},
 	);
 
+	$self->AppendSeparator;
+
+
+
+
+
+	# Perl-Specific Refactoring
 	Wx::Event::EVT_MENU( $main,
-		$self->Append( -1, Wx::gettext("Lexically replace variable") ),
+		$self->Append( -1, Wx::gettext("Lexically Rename Variable") ),
 		sub {
 			my $doc = $_[0]->current->document;
 			return unless Params::Util::_INSTANCE($doc, 'Padre::Document::Perl');
@@ -67,9 +136,15 @@ sub new {
 		},
 	);
 
-	# Experimental PPI-based highlighting
+	$self->AppendSeparator;
+
+
+
+
+
+	# Perl-Specific Options
 	$self->{ppi_highlight} = $self->AppendCheckItem( -1,
-		Wx::gettext("Use PPI for Perl5 syntax highlighting")
+		Wx::gettext("Use PPI Syntax Highlighting")
 	);
 	Wx::Event::EVT_MENU( $main,
 		$self->{ppi_highlight},
@@ -99,10 +174,10 @@ sub new {
 			return;
 		}
 	);
-	
+
 	# Make it easier to access stack traces
 	$self->{run_with_stack_trace} = $self->AppendCheckItem( -1,
-		Wx::gettext("Run perl scripts with stack trace")
+		Wx::gettext("Run Scripts with Stack Trace")
 	);	
 	Wx::Event::EVT_MENU( $main, $self->{run_with_stack_trace},
 		sub {
@@ -113,13 +188,12 @@ sub new {
 		}
 	);
 
-
 	return $self;
 }
 
 sub refresh {
-	my $self     = shift;
-	my $config   = Padre->ide->config;
+	my $self   = shift;
+	my $config = Padre->ide->config;
 
 	$self->{ppi_highlight}->Check( $config->{ppi_highlight} ? 1 : 0 );
 	$self->{run_with_stack_trace}->Check( $config->{run_with_stack_trace} ? 1 : 0 );
@@ -127,6 +201,37 @@ sub refresh {
 		$config->{ppi_highlight}
 			? Wx::wxSTC_LEX_CONTAINER
 			: Wx::wxSTC_LEX_PERL;
+}
+
+
+
+
+
+#####################################################################
+# Menu Event Methods
+
+sub install_file {
+	my $self = shift;
+	my $main = shift;
+	$main->error("TO BE COMPLETED");
+}
+
+sub install_url {
+	my $self = shift;
+	my $main = shift;
+	$main->error("TO BE COMPLETED");
+}
+
+sub install_cpan {
+	my $self = shift;
+	my $main = shift;
+	$main->error("TO BE COMPLETED");
+}
+
+sub edit_config {
+	my $self = shift;
+	my $main = shift;
+	$main->error("TO BE COMPLETED");
 }
 
 1;
