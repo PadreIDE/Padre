c29462d9be90ca35e7f462d541c4b453a5a8d6e9
adamk <adamk@cpan.org>
2009-01-08 04:16:26 +0000

    Adding skeleton for the user-specific config file
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@2606 bc93166f-e056-4eb9-8c59-0a0840647e1b

---
+++ lib/Padre/Config/User.pm
@@ -0,0 +1,44 @@
+package Padre::Config::User;
+
+use 5.008;
+use strict;
+use warnings;
+use YAML::Tiny   ();
+use Params::Util qw{_HASH0};
+
+our $VERSION = '0.24';
+
+
+
+
+
+######################################################################
+# Constructor
+
+sub new {
+	my $class = shift;
+	my $self  = bless { @_ }, $class;
+
+	# Check the config
+
+	return $self;
+}
+
+sub read {
+	my $class = shift;
+
+	# Check the file
+	my $file = shift;
+	unless ( defined $file and -f $file and -r $file ) {
+		return;
+	}
+
+	# Load the user configuration
+	my $hash = YAML::Tiny::LoadFile($file);
+	return unless _HASH0($hash);
+
+	# Create the object
+	return $class->new( %$hash );
+}
+
+1;
