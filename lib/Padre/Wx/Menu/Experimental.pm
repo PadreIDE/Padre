9aadcba8e901f2af1c5b54e1c2f9ab0e666cb825
adamk <adamk@cpan.org>
2009-01-09 10:22:20 +0000

    Removing "Run in Padre" from the experimental menu
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@2651 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- lib/Padre/Wx/Menu/Experimental.pm
+++ lib/Padre/Wx/Menu/Experimental.pm
@@ -3,7 +3,7 @@ package Padre::Wx::Menu::Experimental;
 use 5.008;
 use strict;
 use warnings;
-use Padre::Wx          ();
+use Padre::Wx       ();
 use Padre::Wx::Menu ();
 
 our $VERSION = '0.24';
@@ -25,7 +25,9 @@ sub new {
 
 	# Disable experimental mode
 	Wx::Event::EVT_MENU( $main,
-		$self->Append( -1, Wx::gettext('Disable Experimental Mode') ),
+		$self->Append( -1,
+			Wx::gettext('Disable Experimental Mode')
+		),
 		sub {
 			Padre->ide->config->{experimental} = 0;
 			$_[0]->menu->refresh($_[0]->current);
@@ -47,7 +49,8 @@ sub new {
 	# Force-refresh the menu
 	$self->{refresh_counter} = 0;
 	$self->{refresh_count}   = $self->Append( -1,
-		Wx::gettext('Refresh Counter: ') . $self->{refresh_counter}
+		Wx::gettext('Refresh Counter: ')
+		. $self->{refresh_counter}
 	);
 	Wx::Event::EVT_MENU( $main,
 		$self->{refresh_count},
@@ -65,20 +68,12 @@ sub new {
 		$self->{recent_projects},
 	);
 
-	# Launch a script INSIDE the running Padre instance
-	Wx::Event::EVT_MENU(
-		$main,
-		$self->Append( -1, Wx::gettext('Run in &Padre') ),
-		\&Padre::Wx::MainWindow::run_in_padre,
-	);
-
 	return $self;
 }
 
 # Update the checkstate for several menu items
 sub refresh {
 	my $self   = shift;
-	my $config = Padre->ide->config;
 
 	# Update the refresh counter
 	$self->{refresh_counter}++;
