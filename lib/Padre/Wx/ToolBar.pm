38a268a83dadc3f8dc72cf68ad1425323d130e34
adamk <adamk@cpan.org>
2009-01-09 00:38:08 +0000

    Typo fix
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@2635 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- lib/Padre/Wx/ToolBar.pm
+++ lib/Padre/Wx/ToolBar.pm
@@ -19,8 +19,8 @@ sub new {
 	my $style = Wx::wxTB_HORIZONTAL
 		| Wx::wxTB_FLAT
 		| Wx::wxTB_NO_TOOLTIPS
-		| Wx::wxBORDER_NONE
-		| Wx::wxNODIVIDER;
+		| Wx::wxTB_NODIVIDER
+		| Wx::wxBORDER_NONE;
 	unless ( $main->config->{main_lockpanels} ) {
 		$style = $style | Wx::wxTB_DOCKABLE;
 	}
