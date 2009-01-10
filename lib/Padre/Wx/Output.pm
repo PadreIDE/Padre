ac550f6bdd80da29d8b3875ce2d9741d239adc23
Steffen Müller <smueller@cpan.org>
2009-01-10 10:39:59 +0000

    ANSI Color and type face support for output window
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@2700 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- lib/Padre/Wx/Output.pm
+++ lib/Padre/Wx/Output.pm
@@ -64,7 +64,118 @@ sub AppendText {
 		return $self->SUPER::AppendText($_[0]);
 	}
 	my $text = Encode::decode('utf8', $_[0]);
-	$self->SUPER::AppendText($text);
+	if (1) { # FIXME switch
+		$self->_handle_ansi_escapes($text);
+	}
+	else {
+		$self->SUPER::AppendText($text);
+	}
+}
+
+{
+	# TODO: This should be some sort of style file, but the main editor style support is too wacky to add this at the moment
+	my $fg_colors = [
+		Wx::Colour->new('#000000'), # black
+		Wx::Colour->new('#FF0000'), # red
+		Wx::Colour->new('#00FF00'), # green
+		Wx::Colour->new('#FFFF00'), # yellow
+		Wx::Colour->new('#0000FF'), # blue
+		Wx::Colour->new('#FF00FF'), # magenta
+		Wx::Colour->new('#00FFFF'), # cyan
+		Wx::Colour->new('#FFFFFF'), # white
+		undef,
+		Wx::Colour->new('#000000'), # reset to default (black)
+	];
+	my $bg_colors = [
+		Wx::Colour->new('#000000'), # black
+		Wx::Colour->new('#FF0000'), # red
+		Wx::Colour->new('#00FF00'), # green
+		Wx::Colour->new('#FFFF00'), # yellow
+		Wx::Colour->new('#0000FF'), # blue
+		Wx::Colour->new('#FF00FF'), # magenta
+		Wx::Colour->new('#00FFFF'), # cyan
+		Wx::Colour->new('#FFFFFF'), # white
+		undef,
+		Wx::Colour->new('#FFFFFF'), # reset to default (white)
+	];
+	
+	sub _handle_ansi_escapes {
+		my $self = shift;
+		my $newtext = shift;
+
+		# read the next TEXT CONTROL-SEQUENCE pair
+		my $style = $self->GetDefaultStyle();
+		while ($newtext =~ m{ \G (.*?) \033\[ ( (?: \d+ (?:;\d+)* )? ) m }xcg) {
+			my $ctrl = $2;
+			# first print the text preceding the control sequence
+			$self->SUPER::AppendText($1);
+			
+			# split the sequence on ; -- this may be specific to the graphics 'm' sequences, but
+			# we don't handle any others at the moment (see regexp above)
+			my @cmds = split /;/, $ctrl;
+			
+			foreach my $cmd (@cmds) {
+				if ($cmd >= 0 and $cmd < 30) {
+					# for all these, we need the font object:
+					my $font = $style->GetFont();
+					if ($cmd == 0) { # reset
+						$style->SetTextColour($fg_colors->[9]); # reset text color
+						$style->SetBackgroundColour($bg_colors->[9]); # reset bg color
+						# reset bold/italic/underlined state
+						$font->SetWeight(Wx::wxFONTWEIGHT_NORMAL);
+						$font->SetUnderlined(0);
+						$font->SetStyle(Wx::wxFONTSTYLE_NORMAL);
+					}
+					elsif ($cmd == 1) { # bold
+						$font->SetWeight(Wx::wxFONTWEIGHT_BOLD);
+					}
+					elsif ($cmd == 2) { # faint
+						$font->SetWeight(Wx::wxFONTWEIGHT_LIGHT);
+					}
+					elsif ($cmd == 3) { # italic
+						$font->SetStyle(Wx::wxFONTSTYLE_ITALIC);
+					}
+					elsif ($cmd == 4 || $cmd == 21) { # underline (21==double, but we can't do that)
+						$font->SetUnderlined(1);
+					}
+					elsif ($cmd == 22) { # reset bold and faint
+						$font->SetWeight(Wx::wxFONTWEIGHT_NORMAL);
+					}
+					elsif ($cmd == 24) { # reset underline
+						$font->SetUnderlined(0);
+					}
+					$style->SetFont($font);
+				} # end if < 30 (aka need font)
+				
+				# the high range is supposed to be 'high intensity' as supported by aixterm
+				elsif ($cmd >= 30 && $cmd < 40 or $cmd >= 90 && $cmd < 100) {
+					# foreground
+					$cmd -= $cmd > 40 ? 90 : 30;
+					my $color = $fg_colors->[$cmd];
+					if (defined $color) {
+						$style->SetTextColour($color);
+						$self->SetDefaultStyle($style);
+					}
+				}
+				# the high range is supposed to be 'high intensity' as supported by aixterm
+				elsif ($cmd >= 40 && $cmd < 50 or $cmd >= 100 && $cmd < 110) {
+					# background
+					$cmd -= $cmd > 50 ? 100 : 40;
+					my $color = $bg_colors->[$cmd];
+					if (defined $color) {
+						$style->SetBackgroundColour($color);
+					}
+				}
+				
+				$self->SetDefaultStyle($style);
+			} # end foreach command in the sequence
+		} # end while more control sequences
+		
+		# the remaining text
+		if (defined(pos($newtext))) {
+			$self->SUPER::AppendText(substr($newtext, pos($newtext)));
+		}
+	}
 }
 
 sub select {
@@ -74,7 +185,6 @@ sub select {
 	return;
 }
 
-# A convenience not provided by the original version
 sub SetBackgroundColour {
 	my $self = shift;
 	my $arg  = shift;
