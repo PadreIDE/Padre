41601040ae7628d9c7d8ea3fae2d53878bf1aa72
adamk <adamk@cpan.org>
2009-01-11 14:54:24 +0000

    Allow tabs on the main files notebook to be dragged to reorder them
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@2737 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- lib/Padre/Wx/Notebook.pm
+++ lib/Padre/Wx/Notebook.pm
@@ -18,8 +18,9 @@ sub new {
 		Wx::wxAUI_NB_TOP
 		| Wx::wxBORDER_NONE
 		| Wx::wxAUI_NB_SCROLL_BUTTONS
+		| Wx::wxAUI_NB_TAB_MOVE
 		| Wx::wxAUI_NB_CLOSE_ON_ACTIVE_TAB
-		| Wx::wxAUI_NB_WINDOWLIST_BUTTON,
+		| Wx::wxAUI_NB_WINDOWLIST_BUTTON
 	);
 
 	# Add ourself to the main window
