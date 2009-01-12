6308aa405122f42c3bdee2a3ba7b496ec0f17b68
fayland <fayland@gmail.com>
2009-01-12 23:48:52 +0000

    add Module::Install back
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@2777 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- lib/Padre/Wx/Dialog/ModuleStart.pm
+++ lib/Padre/Wx/Dialog/ModuleStart.pm
@@ -18,7 +18,7 @@ our $VERSION = '0.25';
 sub get_layout {
 	my ($config) = @_;
 
-	my @builders = ('Module::Build', 'ExtUtils::MakeMaker'); #, 'Module::Install');
+	my @builders = ('Module::Build', 'ExtUtils::MakeMaker', 'Module::Install');
 	my @licenses = qw(apache artistic artistic_2 bsd gpl lgpl mit mozilla open_source perl restrictive unrestricted);
 	# licenses list taken from 
 	# http://search.cpan.org/dist/Module-Build/lib/Module/Build/API.pod
