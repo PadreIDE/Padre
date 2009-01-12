00cf6ab364915118f1d2276eb38a3628be4951f5
szabgab <szabgab@gmail.com>
2009-01-12 07:06:19 +0000

    add basic copyright info
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@2760 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- lib/Padre/Config2.pm
+++ lib/Padre/Config2.pm
@@ -120,3 +120,8 @@ sub config {
 }
 
 1;
+
+# Copyright 2008 Gabor Szabo.
+# LICENSE
+# This program is free software; you can redistribute it and/or
+# modify it under the same terms as Perl 5 itself.
