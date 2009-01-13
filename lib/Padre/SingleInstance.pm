d90d76c48892858f2ce28b514a58424e325d1db5
azawawi <ahmad.zawawi@gmail.com>
2009-01-13 18:14:55 +0000

    Adding Padre::SingleInstance
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@2787 bc93166f-e056-4eb9-8c59-0a0840647e1b

---
+++ lib/Padre/SingleInstance.pm
@@ -0,0 +1,123 @@
+package Padre::SingleInstance;
+
+# core modules
+use strict;
+use warnings;
+use Carp;
+use IO::File;
+use IO::Socket;
+use threads;
+
+# constants
+use constant REMOTE_HOST => '127.0.0.1';
+use constant SERVER_PORT => 9999;
+
+#
+# constructor
+#
+sub new {
+    my ($class,%options) = @_;
+    
+    if(! defined $options{on_file_request}) {
+        croak "on_file_request is not defined";
+    }
+    if(! defined $options{on_focus_request}) {
+        croak "on_focus_request is not defined";
+    }
+    my $self = bless \%options, $class;
+    return $self;
+}
+
+#
+# checks whether another instance is running or not
+#
+sub is_running {
+    my $self = shift;
+    
+    my $socket = IO::Socket::INET->new(PeerAddr => REMOTE_HOST,
+                                    PeerPort => SERVER_PORT,
+                                    Proto    => "tcp",
+                                    Type     => SOCK_STREAM);
+
+    if($socket) {    
+        print "It is alive\n";
+        if($#ARGV > 0) {
+            foreach my $argnum (0 .. $#ARGV) {
+               my $arg = $ARGV[$argnum];
+               print $socket "open $ARGV[$argnum]\n";
+            }
+            close $socket
+                or croak "Cant close socket\n";
+        } else {
+            print $socket "restore_focus";
+            die "Nothing to write...\n";
+        }
+        die "Sent it my work.... bye bye\n";
+    }
+    
+    return $socket ? 1 : 0;
+}
+
+#
+# start TCP server socket thread
+#
+sub start_server {
+    my $self = shift;
+    $self->{server_thread} = threads->create(sub { $self->_run; } );
+    return $self->{server_thread};
+}
+
+#
+# Main thread that services TCP clients
+#
+sub _run {
+    my $self = shift;
+    
+    print "Try to run server on " . SERVER_PORT ."\n";
+    my $server = IO::Socket::INET->new(LocalPort => SERVER_PORT,
+                                    Type      => SOCK_STREAM,
+                                    Reuse     => 1,
+                                    Listen    => 10 )
+        or croak "Couldn't be a tcp server on port " . SERVER_PORT .  ": $@\n";
+    LOOP: while (my $client = $server->accept()) {
+        # $client is the new connection
+        while(my $line = <$client>) {
+            if($line =~ /^open\s+(.+)$/) {
+                #XXX- I should open filename... 
+                my $filename = $1;
+                $self->{on_file_request}($filename);
+                if($filename eq 'shutdown') {
+                    print "Going to shutdown";
+                    last LOOP;
+                }
+            } elsif($line =~ /^restore_focus$/) {
+                #XXX- I should restore focus
+                $self->{on_focus_request}();
+            }
+        }
+    }
+}    
+
+1;
+
+# package main;
+
+#autoflush it
+# STDOUT->autoflush(1);
+
+# my $t = SingleInstance->new( 
+    # on_file_request => sub { my $filename = shift; print "filename: " . $filename . " open requested\n";},
+    # on_focus_request => sub { print "focus requested\n" }
+# );
+# if ($t->is_running) {
+    # print "It is running\n";
+# } else {
+    # print "Nothing is running... Going to start server\n";
+    # my $thr = $t->start_server;
+    # $thr->detach;
+    # while($thr->is_running) {
+        # sleep 1;
+    # }
+# }
+
+0;
