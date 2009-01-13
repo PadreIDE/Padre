8fb74a074e53fa59e40c44b9c6c041af5894b786
azawawi <ahmad.zawawi@gmail.com>
2009-01-13 20:56:34 +0000

    Added missing license to pass copyright tests :)
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@2796 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- lib/Padre/SingleInstance.pm
+++ lib/Padre/SingleInstance.pm
@@ -99,3 +99,8 @@ sub _run {
 }    
 
 1;
+
+# Copyright 2008 Gabor Szabo.
+# LICENSE
+# This program is free software; you can redistribute it and/or
+# modify it under the same terms as Perl 5 itself.
