a9f295aa12b884944304023048a3b977e521ed69
Aaron Trevena <aaron.trevena@gmail.com>
2009-01-15 19:45:21 +0000

    added skeleton of key binding management
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@2815 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- lib/Padre/Wx/Menu.pm
+++ lib/Padre/Wx/Menu.pm
@@ -6,6 +6,8 @@ use strict;
 use warnings;
 use Padre::Wx ();
 
+use Data::Dumper;
+
 use Class::Adapter::Builder
 	ISA      => 'Wx::Menu',
 	NEW      => 'Wx::Menu',
@@ -19,9 +21,37 @@ use Class::XSAccessor
 	};
 
 # Default implementation of refresh
+
 sub refresh { 1 }
 
+# over-rides and then calls XS wx Menu::Append
+# adds any hotkeys to global registry of bound keys
+
+sub Append {
+    my ($self, @args) = (shift, @_);
+    my $item = $self->wx->Append( @_ );
+    my $string = $args[1];
+    my ($underlined) = ( $string =~ m/(\&\w)/ );
+    my ($accel) = ( $string =~ m/(Ctrl-.+|Alt-.+)/ );
+    if ($underlined or $accel) {
+	$self->{main}{accel_keys} ||= {};
+	if ($underlined) {
+	    $underlined =~ s/&(\w)/$1/;
+	    $self->{main}{accel_keys}{underlined}{$underlined} = $item;
+	}
+	if ($accel) {
+	    my ($mod, $mod2, $key) = ( $accel =~ m/(Ctrl|Alt)(-Shift)?\-(.)/);#
+	    $mod .= $mod2 if ($mod2);
+	    $self->{main}{accel_keys}{hotkeys}{$mod}{$key} = $item;
+	}
+    }
+    return $item;
+}
+
+
+
 1;
+
 # Copyright 2008 Gabor Szabo.
 # LICENSE
 # This program is free software; you can redistribute it and/or
