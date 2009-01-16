b443ba040f1da07cab85a4102d9ddf3f3bf43ff5
adamk <adamk@cpan.org>
2009-01-16 06:31:36 +0000

    Removing the old modules table, and merging the Padre::DB root documentation into the .pm module so it's easier to diff between that and any new generated versions
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@2817 bc93166f-e056-4eb9-8c59-0a0840647e1b

---
+++ share/timeline/migrate-3.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/perl
+
+# This patch removes the old modules table.
+
+use strict;
+use File::Spec ();
+use lib File::Spec->rel2abs(
+	File::Spec->catdir(
+		File::Spec->updir,
+		File::Spec->updir,
+		File::Spec->updir,
+		File::Spec->updir,
+		File::Spec->updir,
+	)
+);
+use Padre::DB::Patch;
+
+
+
+
+
+#####################################################################
+# Patch Content
+
+# Create the host settings table
+do(<<'END_SQL');
+DROP TABLE modules
+END_SQL
+
+exit(0);
