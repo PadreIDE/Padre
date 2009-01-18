5dfa2a6d313cc21127c9730f77206b71354960c2
adamk <adamk@cpan.org>
2009-01-18 07:34:23 +0000

    Adding an id column as well
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@2832 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- share/timeline/migrate-4.pl
+++ share/timeline/migrate-4.pl
@@ -23,7 +23,8 @@ use Padre::DB::Patch;
 # Create the bookmark table
 do(<<'END_SQL');
 CREATE TABLE bookmark (
-	name VARCHAR(255) NOT NULL PRIMARY KEY,
+	id   INTEGER NOT NULL PRIMARY KEY,
+	name VARCHAR(255) UNIQUE NOT NULL,
 	file VARCHAR(255) NOT NULL,
 	line INTEGER NOT NULL
 )
