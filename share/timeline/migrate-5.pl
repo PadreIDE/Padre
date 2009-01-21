44a42924515ce1e4bd40096f4bfb842e6e378212
adamk <adamk@cpan.org>
2009-01-21 00:30:23 +0000

    Adding migration script for session storage
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@2862 bc93166f-e056-4eb9-8c59-0a0840647e1b

---
+++ share/timeline/migrate-5.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/perl
+
+use strict;
+use File::Spec ();
+use lib File::Spec->rel2abs(
+	File::Spec->catdir(
+		File::Spec->updir,
+		File::Spec->updir,
+		File::Spec->updir,
+		File::Spec->updir,
+		File::Spec->updir,
+	)
+);
+use Padre::DB::Patch;
+
+
+
+
+
+#####################################################################
+# Patch Content
+
+# Create the bookmark table
+do(<<'END_SQL');
+CREATE TABLE session (
+	file VARCHAR(255) UNIQUE NOT NULL,
+	line INTEGER NOT NULL,
+	character INTEGER NOT NULL,
+	focus BOOLEAN NOY NULL
+)
+END_SQL
+
+exit(0);
