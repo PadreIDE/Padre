2c197b346f16ff1f0b262e726e230f1fbc0a1342
adamk <adamk@cpan.org>
2009-01-23 14:14:43 +0000

    Correcting version
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@2902 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- lib/Padre/Plugin/Devel.pm
+++ lib/Padre/Plugin/Devel.pm
@@ -18,8 +18,8 @@ our @ISA     = 'Padre::Plugin';
 # Padre::Plugin Methods
 
 sub padre_interfaces {
-	'Padre::Plugin'   => 0.25,
-	'Padre::Wx::Main' => 0.25,
+	'Padre::Plugin'   => 0.26,
+	'Padre::Wx::Main' => 0.26,
 }
 
 sub plugin_name {
