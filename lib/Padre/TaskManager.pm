4533c611cac0afc5ac600387d3adcd20b9fcc76e
adamk <adamk@cpan.org>
2009-01-28 13:58:49 +0000

    Whitespace cleanup
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@2957 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- lib/Padre/TaskManager.pm
+++ lib/Padre/TaskManager.pm
@@ -513,6 +513,7 @@ This program is free software; you can redistribute it and/or
 modify it under the same terms as Perl 5 itself.
 
 =cut
+
 # Copyright 2008 Gabor Szabo.
 # LICENSE
 # This program is free software; you can redistribute it and/or
