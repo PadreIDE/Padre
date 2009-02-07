c60c8e9de6dfb88e970ac74ef324440d4d520b9a
heikojansen <hjansen@cpan.org>
2009-02-07 21:24:31 +0000

    Comment with some documentation (got confused with the codes myself)
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@3029 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- lib/Padre/Document/Perl.pm
+++ lib/Padre/Document/Perl.pm
@@ -557,7 +557,8 @@ sub event_on_char {
 	my $key = $event->GetUnicodeKey;
 
 	if ( Padre->ide->config->autocomplete_brackets ) {
-		my %table = ( 40 => 41, 60 => 62, 91 => 93, 123 => 125, ); # <> would be: 123 => 125, );
+		# these are:   (    )    <    >    [    ]     {    }
+		my %table = ( 40 => 41, 60 => 62, 91 => 93, 123 => 125, );
 		my $pos = $editor->GetCurrentPos;
 		foreach my $code ( keys %table ) {
 			if ( $key == $code ) {
