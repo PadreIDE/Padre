6f485e6fef30b86f4e75bd7b4135d6b9f1f77d09
szabgab <szabgab@gmail.com>
2009-03-08 12:03:01 +0000

    update directory tree when ui is updated sort directory tree allow opening of files from Directory view
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@3199 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- lib/Padre/Wx/Directory.pm
+++ lib/Padre/Wx/Directory.pm
@@ -72,21 +72,20 @@ sub force_next {
 
 sub on_tree_item_activated {
 	my ($self, $event) = @_;
-	my $page = $self->main->current->editor;
+
 
 	my $item = $self->GetPlData( $event->GetItem );
 	return if not defined $item;
 
-	my $line_number = $item->{line};
-	return if not defined($line_number)
-		  or $line_number !~ /^\d+$/o
-		  or $page->GetLineCount < $line_number;
-
-	$line_number--;
-	$page->EnsureVisible($line_number);
-	$page->goto_pos_centerize( $page->GetLineIndentPosition($line_number) );
-	$page->SetFocus;
-
+	my $path = File::Spec->catfile($item->{dir}, $item->{name});
+	return if not defined $path;
+	my $main = $self->main;
+	if (my $id = $main->find_editor_of_file($path)) {
+		my $page = $main->notebook->GetPage($id);
+		$page->SetFocus;
+	} else {
+		$main->setup_editors($path);
+	}
 	return;
 }
 
@@ -100,11 +99,12 @@ sub update_gui {
 	my $dir = File::Basename::dirname($filename);
 	my @data;
 	if (opendir my $dh, $dir) {
-		while (my $thing = readdir $dh) {
+		my @items = sort readdir $dh;
+		foreach my $thing (@items)  {
 			next if $thing eq '.' or $thing eq '..';
 			push @data, {
 				name => $thing,
-				line => 1,
+				dir  => $dir,
 			};
 		}
 	}
@@ -137,8 +137,8 @@ sub _on_tree_item_right_click {
 	my $menu = Wx::Menu->new;
 	my $itemData = $dir->GetPlData( $event->GetItem );
 
-	if ( defined($itemData) && defined( $itemData->{line} ) && $itemData->{line} > 0 ) {
-		my $goTo = $menu->Append( -1, Wx::gettext("&GoTo Element") );
+	if ( defined($itemData) ) {
+		my $goTo = $menu->Append( -1, Wx::gettext("Open File") );
 		Wx::Event::EVT_MENU( $dir, $goTo,
 			sub { $dir->on_tree_item_activated($event); },
 		);
@@ -183,7 +183,7 @@ sub _update_treectrl {
 			-1,
 			-1,
 			Wx::TreeItemData->new( {
-				line => $pkg->{line},
+				dir  => $pkg->{dir},
 				name => $pkg->{name},
 				type => 'package',
 			} )
@@ -239,7 +239,7 @@ sub _add_subtree {
 				-1,
 				-1,
 				Wx::TreeItemData->new( {
-					line => $item->{line},
+					dir  => $item->{dir},
 					name => $item->{name},
 					type => $type,
 				} )
--- lib/Padre/Wx/Main.pm
+++ lib/Padre/Wx/Main.pm
@@ -2203,6 +2203,10 @@ sub on_stc_update_ui {
 	#$self->refresh_functions;
 	#$self->refresh_syntaxcheck;
 
+	# TODO move this to a more appropriate place (when switching between buffers?)
+	if (my $directory = $self->directory) {
+		$directory->update_gui;
+	}
 	return;
 }
 
