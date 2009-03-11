c911eb170899afaacb37011c1dad06398f59ef8f
Jérôme Quelin <jquelin@gmail.com>
2009-03-11 19:37:26 +0000

    new module padre::config::constants
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@3241 bc93166f-e056-4eb9-8c59-0a0840647e1b

---
+++ lib/Padre/Config/Constants.pm
@@ -0,0 +1,50 @@
+#
+# Constants used by various configuration systems.
+#
+
+package Padre::Config::Constants;
+
+
+1;
+
+__END__
+
+=head1 NAME
+
+Padre::Config::Constants - constants used by config subsystems
+
+
+=head1 DESCRIPTION
+
+Padre uses various configuration subsystems (see C<Padre::Config> for more
+information). Those systems needs to somehow agree on some basic stuff, which
+is defined in this module.
+
+
+=head1 PUBLIC API
+
+=head2 Available constants
+
+This module exports nothing by default. However, the following constants can
+be imported:
+
+=over 4
+
+=item *
+
+=back
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2008-2009 The Padre development team as listed in Padre.pm.
+
+This program is free software; you can redistribute it and/or modify it under the
+same terms as Perl 5 itself.
+
+=cut
+
+# Copyright 2008-2009 The Padre development team as listed in Padre.pm.
+# LICENSE
+# This program is free software; you can redistribute it and/or
+# modify it under the same terms as Perl 5 itself.
