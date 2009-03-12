f398b135127c838b954fdacda26157fc3d0d339e
Jérôme Quelin <jquelin@gmail.com>
2009-03-12 08:56:41 +0000

    comments & pod for padre::config::host
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@3251 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- lib/Padre/Config/Host.pm
+++ lib/Padre/Config/Host.pm
@@ -1,6 +1,8 @@
-package Padre::Config::Host;
-
+#
 # Configuration and state data related to the host that Padre is running on.
+#
+
+package Padre::Config::Host;
 
 use 5.008;
 use strict;
@@ -10,8 +12,6 @@ use warnings;
 # use Padre::DB ();
 
 our $VERSION = '0.28';
-
-
 
 
 
@@ -59,6 +59,71 @@ sub version {
 }
 
 1;
+
+__END__
+
+=head1 NAME
+
+Padre::Config::Host - Padre configuration storing host state data
+
+
+=head1 DESCRIPTION
+
+This class implements the state data of the host on which Padre is running.
+See C<Padre::Config> for more information on the various types of preferences
+supported by Padre.
+
+All those state data are stored in a database managed with C<Padre::DB>.
+Refer to this module for more information on how this works.
+
+
+=head1 PUBLIC API
+
+=head2 Constructors
+
+=over 4
+
+=item my $config = Padre::Config::Host->read;
+
+Load & return the host configuration from the database. Return undef in
+case of failure.
+
+No params.
+
+
+=back
+
+
+=head2 Object methods
+
+=over 4
+
+=item my $revision = $config->version;
+
+Return the config schema revision. Indeed, we might want to change the
+underlying storage later on.
+
+No params.
+
+
+=item $config->write;
+
+(Over-)write host configuration to the database.
+
+No params.
+
+=back
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2008-2009 The Padre development team as listed in Padre.pm.
+
+This program is free software; you can redistribute it and/or modify it under the
+same terms as Perl 5 itself.
+
+=cut
+
 
 # Copyright 2008-2009 The Padre development team as listed in Padre.pm.
 # LICENSE
