857b40b3262100035f62188e44495e1e24f8fbcc
Jérôme Quelin <jquelin@gmail.com>
2009-03-26 23:24:18 +0000

    third gen plugin manager
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@3475 bc93166f-e056-4eb9-8c59-0a0840647e1b

---
+++ lib/Padre/Wx/Dialog/PluginManager3.pm
@@ -0,0 +1,85 @@
+package Padre::Wx::Dialog::PluginManager3;
+
+# Third-generation plugin manager
+
+use strict;
+use warnings;
+use Carp                    ();
+use URI::file               ();
+use Params::Util            qw{_INSTANCE};
+use Padre::Util             ();
+use Padre::Wx               ();
+use Padre::Wx::Dialog::HTML ();
+
+our $VERSION = '0.29';
+use base 'Padre::Wx::Dialog::HTML';
+
+sub new {
+	my ($class, $parent, $manager) = @_;
+	my $self  = $class->SUPER::new(
+		title => Wx::gettext('Plugin Manager'),
+	);
+
+    $self->{manager} = $manager;
+	unless ( _INSTANCE($self->{manager}, 'Padre::PluginManager') ) {
+		Carp::croak("Missing or invalid Padre::PluginManager object");
+	}
+
+	return $self;
+}
+
+
+sub show {
+    my $self = shift;
+    $self->refresh;
+    $self->Show;
+}
+
+# Render the content of the dialog based on the plugins
+sub html {
+	my $self    = shift;
+	my $manager = $self->{manager};
+    return '' unless defined $manager;
+
+	my @rows = ();
+	my $file = Padre::Util::sharefile('plugin.png');
+	unless ( -f $file ) {
+		die "Failed to find $file";
+	}
+	my $icon = URI::file->new( $file )->as_string;
+	foreach my $name ( $manager->plugin_names ) {
+		my $plugin   = $manager->_plugin($name);
+		my $namehtml = "<b>"  . $plugin->plugin_name . "</b>";
+        my $version  = $plugin->version || '???';
+		my $cellhtml = "<td bgcolor='#FFFFFF'>"
+			. $namehtml
+			. "&nbsp;&nbsp;&nbsp;"
+			. $version
+			. "</td>";
+		my $rowhtml  = "<tr>"
+			. "<td width='32'><img src='$icon' height='16' width='16'></td>"
+			. $cellhtml
+			. "</tr>";
+		push @rows, $rowhtml;
+	}
+
+	# Wrap in the overall page
+	my $rowshtml = join( "\n", @rows );
+	return <<"END_HTML";
+<html>
+<head>
+</head>
+<body bgcolor="#CCCCCC">
+<table border="1" cellpadding="10" cellspacing="0" width="100%">
+$rowshtml
+</table>
+</body>
+</html>
+END_HTML
+}
+
+1;
+# Copyright 2008-2009 The Padre development team as listed in Padre.pm.
+# LICENSE
+# This program is free software; you can redistribute it and/or
+# modify it under the same terms as Perl 5 itself.
