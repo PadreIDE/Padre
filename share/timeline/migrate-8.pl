4fb64ca2cd3ba773994a922dc1374156269fc287
Jérôme Quelin <jquelin@gmail.com>
2009-04-06 19:43:47 +0000

    new migration script
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@3800 bc93166f-e056-4eb9-8c59-0a0840647e1b

---
+++ share/timeline/migrate-8.pl
@@ -0,0 +1,48 @@
+use strict;
+use File::Spec ();
+use lib File::Spec->rel2abs(
+	File::Spec->catdir(
+		File::Spec->updir,
+		File::Spec->updir,
+		File::Spec->updir,
+		File::Spec->updir,
+		File::Spec->updir,
+	)
+);
+use Padre::DB::Patch;
+
+
+
+
+
+#####################################################################
+# patch content
+
+# remove the session table created in migrate-5
+do(<<'END_SQL');
+DROP TABLE session
+END_SQL
+
+# create the new session table
+do(<<'END_SQL');
+CREATE TABLE session (
+	id INTEGER NOT NULL PRIMARY KEY,
+	name VARCHAR(255) UNIQUE NOT NULL,
+	description VARCHAR(255),
+	last_update DATE
+)
+END_SQL
+
+# create the table containing the session files
+do(<<'END_SQL');
+CREATE TABLE session_file (
+	id INTEGER NOT NULL PRIMARY KEY,
+	file VARCHAR(255) NOT NULL,
+	position INTEGER NOT NULL,
+	focus BOOLEAN NOT NULL,
+	session INTEGER NOT NULL,
+	FOREIGN KEY (session) REFERENCES session(id)
+)
+END_SQL
+
+exit(0);
