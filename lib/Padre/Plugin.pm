67494442591567577e1de1ccc0b040eee157c69e
Jérôme Quelin <jquelin@gmail.com>
2009-04-24 17:03:37 +0000

    documenting + implementing default method plugin_locale_directory()
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@4116 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- lib/Padre/Plugin.pm
+++ lib/Padre/Plugin.pm
@@ -45,6 +45,8 @@ use 5.008;
 use strict;
 use warnings;
 use Carp         ();
+use File::Spec::Functions qw{ catdir };
+use File::ShareDir        ();
 use Scalar::Util ();
 use Params::Util ( '_HASH0', '_INSTANCE' );
 use YAML::Tiny   ();
@@ -94,6 +96,28 @@ C<Wx::Bitmap> object.
 There is no default default implementation, meaning that a default
 plugin icon will be displayed for the plugin.
 
+
+
+=head2 plugin_locale_directory
+
+The C<plugin_directory_locale()> method will be called by Padre to
+know where to look for your plugin l10n catalog. Those catalogs
+should be named C<$plugin-$locale.po> (or C<.mo> for the compiled
+form). That is, C<Vi-de.po> for the german locale of
+C<Padre::Plugin::Vi>. It defaults to C<$sharedir/locale> (with
+C<$sharedir> as defined by C<File::ShareDir>), and thus should
+basically work as is for your plugin.
+
+=cut
+
+sub plugin_locale_directory {
+	my ($self) = @_;
+	my $pkg = ref($self) || $self;
+
+	my $distdir;
+	eval { $distdir = File::ShareDir::dist_dir($pkg); };
+	return $@ ? undef : catdir( $distdir, 'share', 'locale' )
+}
 
 
 =head2 padre_interfaces
@@ -613,9 +637,6 @@ sub main {
 
 =pod
 
-=head1 AUTHOR
-
-Adam Kennedy C<adamk@cpan.org>
 
 =head1 SEE ALSO
 
