3c41907026665325bf7c9ae3baad2a3641cdb21a
Steffen Müller <smueller@cpan.org>
2009-05-08 12:57:07 +0000

    Ctrl-Click functions to jump to their definition within the document
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@4364 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- lib/Padre/Document/Perl.pm
+++ lib/Padre/Document/Perl.pm
@@ -261,14 +261,14 @@ sub keywords {
 sub get_functions {
 	my $self = shift;
 	my $text = $self->text_get;
-	return $text =~ m/[\012\015]sub\s+(\w+(?:::\w+)*)/g;
+	return $text =~ m/[\012\015]\s*sub\s+(\w+(?:::\w+)*)/g;
 }
 
 sub get_function_regex {
 
-	# This emulates qr/(?<=^|[\012\0125])sub\s$name\b/ but without
+	# This emulates qr/(?<=^|[\012\015])sub\s$name\b/ but without
 	# triggering a "Variable length lookbehind not implemented" error.
-	return qr/(?:(?<=^)sub\s+$_[1]|(?<=[\012\0125])sub\s+$_[1])\b/;
+	return qr/(?:(?<=^)\s*sub\s+$_[1]|(?<=[\012\015])\s*sub\s+$_[1])\b/;
 }
 
 sub get_command {
@@ -476,9 +476,10 @@ sub _get_current_symbol {
 		$col--;
 	}
 
-	if ( $col == 0 or substr( $line_content, $col + 1, 1 ) !~ /^[#\w:\']$/ ) {
+	if ( substr( $line_content, $col + 1, 1 ) !~ /^[#\w:\']$/ ) {
 		return ();
 	}
+
 	
 	# Extract the token, too.
 	my $token;
@@ -488,6 +489,13 @@ sub _get_current_symbol {
 	else {
 		die "This shouldn't happen. The algorithm is wrong";
 	}
+	# truncate token
+	if ($token =~ /^(\W*[\w:]+)/) {
+		$token = $1;
+	}
+	# remove garbage first charactor from the token in case it's
+	# not a variable (Example: ->foo becomes >foo but should be foo)
+	$token =~ s/^[^\w\$\@\%\*\&:]//;
 	
 	return([ $line + 1, $col + 1 ], $token);
 }
@@ -693,11 +701,23 @@ sub event_on_left_up {
 		my $pos = $editor->PositionFromPoint($point);
 		
 		my ($location, $token) = _get_current_symbol( $self->editor, $pos );
-		
+
 		# Does it look like a variable?
 		if ( defined $location and $token =~ /^[\$\*\@\%\&]/) {
 			# FIXME editor document accessor?
 			$editor->{Document}->find_variable_declaration();
+		}
+		# Does it look like a function?
+		else {
+			my ( $start, $end ) = Padre::Util::get_matches(
+				$editor->GetText,
+				$self->get_function_regex($token),
+				$editor->GetSelection,    # Provides two params
+			);
+			if ( defined $start ) {
+				# Move the selection to the sub location
+				$editor->goto_pos_centerize($start);
+			}
 		}
 	} # end if control-click
 }
