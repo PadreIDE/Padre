cf26b4996e9f3c6367d702131cfbaadd595d45e0
Jérôme Quelin <jquelin@gmail.com>
2009-05-19 17:09:01 +0000

    removing task status from toolbar where it doesn't belong anymore
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@4724 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- lib/Padre/Wx/ToolBar.pm
+++ lib/Padre/Wx/ToolBar.pm
@@ -133,117 +133,7 @@ sub new {
 		},
 	);
 
-	$self->AddSeparator;
-
-	# Task status
-
-	# There can be three statuses:
-	# idle, running (light load), and load (high load).
-	# They'll be switched on demand by update_task_status()
-	# Here, we just set up a default state of idle
-	$self->{task_status_idle_id}    = Wx::NewId;
-	$self->{task_status_running_id} = Wx::NewId;
-	$self->{task_status_load_id}    = Wx::NewId;
-
-	$self->AddTool(
-		$self->{task_status_idle_id}, '',
-		Padre::Wx::Icon::find('status/padre-tasks-idle'),
-		Wx::gettext('Background Tasks are idle'),
-	);
-
-	# connect the dumping of the running task map to the output
-	# window to each one of the tool states
-	foreach my $id ( map { $self->{"task_status_${_}_id"} } qw(idle running load) ) {
-		Wx::Event::EVT_TOOL(
-			$main, $id, \&Padre::TaskManager::on_dump_running_tasks,
-		);
-	}
-
-	# Remember the id of the current status for update checks
-	$self->{task_status_id} = $self->{task_status_idle_id};
-
-	# Remember the position of the status icon for replacement
-	$self->{task_status_tool_pos} = $self->GetToolPos( $self->{task_status_idle_id} );
-
 	return $self;
-}
-
-# checks whether a Task status icon update is in order
-# and if so, changes the icon to one of the other states
-sub update_task_status {
-	my $self    = shift;
-	my $manager = Padre->ide->task_manager;
-
-	# Still in editor-startup phase, default to idle
-	return $self->set_task_status_idle unless defined $manager;
-
-	my $running = $manager->running_tasks;
-	return $self->set_task_status_idle unless $running;
-
-	my $max_workers = $manager->max_no_workers;
-	my $jobs        = $manager->task_queue->pending + $running;
-
-	# High load is defined as the state when the number of
-	# running and pending jobs is larger that twice the
-	# MAXIMUM number of workers
-	if ( $jobs > 2 * $max_workers ) {
-		return $self->set_task_status_load;
-	}
-	return $self->set_task_status_running;
-}
-
-sub set_task_status_idle {
-	my $self = shift;
-	my $id   = $self->{task_status_idle_id};
-	return if $self->{task_status_id} == $id;
-
-	my $bitmap = Padre::Wx::Icon::find('status/padre-tasks-idle');
-	my $text   = Wx::gettext('Background Tasks are idle');
-	return $self->_set_task_status( $id, $bitmap, $text );
-}
-
-sub set_task_status_running {
-	my $self = shift;
-	my $id   = $self->{task_status_running_id};
-	return if $self->{task_status_id} == $id;
-
-	my $bitmap = Padre::Wx::Icon::find('status/padre-tasks-running');
-	my $text   = Wx::gettext('Background Tasks are running');
-	return $self->_set_task_status( $id, $bitmap, $text );
-}
-
-sub set_task_status_load {
-	my $self = shift;
-	my $id   = $self->{task_status_load_id};
-	return if $self->{task_status_id} == $id;
-
-	my $bitmap = Padre::Wx::Icon::find('status/padre-tasks-load');
-	my $text   = Wx::gettext('Background Tasks are running with high load');
-	return $self->_set_task_status( $id, $bitmap, $text );
-}
-
-# Replaces the actual Task status Tool in the ToolBar.
-# Starting with wx 2.9, this can be removed in favour
-# of simply updating the bitmap.
-sub _set_task_status {
-	my $self   = shift;
-	my $id     = shift;
-	my $bitmap = shift;
-	my $text   = shift;
-
-	$self->{task_status_id} = $id;
-	$self->DeleteToolByPos( $self->{task_status_tool_pos} );
-	$self->InsertTool(
-		$self->{task_status_tool_pos},
-		$id, '',
-		$bitmap,
-		Wx::wxNullBitmap,
-		Wx::wxITEM_NORMAL,
-		$text,
-	);
-	$self->Realize;
-
-	return 1;
 }
 
 sub refresh {
@@ -262,8 +152,6 @@ sub refresh {
 	$self->EnableTool( Wx::wxID_COPY,  ($selection) );
 	$self->EnableTool( Wx::wxID_PASTE, ( $editor and $editor->CanPaste ) );
 	$self->EnableTool( Wx::wxID_SELECTALL, ( $editor ? 1 : 0 ) );
-
-	$self->update_task_status;
 
 	return;
 }
