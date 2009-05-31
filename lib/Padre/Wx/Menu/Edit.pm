4e4b0034159dfa54bbfc439d45337f22105e303e
Breno G. de Oliveira <garu@cpan.org>
2009-05-31 04:20:03 +0000

    Edit menu now correctly disables "toggle comments" option on refresh, otherwise it crashed bigtime if selected with no doc opened
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@5048 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- lib/Padre/Wx/Menu/Edit.pm
+++ lib/Padre/Wx/Menu/Edit.pm
@@ -477,6 +477,7 @@ sub refresh {
 	$self->{brace_match}->Enable($hasdoc);
 	$self->{join_lines}->Enable($hasdoc);
 	$self->{snippets}->Enable($hasdoc);
+	$self->{comment_toggle}->Enable($hasdoc);
 	$self->{comment_out}->Enable($hasdoc);
 	$self->{uncomment}->Enable($hasdoc);
 	$self->{diff2saved}->Enable($hasdoc);
