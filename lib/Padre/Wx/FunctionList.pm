9d3fbe51344e26925e593964a6d72860147898e5
adamk <adamk@cpan.org>
2009-06-07 07:48:20 +0000

    I've decided that I really really REALLY hate "Sub List" and I'm reverting it to Functions
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@5116 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- lib/Padre/Wx/FunctionList.pm
+++ lib/Padre/Wx/FunctionList.pm
@@ -68,7 +68,7 @@ sub main {
 }
 
 sub gettext_label {
-	Wx::gettext('Sub List');
+	Wx::gettext('Functions');
 }
 
 #####################################################################
@@ -86,9 +86,8 @@ sub on_char {
 	# Remove the bit ( Wx::wxMOD_META) set by Num Lock being pressed on Linux
 	# TODO: This is cargo-cult
 	$mod = $mod & ( Wx::wxMOD_ALT + Wx::wxMOD_CMD + Wx::wxMOD_SHIFT );
-	unless ($mod) {
+	unless ( $mod ) {
 		if ( $code <= 255 and $code > 0 and chr($code) =~ /^[\w_:-]$/ ) {
-
 			# transform - => _ for convenience
 			$code = 95 if $code == 45;
 
@@ -127,7 +126,6 @@ sub on_list_item_activated {
 		$editor->GetSelection,    # Provides two params
 	);
 	unless ( defined $start ) {
-
 		# Couldn't find it
 		return;
 	}
