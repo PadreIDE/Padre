eb93e0b4ed8e7da7515112abb4918af32e048d0e
adamk <adamk@cpan.org>
2009-06-10 13:46:24 +0000

    Abstract search object stub to reserve the name
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@5149 bc93166f-e056-4eb9-8c59-0a0840647e1b

---
+++ lib/Padre/Search.pm
@@ -0,0 +1,19 @@
+package Padre::Search;
+
+# This is the beginnings of a new Padre Search API.
+# The primary goal is to support the concept of a "search" independantly
+# from the dialog or other mechanism used to create the search.
+# Please ignore this file until I get something actually working here.
+# --Adam K
+
+use strict;
+use warnings;
+
+our $VERSION = '0.36';
+
+1;
+
+# Copyright 2008-2009 The Padre development team as listed in Padre.pm.
+# LICENSE
+# This program is free software; you can redistribute it and/or
+# modify it under the same terms as Perl 5 itself.
