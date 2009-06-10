bf27a5194b10770d09134d9478e015c92358ada0
adamk <adamk@cpan.org>
2009-06-10 13:50:32 +0000

    Added a MainChild role to simplify the plethora of things that have Padre::Wx::Main as a Wx-layer parent
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@5155 bc93166f-e056-4eb9-8c59-0a0840647e1b

---
+++ lib/Padre/Wx/Role/MainChild.pm
@@ -0,0 +1,104 @@
+package Padre::Wx::Role::MainChild;
+
+=pod
+
+=head1 NAME
+
+Padre::Wx::Role::MainChild - Convenience methods for children of the main window
+
+=head1 DESCRIPTION
+
+This psuedo-role implements the fairly common method pattern for Wx elements that
+are children of L<Padre::Wx::Main>.
+
+=head1 METHODS
+
+=cut
+
+use strict;
+use warnings;
+use Padre::Current ();
+
+our $VERSION = '0.36';
+
+# The three most common things we need are implemented directly
+
+=pod
+
+=head2 main
+
+    my $main = $object->main;
+
+Get the L<Padre::Wx::Main> main window that this object is a child of.
+
+=cut
+
+sub main {
+	$_[0]->GetParent;
+}
+
+=pod
+
+=head2 ide
+
+    my $ide = $object->ide;
+
+Get the L<Padre> IDE instance that this object is a child of.
+
+=cut
+
+sub ide {
+	$_[0]->GetGrandParent;
+}
+
+=pod
+
+=head2 config
+
+    my $config = $object->config;
+
+Get the L<Padre::Config> for the current user. Provided mainly as a
+convenience because it is needed so often.
+
+=cut
+
+sub config {
+	$_[0]->GetGrandParent->config;
+}
+
+=pod
+
+=head2 current
+
+    my $current = $object->current;
+
+Get a new C<Padre::Current> context object.
+
+=cut
+
+sub current {
+	Padre::Current->new( main => $_[0]->GetParent );
+}
+
+1;
+
+=pod
+
+=back
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2008-2009 The Padre development team as listed in Padre.pm.
+
+This program is free software; you can redistribute
+it and/or modify it under the same terms as Perl itself.
+
+The full text of the license can be found in the
+LICENSE file included with this module.
+
+=cut
+
+# Copyright 2008-2009 The Padre development team as listed in Padre.pm.
+# LICENSE
+# This program is free software; you can redistribute it and/or
+# modify it under the same terms as Perl 5 itself.
