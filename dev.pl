5c18707c700a68fc7d041c148b57ec89b77082a6
szabgab <szabgab@gmail.com>
2009-06-18 14:05:08 +0000

    refactoring the mo creating code
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@5304 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- dev.pl
+++ dev.pl
@@ -22,27 +22,8 @@ unless ( -d "$FindBin::Bin/blib" ) {
 	error("You must now have run 'perl Makefile.PL' and '$make' in order to run dev.pl");
 }
 
-my $msgfmt;
-if ( $^O =~ /(linux|bsd)/ ) {
-	$msgfmt = scalar File::Which::which('msgfmt');
-} elsif ( $^O eq 'MSWin32' ) {
-	eval {
-		require File::Glob::Windows;
-	};
-	if( $@ ) {
-		die("Default glob() will misinterpret spaces in folder names as seperators, install File::Glob::Windows to fix this behavior!");
-	}
-	my $p = "C:/Program Files/GnuWin32/bin/msgfmt.exe";
-	if ( -e $p ) {
-		$msgfmt = $p;
-	} else {
-		$msgfmt = scalar File::Which::which('msgfmt');
-	}
-}
+convert_po_to_mo($FindBin::Bin);
 
-if ( $msgfmt ) {
-	convert_po_to_mo($FindBin::Bin);
-}
 
 my $perl = Probe::Perl->find_perl_interpreter;
 if ( $^O eq 'darwin' ) {
@@ -75,9 +56,7 @@ if ( grep { $_ eq '-h' } @ARGV ) {
 	if ( opendir my $dh, $dir ) {
 		my @plugins = grep { $_ =~ /^Padre-Plugin-/ } readdir $dh;
 		foreach my $plugin ( @plugins ) {
-			if( $msgfmt ) {
-				convert_po_to_mo("$dir/$plugin");
-			}
+			convert_po_to_mo("$dir/$plugin");
 			push @cmd, "-I$dir/$plugin/lib";
 		}
 	}
@@ -93,7 +72,10 @@ sub error {
 }
 
 sub convert_po_to_mo {
-	my $path = shift;
+	my $path   = shift;
+	my $msgfmt = get_msgfmt();
+	return if not $msgfmt;
+
 	my @mo;
 	if ( $^O eq 'MSWin32' ) {
 		@mo = map {
@@ -111,4 +93,27 @@ sub convert_po_to_mo {
 			"$path/share/locale/$locale.po",
 		);
 	}
+}
+
+sub get_msgfmt {
+
+	my $msgfmt;
+	if ( $^O =~ /(linux|bsd)/ ) {
+		$msgfmt = scalar File::Which::which('msgfmt');
+	} elsif ( $^O eq 'MSWin32' ) {
+		eval {
+			require File::Glob::Windows;
+		};
+		if( $@ ) {
+			die("Default glob() will misinterpret spaces in folder names as seperators, install File::Glob::Windows to fix this behavior!");
+		}
+		my $p = "C:/Program Files/GnuWin32/bin/msgfmt.exe";
+		if ( -e $p ) {
+			$msgfmt = $p;
+		} else {
+			$msgfmt = scalar File::Which::which('msgfmt');
+		}
+	}
+	
+	return $msgfmt;
 }
