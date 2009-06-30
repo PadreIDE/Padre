12b62466be850d13c54077bd04ae77b8df439bec
submersible_toaster <submersible_toaster>
2009-06-30 10:04:10 +0000

    document the Service api
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@5733 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- lib/Padre/Service.pm
+++ lib/Padre/Service.pm
@@ -6,9 +6,63 @@ our @ISA = 'Padre::Task';
 
 =pod
 
+=head1 NAME
+
+Padre::Service - API for non trivial Padre::Task
+
+=head2 SYNOPSIS
+
+  # Create your service, default implementation warns to output
+  #  sleeps 1 second and loops over.
+  my $service = Padre::Service->new( 
+	main_thread_only => \&show_my_dialog,
+  );
+  $service->schedule;
+  
+  # Later
+  $service->shutdown; # Your show_my_dialog will be called...,eventually
+
+=head1 DESCRIPTION
+
+Padre::Service extends L<Padre::Task> to provide a means to launch and 
+control a long running background service, without blocking the editor.
+
+=head2 EXTENDING
+
+To extend this class, inherit it and implement C<service_loop> and preferabbly
+C<hangup>
+
+C<service_loop> should not block forever. If there is no work for the service to do
+then return immediately, allowing the C<<Task->run>> loop to
+
+  package Padre::Service::HTTPD
+  use base qw( Padre::Service );
+  
+  sub prepare { # Build a dummy httpd.conf from $self , "BREAK" if error }
+  
+  sub service_start { # Launch httpd binary goodness, IPC::Run3 maybe? }
+  
+  sub service_shutdown { # Clean shutdown httpd binary }
+  
+  sub service_loop { # ->select($timeout) on your IPC handles }
+  
+  sub hangup { ->service_shutdown ?!?}
+  
+  sub terminate { # Stop everything, brutally }
+  
+  sub service_results { # Returned as the task return to Padre, }
+  
+=head1 METHODS
+
+=head2 run
+
+Overrides C<Padre::Task::run> providing a non-blocking loop around the
+TaskManager to Service shared queue.
+C<run> will call ->hangup or ->terminate on your service if instructed
+by the main thread, otherwise C<service_loop> is called in void context
+with no arguments B<IN A TIGHT LOOP>.
+
 =cut
-
-
 
 sub run {
 	my ($self,$queue) = @_;
@@ -24,6 +78,8 @@ sub run {
 		
 		my $incoming = $queue->peek(0);
 		# Peek at the queue - for something addressed to us
+		# YUK - how about a dedicated queue per service
+		# peek and poke went out in the dark ages didn't they
 		if ( $incoming =~ m{$tid;} ) {
 			my $instruction = $queue->dequeue;
 			my ($command) =  $instruction =~ m{^$tid;(HANGUP|TERMINATE)};
@@ -35,28 +91,54 @@ sub run {
 				$self->terminate;
 				$running = 0;
 			}
-			else { warn "Unrecognised command event $command"; }
+			elsif ( $command eq 'PING' ) {
+				$self->post_event( $event , "$tid;ALIVE" );
+			}
+			else { 
+				$self->task_warn( "$self : Unrecognised command event $command" 
+				)
+			}
+		
 		}
 		
 		
-		#$self->post_event( $event , "$tid;ALIVE" );		
 	}
-	
-	return $self->service_results;
+	return;
 }
 
+=head2 hangup
+
+Called on your service when the editor requests a hangup. Your service is obliged
+to gracefully stop what it is doing and return from this method as soon as possible
+
+=cut
 
 sub hangup {
 	my ($self) = @_;
 	
 }
 
+=head2 terminate
+
+Called on your service when TaskManager believes your service is hung or not
+responding to a C<<->hangup>. Your service is obliged to B<IMMEDIATELY> stop
+everything and to hell with the consequences. 
+
+=cut
 
 sub terminate {
 	my ($self) = @_;
 
 }
 
+
+=head2 service_loop
+
+Called in a loop while the service is believed to be running
+The default implementation emits output to the editor and sleeps for a
+second before returning control to the loop.
+
+=cut
 
 {
 my $i = 0;
@@ -69,8 +151,5 @@ sub service_loop {
 }
 }
 
-sub service_results {
-	"Service Completed!";
-}
 
 1;
