fd81361ae367fa1d9d1c9b4f932964212ec5aa08
szabgab <szabgab@gmail.com>
2009-07-02 10:24:06 +0000

    example of a progress bar
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@5764 bc93166f-e056-4eb9-8c59-0a0840647e1b

---
+++ eg/wx/21_progress_bar.pl
@@ -0,0 +1,48 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+#############################################################################
+##
+## Based on lib/Wx/DemoModules/wxProgressDialog.pm 
+## from the wxDemo ## written by Mattia Barbon
+## Copyright:   (c) The Padre development team
+## Licence:     This program is free software; you can redistribute it and/or
+##              modify it under the same terms as Perl itself
+#############################################################################
+
+use Wx qw(:progressdialog);
+show_progress_bar();
+
+sub show_progress_bar {
+    my( $window ) = @_;
+
+    my $max = 20;
+
+    my $dialog = Wx::ProgressDialog->new( 'Progress dialog example',
+                                          'An informative message',
+                                          $max, $window,
+                                          wxPD_CAN_ABORT|wxPD_AUTO_HIDE|
+                                          wxPD_APP_MODAL|wxPD_ELAPSED_TIME|
+                                          wxPD_ESTIMATED_TIME|
+                                          wxPD_REMAINING_TIME );
+
+    my $continue;
+    foreach my $i ( 1 .. $max ) {
+        sleep 1;
+        if( $i == $max ) { 
+            $continue = $dialog->Update( $i, "That's all, folks!" );
+        } elsif( $i == int( $max / 2 ) ) {
+            $continue = $dialog->Update( $i, "Only a half left" );
+        } else {
+            $continue = $dialog->Update( $i );
+        }
+        last unless $continue;
+    }
+
+    Wx::LogMessage( $continue ? "Countdown from $max finished" :
+                                "Progress dialog aborted" );
+
+    $dialog->Destroy;
+}
+
