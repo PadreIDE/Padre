5bc4257ae3a6da1c4dadee8b42b3b60eb498735f
Ryan Niebur <RyanRyan52@gmail.com>
2009-07-06 05:10:58 +0000

    remove the .mo file generation
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@5862 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- privlib/Tools.pm
+++ privlib/Tools.pm
@@ -13,48 +13,6 @@ if ( $^O eq 'MSWin32' ) {
 	require File::Glob::Windows;
 }
 
-sub convert_po_to_mo {
-	my $path   = shift;
-	
-	my $msgfmt = get_msgfmt();
-	return if not $msgfmt;
-
-	my @mo;
-	if ( $^O eq 'MSWin32' ) {
-		@mo = map {
-			substr( File::Basename::basename($_), 0, -3 )
-		} File::Glob::Windows::glob("$path/share/locale/*.po");
-	} else {
-		@mo = map {
-			substr( File::Basename::basename($_), 0, -3 )
-		} glob "$path/share/locale/*.po";
-	}
-	foreach my $locale ( @mo ) {
-		system(
-			$msgfmt, "-o",
-			"$path/share/locale/$locale.mo",
-			"$path/share/locale/$locale.po",
-		);
-	}
-}
-
-sub get_msgfmt {
-
-	my $msgfmt;
-	if ( $^O =~ /(linux|bsd)/ ) {
-		$msgfmt = scalar File::Which::which('msgfmt');
-	} elsif ( $^O eq 'MSWin32' ) {
-		my $p = "C:/Program Files/GnuWin32/bin/msgfmt.exe";
-		if ( -e $p ) {
-			$msgfmt = $p;
-		} else {
-			$msgfmt = scalar File::Which::which('msgfmt');
-		}
-	}
-	
-	return $msgfmt;
-}
-
 sub get_perl {
 	my $perl = Probe::Perl->find_perl_interpreter;
 	if ( $^O eq 'darwin' ) {
