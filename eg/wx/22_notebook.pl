1b2721097471744c50876f5552fbcae01935040e
szabgab <szabgab@gmail.com>
2009-07-19 19:23:42 +0000

    start writing simple example with wxNotebook
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@6271 bc93166f-e056-4eb9-8c59-0a0840647e1b

---
+++ eg/wx/22_notebook.pl
@@ -0,0 +1,66 @@
+#!/usr/bin/perl 
+use strict;
+use warnings;
+
+
+#############################################################################
+##
+##
+## Copyright:   (c) The Padre development team
+## Licence:     This program is free software; you can redistribute it and/or
+##              modify it under the same terms as Perl itself
+#############################################################################
+
+my $app = Demo::ListView->new;
+$app->MainLoop;
+
+
+#####################
+package Demo::ListView;
+use strict;
+use warnings;
+
+use base 'Wx::App';
+
+sub OnInit {
+    my $frame = Demo::Frame->new;
+    $frame->Show( 1 );
+}
+
+#####################
+package Demo::Frame;
+use strict;
+use warnings;
+
+
+use Wx ':everything';
+use Wx::Event ':everything';
+
+use base 'Wx::Frame';
+
+sub new {
+    my ($class) = @_;
+
+    my $self = $class->SUPER::new( undef,
+                                 -1,
+                                 'Notebook ',
+                                 [-1, -1],
+                                 [750, 700],
+                                 );
+                                 
+    # Creating the notebook with tabs (also called panes)
+    my $nb = Wx::Notebook->new
+      ( $self, -1, wxDefaultPosition, wxDefaultSize,
+        wxNO_FULL_REPAINT_ON_RESIZE|wxCLIP_CHILDREN );
+
+
+    # creating the content of the first tab
+    my $editor = Wx::TextCtrl->new($nb, -1, '', Wx::wxDefaultPosition, Wx::wxDefaultSize,
+        wxTE_MULTILINE|wxNO_FULL_REPAINT_ON_RESIZE);
+    # add first tab
+    $nb->AddPage($editor, 'Editor', 1);
+
+
+    return $self;
+}
+
