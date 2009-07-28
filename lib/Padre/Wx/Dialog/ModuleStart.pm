258ed5e1b4e79e44a843ff064051483271b0c564
nxadm <pub.claudio@gmail.com>
2009-07-28 11:50:27 +0000

    Set dafaults for modules creator backend and license
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@6739 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- lib/Padre/Wx/Dialog/ModuleStart.pm
+++ lib/Padre/Wx/Dialog/ModuleStart.pm
@@ -73,8 +73,13 @@ sub dialog {
 
 	$dialog->{_widgets_}->{_author_name_}->SetValue( $config->identity_name );
 	$dialog->{_widgets_}->{_email_}->SetValue( $config->identity_email );
-	$dialog->{_widgets_}->{_builder_choice_}->SetValue( $config->builder );
-	$dialog->{_widgets_}->{_license_choice_}->SetValue( $config->license );
+	if ($config->builder) {
+		$dialog->{_widgets_}->{_builder_choice_}->SetValue( $config->builder );
+	}
+	else { $dialog->{_widgets_}->{_builder_choice_}->SetValue( 'ExtUtils::MakeMaker' ) }
+	if ($config->license) {
+	$dialog->{_widgets_}->{_license_choice_}->SetValue( $config->license ); }
+	else { $dialog->{_widgets_}->{_license_choice_}->SetValue( 'restrictive'); }
 	$dialog->{_widgets_}->{_directory_}->SetPath( $config->module_start_directory );
 
 	$dialog->{_widgets_}->{_ok_}->SetDefault;
