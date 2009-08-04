a11783987914ae3a75a96ed766b57dbb67eea323
adamk <adamk@cpan.org>
2009-08-04 13:37:49 +0000

    Adding copyright
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@6879 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- lib/Padre/Splash.pm
+++ lib/Padre/Splash.pm
@@ -24,3 +24,8 @@ unless ( $ENV{HARNESS_ACTIVE} ) {
 }
 
 1;
+
+# Copyright 2008-2009 The Padre development team as listed in Padre.pm.
+# LICENSE
+# This program is free software; you can redistribute it and/or
+# modify it under the same terms as Perl 5 itself.
