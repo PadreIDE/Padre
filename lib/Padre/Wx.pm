3b8da9ffe268a3899addd511b6130905ee59ac16
adamk <adamk@cpan.org>
2009-08-08 12:05:31 +0000

    Add an aggressive check for a new enough Wx that actually gives a useful error message
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@6943 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- lib/Padre/Wx.pm
+++ lib/Padre/Wx.pm
@@ -22,6 +22,17 @@ use Padre::Util ();
 
 our $VERSION = '0.42';
 
+# Hard version lock on a new enough Wx.pm
+BEGIN {
+	unless (
+		$Wx::VERSION
+		and
+		$Wx::VERSION >= 0.91
+	) {
+		die("You Wx.pm is not new enough (need 0.91, found $Wx::VERSION)";
+	}
+}
+
 
 
 
