4d04f26d630ce78eae3a7eb8f66fd6666f3dc73e
azawawi <ahmad.zawawi@gmail.com>
2009-08-12 21:11:06 +0000

    [Padre] made migration #11 work!
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@7071 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- share/timeline/migrate-11.pl
+++ share/timeline/migrate-11.pl
@@ -4,9 +4,9 @@ use ORLite::Migrate::Patch;
 # Create the recently used table
 do(<<'END_SQL');
 CREATE TABLE recently_used (
-	name        VARCHAR(255) PRIMARY KEY,
-	value       VARCHAR(255) NOT NULL,
-	type        VARCHAR(255) NOT NULL,
-	last_used   DATE         NOT NULL
+	name       VARCHAR(255) PRIMARY KEY,
+	value      VARCHAR(255) NOT NULL,
+	type       VARCHAR(255) NOT NULL,
+	last_used  DATE
 )
 END_SQL
