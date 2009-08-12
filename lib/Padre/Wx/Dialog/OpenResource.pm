2a4ffb3dd6159d5b0e4c5de8870414a6b7bbf9ce
azawawi <ahmad.zawawi@gmail.com>
2009-08-12 10:07:32 +0000

    [Padre] Added Padre copyright
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@7030 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- lib/Padre/Wx/Dialog/OpenResource.pm
+++ lib/Padre/Wx/Dialog/OpenResource.pm
@@ -468,9 +468,16 @@ You can simply ignore CVS, .svn and .git folders using a simple checkbox
 
 Ahmad M. Zawawi C<< <ahmad.zawawi at gmail.com> >>
 
-=head1 COPYRIGHT AND LICENSE
+=head1 COPYRIGHT & LICENSE
 
-Copyright 2009 C<< <ahmad.zawawi at gmail.com> >>
+Copyright 2008-2009 The Padre development team as listed in Padre.pm.
 
-This program is free software; you can redistribute it and/or
-modify it under the same terms as Perl 5 itself.
+This program is free software; you can redistribute
+it and/or modify it under the same terms as Perl itself.
+
+=cut
+
+# Copyright 2008-2009 The Padre development team as listed in Padre.pm.
+# LICENSE
+# This program is free software; you can redistribute it and/or
+# modify it under the same terms as Perl 5 itself.
--- lib/Padre/Wx/Dialog/OpenResource/SearchTask.pm
+++ lib/Padre/Wx/Dialog/OpenResource/SearchTask.pm
@@ -107,9 +107,16 @@ __END__
 
 Ahmad M. Zawawi C<< <ahmad.zawawi at gmail.com> >>
 
-=head1 COPYRIGHT AND LICENSE
+=head1 COPYRIGHT & LICENSE
 
-Copyright 2009 C<< <ahmad.zawawi at gmail.com> >>
+Copyright 2008-2009 The Padre development team as listed in Padre.pm.
 
-This program is free software; you can redistribute it and/or
-modify it under the same terms as Perl 5 itself.
+This program is free software; you can redistribute
+it and/or modify it under the same terms as Perl itself.
+
+=cut
+
+# Copyright 2008-2009 The Padre development team as listed in Padre.pm.
+# LICENSE
+# This program is free software; you can redistribute it and/or
+# modify it under the same terms as Perl 5 itself.
