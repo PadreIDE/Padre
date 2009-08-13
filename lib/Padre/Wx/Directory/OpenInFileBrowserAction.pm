4f072b0b16c8baf33c2e7a47d5d9b0bcab059452
azawawi <ahmad.zawawi@gmail.com>
2009-08-13 15:02:43 +0000

    [Padre] Fixed copyright/POD in OpenInFileBrowserAction
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@7140 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- lib/Padre/Wx/Directory/OpenInFileBrowserAction.pm
+++ lib/Padre/Wx/Directory/OpenInFileBrowserAction.pm
@@ -93,13 +93,33 @@ sub open_in_file_browser {
 
 __END__
 
+=head1 NAME
+
+Padre::Wx::Directory::OpenInFileBrowserAction - Ecliptic's Open in file browser action
+
+=head1 DESCRIPTION
+
+=head2 Open in File Browser (Shortcut: Ctrl + 6)
+
+For the current saved Padre document, open the platform's file manager/browser and 
+tries to select it if possible. On win32, opens the containing folder and 
+selects the file in its explorer. On *inux KDE/GNOME, opens the containing folder 
+for it.
+
 =head1 AUTHOR
 
 Ahmad M. Zawawi C<< <ahmad.zawawi at gmail.com> >>
 
-=head1 COPYRIGHT AND LICENSE
+=head1 COPYRIGHT & LICENSE
 
-Copyright 2009 C<< <ahmad.zawawi at gmail.com> >>
+Copyright 2008-2009 The Padre development team as listed in Padre.pm.
 
-This program is free software; you can redistribute it and/or
-modify it under the same terms as Perl 5 itself.
+This program is free software; you can redistribute
+it and/or modify it under the same terms as Perl itself.
+
+=cut
+
+# Copyright 2008-2009 The Padre development team as listed in Padre.pm.
+# LICENSE
+# This program is free software; you can redistribute it and/or
+# modify it under the same terms as Perl 5 itself.
