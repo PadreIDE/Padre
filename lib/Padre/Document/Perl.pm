ec811873e37c9a5d3e47a5c1352ccf3f59b30369
azawawi <ahmad.zawawi@gmail.com>
2009-08-15 18:23:53 +0000

    [Padre] Fixed a bug from the last commit.
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@7229 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- lib/Padre/Document/Perl.pm
+++ lib/Padre/Document/Perl.pm
@@ -847,8 +847,9 @@ sub on_help_render {
 	require Padre::DocBrowser::POD;
 	use Pod::Functions;
 	
+	my $hints = $Type{$topic} ? {perlfunc => 1} : ();
 	my $pod = Padre::DocBrowser::POD->new;
-	my $doc = $pod->resolve($topic, {perlfunc => $Type{$topic}});
+	my $doc = $pod->resolve($topic, $hints);
 
 	my $html = $pod->render($doc);
 	return ($html->body,$topic);
