21db8aa1178613b2de601f93e1b98af9ca0e2084
azawawi <ahmad.zawawi@gmail.com>
2009-08-15 05:16:11 +0000

    [Padre] Merged back Perl5.pm copyright fix... Or simply copied :)
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@7193 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- lib/Padre/Plugin/Perl5.pm
+++ lib/Padre/Plugin/Perl5.pm
@@ -17,3 +17,8 @@ sub plugin_name {
 }
 
 1;
+
+# Copyright 2008-2009 The Padre development team as listed in Padre.pm.
+# LICENSE
+# This program is free software; you can redistribute it and/or
+# modify it under the same terms as Perl 5 itself.
