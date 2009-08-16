a7a85f0636b006471589a102c59f7ad9d9431b5e
azawawi <ahmad.zawawi@gmail.com>
2009-08-16 13:12:32 +0000

    [Padre] Added Perl special variables (from perlvar) to Help index
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@7258 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- lib/Padre/Document/Perl.pm
+++ lib/Padre/Document/Perl.pm
@@ -933,6 +933,32 @@ sub on_help_list {
 		pod2usage podselect pstruct ptar ptardiff s2p shasum
 		splain xsubpp perlthanks/;
 
+	# Perl Special Variables (compiled these from perlvar)
+	push @index, ( '$ARG', '$_', '$a', '$b', '$<*digits*>', '$MATCH',
+		'$&', '${^MATCH}', '$PREMATCH', '$`', '${^PREMATCH}', '$POSTMATCH',
+		'$\'', '${^POSTMATCH}', '$LAST_PAREN_MATCH', '$+', '$LAST_SUBMATCH_RESULT', '$^N',
+		'@LAST_MATCH_END', '@+', '%+', '$INPUT_LINE_NUMBER', '$NR', '$.',
+		'$INPUT_RECORD_SEPARATOR', '$RS', '$/', '$OUTPUT_AUTOFLUSH', '$|', '$OUTPUT_FIELD_SEPARATOR',
+		'$OFS', '$,', '$OUTPUT_RECORD_SEPARATOR', '$ORS', '$\\', '$LIST_SEPARATOR',
+		'$"', '$SUBSCRIPT_SEPARATOR', '$SUBSEP', '$;', '$FORMAT_PAGE_NUMBER', '$%',
+		'$FORMAT_LINES_PER_PAGE', '$=', '$FORMAT_LINES_LEFT', '$-', '@LAST_MATCH_START', '@-',
+		'%-', '$FORMAT_NAME', '$~', '$FORMAT_TOP_NAME', '$^', '$FORMAT_LINE_BREAK_CHARACTERS',
+		'$:', '$FORMAT_FORMFEED', '$^L', '$ACCUMULATOR', '$^A', '$CHILD_ERROR',
+		'$?', '${^CHILD_ERROR_NATIVE}', '${^ENCODING}', '$OS_ERROR', '$ERRNO', '$!',
+		'%OS_ERROR', '%ERRNO', '%!', '$EXTENDED_OS_ERROR', '$^E', '$EVAL_ERROR',
+		'$@', '$PROCESS_ID', '$PID', '$$', '$REAL_USER_ID', '$UID',
+		'$<', '$EFFECTIVE_USER_ID', '$EUID', '$>', '$REAL_GROUP_ID', '$GID',
+		'$(', '$EFFECTIVE_GROUP_ID', '$EGID', '$)', '$PROGRAM_NAME', '$0',
+		'$[', '$]', '$COMPILING', '$^C', '$DEBUGGING', '$^D',
+		'${^RE_DEBUG_FLAGS}', '${^RE_TRIE_MAXBUF}', '$SYSTEM_FD_MAX', '$^F', '$^H', '%^H',
+		'$INPLACE_EDIT', '$^I', '$^M', '$OSNAME', '$^O', '${^OPEN}',
+		'$PERLDB', '$^P', '$LAST_REGEXP_CODE_RESULT', '$^R', '$EXCEPTIONS_BEING_CAUGHT', '$^S',
+		'$BASETIME', '$^T', '${^TAINT}', '${^UNICODE}', '${^UTF8CACHE}', '${^UTF8LOCALE}',
+		'$PERL_VERSION', '$^V', '$WARNING', '$^W', '${^WARNING_BITS}', '${^WIN32_SLOPPY_STAT}',
+		'$EXECUTABLE_NAME', '$^X', 'ARGV', '$ARGV', '@ARGV', 'ARGVOUT',
+		'@F', '@INC', '@ARG', '@_', '%INC', '%ENV',
+		'$ENV{expr}', '%SIG', '$SIG{expr}');
+
 	# Add Perl functions (perlfunc)
 	use Pod::Functions;
 	push @index, keys %Type;
