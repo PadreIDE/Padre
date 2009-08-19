63a5667b3ebc91298c8526e4bfaa9569554d7d3d
azawawi <ahmad.zawawi@gmail.com>
2009-08-19 03:35:05 +0000

    [Padre] Landing first draft of Padre::HelpProvider
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@7368 bc93166f-e056-4eb9-8c59-0a0840647e1b

---
+++ lib/Padre/HelpProvider.pm
@@ -0,0 +1,62 @@
+package Padre::HelpProvider;
+
+use 5.008;
+use strict;
+use warnings;
+
+our $VERSION = '0.43';
+
+#
+# Constructor.
+# No need to override this, just override help_init
+#
+sub new {
+	my ($class) = @_;
+
+	# Create myself :)
+	my $self = bless {}, $class;
+
+	# initialize help
+	$self->help_init;
+
+	return $self;
+}
+
+#
+# Initialize help
+#
+sub help_init {
+	warn "help_init, You need to override this to do something useful with help search";
+}
+
+#
+# Renders the help topic content into XHTML
+#
+sub help_render {
+	warn "helpi_render, You need to override this to do something useful with help search";
+}
+
+#
+# Returns the help topic list
+# 
+sub help_list {
+	warn "help_list, You need to override this to do something useful with help search";
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+Padre::HelpProvider - Padre Hel Provider API
+
+=head1 DESCRIPTION
+
+The B<Padre::HelpProvider> class provides a base class, default implementation
+and API documentation for help provision support in L<Padre>.
+
+# Copyright 2008-2009 The Padre development team as listed in Padre.pm.
+# LICENSE
+# This program is free software; you can redistribute it and/or
+# modify it under the same terms as Perl 5 itself.
