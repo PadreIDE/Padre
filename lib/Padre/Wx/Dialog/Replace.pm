45bf6dbd23aada3dd779a7b80ec44816ce5b31c7
adamk <adamk@cpan.org>
2009-08-25 02:47:53 +0000

    Rewrote the Replace dialog to be as fast as the Find dialog. It might be buggy for a little while, but I'll fix that before 0.46
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@7551 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- lib/Padre/Wx/Dialog/Replace.pm
+++ lib/Padre/Wx/Dialog/Replace.pm
@@ -43,9 +43,6 @@ Create and return a C<Padre::Wx::Dialog::Replace> search and replace widget.
 sub new {
 	my $class = shift;
 	my $main  = shift;
-	unless ($main) {
-		die("Did not pass parent to replace dialog constructor");
-	}
 
 	# Create the Wx dialog
 	my $self = $class->SUPER::new(
@@ -95,7 +92,7 @@ sub new {
 	$self->{find_regex} = Wx::CheckBox->new(
 		$self,
 		-1,
-		Wx::gettext('&Use Regex'),
+		Wx::gettext('&Regular Expression'),
 	);
 	Wx::Event::EVT_CHECKBOX(
 		$self,
@@ -109,7 +106,7 @@ sub new {
 	$self->{find_first} = Wx::CheckBox->new(
 		$self,
 		-1,
-		Wx::gettext('Close Window on &hit'),
+		Wx::gettext('Close Window on &Hit'),
 	);
 	Wx::Event::EVT_CHECKBOX(
 		$self,
@@ -148,22 +145,22 @@ sub new {
 	);
 
 	# The "Find" button
-	$self->{find} = Wx::Button->new(
+	$self->{find_button} = Wx::Button->new(
 		$self,
 		Wx::wxID_FIND,
-		Wx::gettext("&Find Next"),
+		Wx::gettext("&Find"),
 	);
 	Wx::Event::EVT_BUTTON(
 		$self,
-		$self->{find},
+		$self->{find_button},
 		sub {
-			$_[0]->find_clicked;
+			$_[0]->find_button;
 		}
 	);
 	Wx::Event::EVT_CHAR(
-		$self->{find},
+		$self->{find_button},
 		sub {
-			$self->_on_hotkey( $_[1]->GetKeyCode );
+			$self->hotkey( $_[1]->GetKeyCode );
 		}
 	);
 
@@ -177,30 +174,32 @@ sub new {
 		$self,
 		$self->{replace},
 		sub {
-			$_[0]->replace_clicked;
+			$_[0]->replace_button;
 		}
 	);
 	Wx::Event::EVT_CHAR(
 		$self->{replace},
 		sub {
-			$self->_on_hotkey( $_[1]->GetKeyCode );
+			$self->hotkey( $_[1]->GetKeyCode );
 		}
 	);
 	$self->{replace}->SetDefault;
 
 	# The "Cancel" button
-	$self->{cancel} = Wx::Button->new(
+	$self->{cancel_button} = Wx::Button->new(
 		$self,
 		Wx::wxID_CANCEL,
 		Wx::gettext("&Cancel"),
 	);
 	Wx::Event::EVT_BUTTON(
 		$self,
-		$self->{cancel},
+		$self->{cancel_button},
 		sub {
-			$_[0]->cancel;
+			$_[0]->cancel_button;
 		}
 	);
+
+	# Form Layout
 
 	# Find sizer begins here
 	my $find = Wx::StaticBoxSizer->new(
@@ -307,7 +306,7 @@ sub new {
 	# Sizer for the buttons
 	my $bottom = Wx::BoxSizer->new(Wx::wxHORIZONTAL);
 	$bottom->Add(
-		$self->{find},
+		$self->{find_button},
 		0,
 		Wx::wxGROW | Wx::wxRIGHT,
 		5,
@@ -319,7 +318,7 @@ sub new {
 		5,
 	);
 	$bottom->Add(
-		$self->{cancel},
+		$self->{cancel_button},
 		0,
 		Wx::wxGROW | Wx::wxLEFT,
 		5,
@@ -369,30 +368,6 @@ sub new {
 
 =pod
 
-=head2 cancel
-
-  $self->cancel
-
-Hide dialog when pressed cancel button.
-
-=cut
-
-sub cancel {
-	my $self = shift;
-	$self->Hide;
-
-	# As we leave the Find dialog, return the user to the current editor
-	# window so they don't need to click it.
-	my $editor = $self->current->editor;
-	if ($editor) {
-		$editor->SetFocus;
-	}
-
-	return;
-}
-
-=pod
-
 =head2 find
 
   $self->find
@@ -401,7 +376,7 @@ Grab currently selected text, if any, and place it in find combo box.
 Bring up the dialog or perform search for strings' next occurence
 if dialog is already displayed.
 
-If selection is more than one line then consider it as the limit
+TODO: if selection is more than one line then consider it as the limit
 of the search and not as the string to be used.
 
 =cut
@@ -410,32 +385,23 @@ sub find {
 	my $self = shift;
 	my $text = $self->current->text;
 
-	my $editor = $self->current->editor;
-	return unless $editor; # no replace if no file is open
+	# No search if no file is open (TODO ??)
+	return unless $self->current->editor;
 
-	# If selection is more than one line then consider it as the limit
-	# of the search and not as the string to be used (which becomes '')
-	if ( $text =~ /\n/ ) {
-		$self->{text_offset}     = $editor->GetSelectionStart;
-		$self->{text_offset_end} = $editor->GetSelectionEnd;
-		$text                    = '';
-	} else {
-		$self->{text_offset}     = 0;
-		$self->{text_offset_end} = $editor->GetLength;
-	}
+	# TODO: if selection is more than one lines then consider it as the
+	# limit of the search and not as the string to be used.
+	$text = '' if $text =~ /\n/;
 
 	# Clear out and reset the dialog, then prepare the new find
 	$self->{find_text}->refresh;
-	$self->{find_text}->SetValue($text);
-	$self->{find_text}->SetFocus;
 	$self->{replace_text}->refresh;
 
 	if ( $self->IsShown ) {
-		$self->find_next;
+		$self->find_button;
 	} else {
 		if ( length $text ) {
-
 			# Go straight to the replace field
+			$self->{find_text}->SetValue($text);
 			$self->{replace_text}->SetFocus;
 		} else {
 			$self->{find_text}->SetFocus;
@@ -446,11 +412,18 @@ sub find {
 	return;
 }
 
+
+
+
+
+######################################################################
+# Button Events
+
 =pod
 
-=head2 find_clicked
+=head2 find_button
 
-  $self->find_clicked
+  $self->find_button
 
 Executed when Find button is clicked.
 
@@ -458,125 +431,61 @@ Performs search on the term specified in the dialog.
 
 =cut
 
-sub find_clicked {
+sub find_button {
 	my $self   = shift;
-	my $config = $self->_sync_config;
+	my $main   = $self->main;
+	my $config = $self->save;
+
+	# Generate the search object
+	my $search = $self->as_search;
+	unless ( $search ) {
+		$main->error("Not a valid search");
+		return;
+	}
+
+	# Apply the search to the current editor
+	$main->search_next($search);
 
 	# If we're only searching once, we won't need the dialog any more
-	if ( $config->find_first ) {
+	if ( $self->{find_first}->GetValue ) {
 		$self->Hide;
 	}
 
-	# Return false if we don't have anything to search for
-	my $search = $self->{find_text}->GetValue;
-	return unless defined _STRING($search);
-
-	# Get the replace term
-	my $replace = $self->{replace_text}->GetValue;
-
-	# Save the terms
-	Padre::DB::History->create(
-		type => 'search',
-		name => $search,
-	) if $search;
-	Padre::DB::History->create(
-		type => 'replace',
-		name => $replace,
-	) if $replace;
-
-	# Execute the first search
-	$self->search;
-
 	return;
 }
 
 =pod
 
-=head2 find_next
+=head2 cancel_button
 
-  $self->find_next
+  $self->cancel_button
 
-Search for given string's next occurence.  If no string is available
-(either as a selected text in editor, if Quick Find is on, or from
-search history) run C<find> method.
+Hide dialog when pressed cancel button.
 
 =cut
 
-sub find_next {
+sub cancel_button {
 	my $self = shift;
-	my $term = Padre::DB::History->previous('search');
-	if ($term) {
-		$self->search;
-	} else {
-		$self->find;
-	}
-	return;
-}
+	$self->Hide;
 
-=pod
+	# Keep any setting changes.
+	$self->save;
 
-=head2 find_previous
-
-  $self->find_previous
-
-Perform backward search for string fetched from search history
-or run C<find> method if search history is empty.
-
-=cut
-
-sub find_previous {
-	my $self = shift;
-	my $term = Padre::DB::History->previous('search');
-	if ($term) {
-		$self->search( rev => 1 );
-	} else {
-		$self->find;
-	}
-	return;
-}
-
-=pod
-
-=head2 search
-
-  $self->search
-
-Perform actual search. Highlight (set as selected) found string.
-
-=cut
-
-sub search {
-	my $self  = shift;
-	my %args  = @_;
-	my $regex = $self->_get_search or return;
-
-	# Forwards or backwards
-	my $backwards = $self->config->find_reverse;
-	if ( $args{rev} ) {
-		$backwards = not $backwards;
-	}
-
-	# Find the range to search within
+	# As we leave the Find dialog, return the user to the current editor
+	# window so they don't need to click it.
 	my $editor = $self->current->editor;
-	$self->{text} = $editor->GetTextRange( $self->{text_offset}, $self->{text_offset_end} );
-	my ( $from, $to ) = $editor->GetSelection;
-
-	# Execute the search and move to the resulting location
-	my ( $start, $end, @matches ) = Padre::Util::get_matches(
-		$self->{text}, $regex, $from - $self->{text_offset}, $to - $self->{text_offset},
-		$backwards
-	);
-	return unless defined $start;
-	$editor->SetSelection( $start + $self->{text_offset}, $end + $self->{text_offset} );
+	if ( $editor ) {
+		$editor->SetFocus;
+	}
 
 	return;
 }
 
 =pod
 
-=head2 replace_clicked
+=head2 replace_button
 
-  $self->replace_clicked;
+  $self->replace_button;
 
 Executed when the Replace button is clicked.
 
@@ -587,37 +496,26 @@ again.
 
 =cut
 
-sub replace_clicked {
+sub replace_button {
 	my $self   = shift;
-	my $config = $self->_sync_config;
+	my $main   = $self->main;
+	my $config = $self->save;
+
+	$DB::single = 1;
+
+	# Generate the search object
+	my $search = $self->as_search;
+	unless ( $search ) {
+		$main->error("Not a valid search");
+		return;
+	}
+
+	# Apply the search to the current editor
+	$main->replace_next($search);
 
 	# If we're only searching once, we won't need the dialog any more
 	if ( $config->find_first ) {
 		$self->Hide;
-	}
-
-	# Return false if we don't have anything to search for
-	my $search = $self->{find_text}->GetValue;
-	return unless defined _STRING($search);
-
-	# Get the replace term
-	my $replace = $self->{replace_text}->GetValue;
-
-	# Save the terms
-	Padre::DB::History->create(
-		type => 'search',
-		name => $search,
-	) if $search;
-	Padre::DB::History->create(
-		type => 'replace',
-		name => $replace,
-	) if $replace;
-
-	# Execute the replace
-	if ( $self->{replace_all}->GetValue ) {
-		$self->replace_all;
-	} else {
-		$self->replace;
 	}
 
 	return;
@@ -636,159 +534,93 @@ Replace all appearances of given string in the current document.
 =cut
 
 sub replace_all {
-	my $self = shift;
+	my $self   = shift;
+	my $main   = $self->main;
+	my $config = $self->save;
 
-	# Prepare the search and replace values
-	my $regex = $self->_get_search or return;
-	my $replace = $self->_get_replace;
-	$replace =~ s/\\t/\t/g if length $replace;
-
-	# Execute the search for all matches
-	my $editor = $self->current->editor;
-	my $text = $editor->GetTextRange( $self->{text_offset}, $self->{text_offset_end} );
-	my ( undef, undef, @matches ) = Padre::Util::get_matches( $text, $regex, 0, 0 );
-
-	# Replace all matches as a single undo
-	if (@matches) {
-		$editor->BeginUndoAction;
-		foreach my $match ( reverse @matches ) {
-			$editor->SetTargetStart( $match->[0] + $self->{text_offset} );
-			$editor->SetTargetEnd( $match->[1] + $self->{text_offset} );
-			$editor->ReplaceTarget($replace);
-		}
-		$editor->EndUndoAction;
-
-		$self->main->message(
-			sprintf(
-				Wx::gettext('%s occurences were replaced'),
-				scalar @matches
-			)
-		);
-	} else {
-		$self->main->message( Wx::gettext("Nothing to replace") );
+	# Generate the search object
+	my $search = $self->as_search;
+	unless ( $search ) {
+		$main->error("Not a valid search");
+		return;
 	}
+
+	# Apply the search to the current editor
+	$main->replace_all($search);
+
+	# Close the dialog
+	$self->Hide;
 
 	return;
 }
 
-=pod
 
-  $self->replace;
 
-Perform actual single replace. Highlight (set as selected) found string.
 
-=cut
-
-sub replace {
-	my $self    = shift;
-	my $current = $self->current;
-	my $text    = $current->text;
-
-	# Prepare the search and replace values
-	my $regex = $self->_get_search or return;
-	my $replace = $self->_get_replace;
-	$replace =~ s/\\t/\t/g if length $replace;
-
-	# Get current search condition and check if they match
-	my ( $start, $end, @matches ) = Padre::Util::get_matches( $text, $regex, 0, 0 );
-
-	# If they match replace it
-	if ( defined $start and $start == 0 and $end == length($text) ) {
-		$current->editor->ReplaceSelection($replace);
-
-		# If replaced text is smaller or larger than original,
-		# change our offset end accordingly
-		if ( length($replace) != ( $end - $start ) ) {
-			$self->{text_offset_end} += ( length($replace) - ( $end - $start ) );
-		}
-
-		# Update text to search with replaced values
-		###################	$self->{text} = $current->editor->GetTextRange( $self->{text_offset}, $self->{text_offset_end} );
-	}
-
-	# If search window is still open, run a search on the whole text again
-	unless ( $current->config->find_first ) {
-		$self->search;
-	}
-
-	return;
-}
 
 #####################################################################
 # Support Methods
 
-# Save the dialog settings to configuration. Returns the config object
-# as a convenience.
-sub _sync_config {
+=pod
+
+=head2 as_search
+
+Integration with L<Padre::Search>. Generates a search instance for the
+currently configured information in the Find dialog.
+
+Returns a L<Padre::Search> object, or C<undef> if current state of the
+dialog does not result in a valid search.
+
+=cut
+
+sub as_search {
 	my $self = shift;
-
-	# Save the search settings to config
-	my $config = $self->current->config;
-	$config->set( find_case    => !$self->{find_case}->GetValue );
-	$config->set( find_regex   => $self->{find_regex}->GetValue );
-	$config->set( find_first   => $self->{find_first}->GetValue );
-	$config->set( find_reverse => $self->{find_reverse}->GetValue );
-	$config->write;
-
-	return $config;
-}
-
-# Internal method. $self->_get_search( $regex )
-# Prepare and return search term defined as a regular expression.
-sub _get_search {
-	my $self   = shift;
-	my $config = $self->config;
-	my $term   = Padre::DB::History->previous('search');
-
-	# Escape the raw search term
-	if ( $config->find_regex ) {
-
-		# Escape non-trailing $ so they won't interpolate
-		$term =~ s/\$(?!\z)/\\\$/g;
-	} else {
-
-		# Escape everything
-		$term = quotemeta $term;
-	}
-
-	# Compile the regex
-	my $regex = eval { $config->find_case ? qr/$term/m : qr/$term/mi };
-	if ($@) {
-		Wx::MessageBox(
-			sprintf( Wx::gettext("Cannot build regex for '%s'"), $term ),
-			Wx::gettext('Search error'),
-			Wx::wxOK,
-			$self->main,
-		);
-		return;
-	}
-
-	return $regex;
-}
-
-# Internal method. $self->_get_replace
-# Returns previous replacement string from history
-# or empty if _replace_choice_ widget is empty.
-# Added to be able to use empty string as a replacement text
-# but without storing in (the empty string) in history.
-sub _get_replace {
-	my $self = shift;
-	if ( $self->{replace_text} ) {
-		return $self->{replace_text}->GetValue;
-	} else {
-		return Padre::DB::History->previous('replace');
-	}
+	Padre::Search->new(
+		find_term    => $self->{find_text}->GetValue,
+		find_case    => $self->{find_case}->GetValue,
+		find_regex   => $self->{find_regex}->GetValue,
+		find_reverse => $self->{find_reverse}->GetValue,
+		replace_term => $self->{replace_text}->GetValue,
+	);
 }
 
 # Adds Ultraedit-like hotkeys for quick find/replace triggering
-sub _on_hotkey {
+sub hotkey {
 	my $self = shift;
 	my $code = shift;
 
-	$self->find_clicked    if $code == 102; # pressed 'f' hotkey
-	$self->replace_clicked if $code == 114; # pressed 'r' hotkey
+	# Handle the 'f' hotkey
+	if ( $code == 102 ) {
+		$self->find_button;
+	}
+
+	# Handle the 'r' hotkey
+	if ( $code == 114 ) {
+		$self->replace_button;
+	}
 
 	return;
+}
+
+# Save the dialog settings to configuration.
+# Returns the config object as a convenience.
+sub save {
+	my $self    = shift;
+	my $config  = $self->current->config;
+	my $changed = 0;
+
+	foreach my $name (
+		qw{ find_case find_regex find_first find_reverse }
+	) {
+		my $value = $self->{$name}->GetValue;
+		next if $config->$name() == $value;
+		$config->set( $name => $value );
+		$changed = 1;
+	}
+
+	$config->write if $changed;
+
+	return $config;
 }
 
 1;
