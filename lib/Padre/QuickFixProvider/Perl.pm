b0f0751779959d091ce7f41f2c2a942b14da0429
azawawi <ahmad.zawawi@gmail.com>
2009-08-26 03:36:42 +0000

    [Padre] Finally refactored Perl 5's quick fixes
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@7574 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- lib/Padre/QuickFixProvider/Perl.pm
+++ lib/Padre/QuickFixProvider/Perl.pm
@@ -30,79 +30,25 @@ sub quick_fix_list {
 	my ( $self, $document, $editor ) = @_;
 
 	my @items           = ();
-	my $text            = $editor->GetText;
-	my $current_line_no = $editor->GetCurrentLine;
 
-	my ( $use_strict_include, $use_warnings_include );
+	my $text            = $editor->GetText;
 	my $doc = PPI::Document->new( \$text );
 	$doc->index_locations;
-	my $includes = $doc->find('PPI::Statement::Include');
-	if ($includes) {
-		foreach my $include ( @{$includes} ) {
-			next if $include->type eq 'no';
-			if ( $include->pragma ) {
-				my $pragma   = $include->pragma;
-				if ( $pragma eq 'strict' ) {
-					$use_strict_include = $include;
-				} elsif ( $pragma eq 'warnings' ) {
-					$use_warnings_include = $include;
-				}
-			}
-		}
-	}
 
-	my ( $replace, $col, $row, $len );
-	if ( $use_strict_include and not $use_warnings_include ) {
+	my @fixes = (
+		'Padre::QuickFixProvider::Perl::StrictWarnings',
+	);
 
-		# insert 'use warnings;' afterwards
-		$replace = "use strict;\nuse warnings;";
-		$row     = $use_strict_include->line_number - 1;
-		$col     = $use_strict_include->column_number - 1;
-		$len     = length $use_strict_include->content;
-	}
-	if ( not $use_strict_include and $use_warnings_include ) {
-
-		# insert 'use strict';' before
-		$replace = "use strict;\nuse warnings;";
-		$row     = $use_warnings_include->line_number - 1;
-		$col     = $use_warnings_include->column_number - 1;
-		$len     = length $use_warnings_include->content;
-	}
-	if ( not $use_strict_include and not $use_warnings_include ) {
-
-		# insert 'use strict; use warnings;' at the top
-		my $first = $doc->find_first(
-			sub {
-				return $_[1]->isa('PPI::Statement')
-					or $_[1]->isa('PPI::Structure');
-			}
-		);
-		$replace = "use strict;\nuse warnings;\n";
-		if ($first) {
-			$row = $first->line_number - 1;
-			$col = $first->column_number - 1;
-			$len = 0;
-
+	foreach my $fix (@fixes) {
+		eval "require $fix;";
+		if($@) {
+			warn "failed to load $fix\n";
 		} else {
-			$row = $current_line_no;
-			$col = 0;
-			$len = 0;
+			push @items, $fix->new->apply($doc, $editor);
 		}
 	}
-
-	if ($replace) {
-		push @items, {
-			text     => qq{Fix '$replace'},
-			listener => sub {
-				my $line_start = $editor->PositionFromLine($row) + $col;
-				my $line_end   = $line_start + $len;
-				my $line       = $editor->GetTextRange( $line_start, $line_end );
-				$editor->SetSelection( $line_start, $line_end );
-				$editor->ReplaceSelection($replace);
-				}
-		};
-	}
-
+	
+	
 	return @items;
 }
 
