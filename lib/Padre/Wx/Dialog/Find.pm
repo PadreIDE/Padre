df1f68b6e456a73f227073b036552f6999ead2fb
Sewi <sewi@cpan.org>
2009-09-03 10:07:54 +0000

    Added a no-match-found-message
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@7705 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- lib/Padre/Wx/Dialog/Find.pm
+++ lib/Padre/Wx/Dialog/Find.pm
@@ -315,11 +315,13 @@ sub find_button {
 	}
 
 	# Apply the search to the current editor
-	$main->search_next($search);
+	my $Result = $main->search_next($search);
 
 	# If we're only searching once, we won't need the dialog any more
 	if ( $self->{find_first}->GetValue ) {
 		$self->Hide;
+	} elsif ( ! $Result) {
+		$main->message(Wx::gettext('No matches found'),Wx::gettext('Search'));
 	}
 
 	return;
