0e36ad5d86484373b38e6b5ea38a33f55c8c7d42
Sewi <sewi@cpan.org>
2009-09-25 15:40:48 +0000

    Next example for beginners
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@8336 bc93166f-e056-4eb9-8c59-0a0840647e1b

---
+++ share/examples/absolute_beginner/02_time.pl
@@ -0,0 +1,54 @@
+#!/usr/bin/perl
+
+# This file assumes that you already read (and understood) 01_hello_world.pl!
+
+use strict; # This will be discussed later
+
+# Perl accepts many things and runs even very bad written code. The following
+# line tells Perl to be more pedantic and show warnings (not errors) if there
+# is code which is supposed not to do what you want. If you're just testing
+# around and everything works, you could disable the follow line by making it
+# a comment (add a # before the line), but you should always use it for
+# productional scripts.
+use warnings;
+
+# This line does nothing more than getting the current time and even if
+# it now looks very, very complex to you, don't worry, it's not too
+# complicated to understand:
+my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime(time);
+# "my" at the beginning tells Perl that the following variables are dedicated
+# to this file. A variable is a piece of memory which holds some information
+# for you. Unlike other languages, Perl cares about size and type of
+# information itself, so you don't need to do it yourself. Every variable
+# has a name which has only few naming conventions:
+#  - The first char MUST be a dollar $ sign
+#  - It may contain letters, numbers and the underscore _
+#  - There must be at least one char after the $ sign.
+# That's it! You could use $egg or $cake_made_of_eggs_and_milk_and_other_things
+# it's really up to you what you use. 
+# It's a good idea to name variables by their usage because you need to
+# know why you used it and for which purpose even in a 3000 lines file.
+#
+# This line has many variables all shown in a comma , seperated list. Their
+# name tells you what they contain:
+#  - $sec for the seconds
+#  - $min for the minutes
+# and so on. Even if you don't understand every name, it's enough for now.
+#
+# = assigns some value to something. The value is always on the right side
+# and the variable(s) are always on the left side.
+#
+# localtime(time) gives you the current time. You'll understand the exact
+# usage later on, please treat it as a fixed text for now.
+
+# Let's print the time to the user. Variables could be used within printed
+# text like normal words:
+print "The time is $hour:$min:$sec\n";
+
+# Now press F5 and Padre will execute this script.
+#
+# You'll see a new window on the bottom of Padre which shows you the
+# output of this script. Congratulations, now you know the time.
+
+# Please go ahead and write your own print command below showing the current
+# date. You could also inspect the other variables, if you want.
