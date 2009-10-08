35fe064dbc365d667e538d683315aeaf72488426
Sewi <sewi@cpan.org>
2009-10-08 21:15:07 +0000

    Updated Run menu (now uses actions)
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@8699 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- lib/Padre/Wx/Menu/Run.pm
+++ lib/Padre/Wx/Menu/Run.pm
@@ -30,74 +30,36 @@ sub new {
 	$self->{main} = $main;
 
 	# Script Execution
-	$self->{run_document} = $self->add_menu_item(
+	$self->{run_document} = $self->add_menu_action(
 		$self,
-		name       => 'run.run_document',
-		label      => Wx::gettext('Run Script'),
-		shortcut   => 'F5',
-		menu_event => sub {
-			$_[0]->run_document;
-			$_[0]->refresh_toolbar( $_[0]->current );
-		},
+		'run.run_document',
 	);
 
-	$self->{run_document_debug} = $self->add_menu_item(
+	$self->{run_document_debug} = $self->add_menu_action(
 		$self,
-		name       => 'run.run_document_debug',
-		label      => Wx::gettext('Run Script (debug info)'),
-		shortcut   => 'Shift-F5',
-		menu_event => sub {
-			$_[0]->run_document(1); # Enable debug info
-		},
+		'run.run_document_debug',
 	);
 
-	$self->{run_command} = $self->add_menu_item(
+	$self->{run_command} = $self->add_menu_action(
 		$self,
-		name       => 'run.run_command',
-		label      => Wx::gettext('Run Command'),
-		shortcut   => 'Ctrl-F5',
-		menu_event => sub {
-			$_[0]->on_run_command;
-		},
+		'run.run_command',
 	);
 
-	$self->{run_tests} = $self->add_menu_item(
+	$self->{run_tests} = $self->add_menu_action(
 		$self,
-		name       => 'run.run_tests',
-		label      => Wx::gettext('Run Tests'),
-		menu_event => sub {
-			$_[0]->on_run_tests;
-		},
+		'run.run_tests',
 	);
 
-	$self->{run_this_test} = $self->add_menu_item(
+	$self->{run_this_test} = $self->add_menu_action(
 		$self,
-		name       => 'run.run_this_test',
-		label      => Wx::gettext('Run This Test'),
-		menu_event => sub {
-			$_[0]->on_run_this_test;
-		},
+		'run.run_this_test',
 	);
 
 	$self->AppendSeparator;
 
-	$self->{stop} = $self->add_menu_item(
+	$self->{stop} = $self->add_menu_action(
 		$self,
-		name       => 'run.stop',
-		label      => Wx::gettext('Stop execution'),
-		shortcut   => 'F6',
-		menu_event => sub {
-			if ( $_[0]->{command} ) {
-				if (Padre::Constant::WIN32) {
-					$_[0]->{command}->KillProcess;
-				} else {
-					$_[0]->{command}->TerminateProcess;
-				}
-			}
-			delete $_[0]->{command};
-			$_[0]->refresh_toolbar( $_[0]->current );
-			return;
-		},
+		'run.stop',
 	);
 
 	# Initialise enabled
