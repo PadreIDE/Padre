a204f9130f3c5ea132e8803f0a7dd179a85007a2
Sewi <sewi@cpan.org>
2009-10-25 20:22:51 +0000

    Add actions for Perl menu
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@8890 bc93166f-e056-4eb9-8c59-0a0840647e1b

---
+++ lib/Padre/Action/Perl.pm
@@ -0,0 +1,126 @@
+package Padre::Action::Perl;
+
+# Actions for Perl
+
+=pod
+
+=head1 NAME
+
+Padre::Action::Perl is a outsourced module. It creates Actions for
+developing Perl files.
+
+=cut
+
+
+use 5.008;
+use strict;
+use warnings;
+use List::Util    ();
+use File::Spec    ();
+use File::HomeDir ();
+use Params::Util qw{_INSTANCE};
+use Padre::Locale   ();
+use Padre::Current qw{_CURRENT};
+
+our $VERSION = '0.48';
+our @ISA     = 'Padre::Wx::Menu';
+
+sub new {
+	my $class = shift;
+	my $main  = shift;
+
+	# Create the empty object as normal, it won't be used usually
+	my $self = bless {}, $class;
+
+	# Add additional properties
+	$self->{main} = $main;
+
+	# Cache the configuration
+	$self->{config} = Padre->ide->config;
+
+	# Perl-Specific Searches
+	Padre::Action->new(
+		name       => 'perl.beginner_check',
+		label      => Wx::gettext('Check for common (beginner) errors'),
+		menu_event => sub {
+			my $doc = $_[0]->current->document;
+			return unless _INSTANCE( $doc, 'Padre::Document::Perl' );
+			$doc->beginner_check;
+		},
+	);
+	
+	Padre::Action->new(
+		name       => 'perl.find_brace',
+		label      => Wx::gettext('Find Unmatched Brace'),
+		menu_event => sub {
+			my $doc = $_[0]->current->document;
+			return unless _INSTANCE( $doc, 'Padre::Document::Perl' );
+			$doc->find_unmatched_brace;
+		},
+	);
+
+	Padre::Action->new(
+		name       => 'perl.find_variable',
+		label      => Wx::gettext('Find Variable Declaration'),
+		menu_event => sub {
+			my $doc = $_[0]->current->document;
+			return unless _INSTANCE( $doc, 'Padre::Document::Perl' );
+			$doc->find_variable_declaration;
+		},
+	);
+
+	Padre::Action->new(
+		name       => 'perl.vertically_align_selected',
+		label      => Wx::gettext('Vertically Align Selected'),
+		menu_event => sub {
+			my $editor = $_[0]->current->editor or return;
+			$editor->vertically_align;
+		},
+	);
+
+	Padre::Action->new(
+		name       => 'perl.newline_keep_column',
+		label      => Wx::gettext('Newline same column'),
+		shortcut   => 'Ctrl-Enter',
+		menu_event => sub {
+			my $document = $_[0]->current->document or return;
+			return unless _INSTANCE( $document, 'Padre::Document::Perl' );
+			$document->newline_keep_column;
+		},
+	);
+
+	# Move of stacktrace to Run
+	#	# Make it easier to access stack traces
+	#	$self->{run_stacktrace} = $self->AppendCheckItem( -1,
+	#		Wx::gettext("Run Scripts with Stack Trace")
+	#	);
+	#	Wx::Event::EVT_MENU( $main, $self->{run_stacktrace},
+	#		sub {
+	#			# Update the saved config setting
+	#			my $config = Padre->ide->config;
+	#			$config->set( run_stacktrace => $_[1]->IsChecked ? 1 : 0 );
+	#			$self->refresh;
+	#		}
+	#	);
+
+	Padre::Action->new(
+		menu_method => 'AppendCheckItem',
+		name       => 'perl.autocomplete_brackets',
+		label      => Wx::gettext('Automatic bracket completion'),
+		menu_event => sub {
+
+			# Update the saved config setting
+			my $config = Padre->ide->config;
+			$config->set( autocomplete_brackets => $_[1]->IsChecked ? 1 : 0 );
+		}
+	);
+
+	return $self;
+}
+
+1;
+
+# Copyright 2008-2009 The Padre development team as listed in Padre.pm.
+# LICENSE
+# This program is free software; you can redistribute it and/or
+# modify it under the same terms as Perl 5 itself.
