3ceb3e23cead319df3902e6aed2085a6aca9434c
szabgab <szabgab@gmail.com>
2009-12-05 19:22:57 +0000

    some description of how to create context help
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@9487 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- lib/Padre/HelpProvider.pm
+++ lib/Padre/HelpProvider.pm
@@ -56,6 +56,34 @@ Padre::HelpProvider - Padre Help Provider API
 The C<Padre::HelpProvider> class provides a base class, default implementation
 and API documentation for help provision support in L<Padre>.
 
+In order to setup a help system for a document type called XYZ one has to do the following:
+Create a module called Padre::HelpProvider::XYZ that subclasses the Padre::HelpProvider module
+and override 3 methods: help_init, help_list and help_render.
+
+In the class representing the Document (Padre::Document::XYX) one should override the
+get_help_provider method and return an object of the help provide module.
+In our case it should contain
+
+	require Padre::HelpProvider::XYZ;
+	return Padre::HelpProvider::XYZ->new;
+
+(TODO: Maybe it should only return the name of the module)
+
+The help_init method is called by the new method of Padre::HelpProvider once for every
+document of XYZ kind. (TODO: maybe it should be only once for every document type, and not
+once for every document of that type).
+
+help_list should return a reference to an array holding the possible strings the system can 
+provide help for.
+
+help_render is called by one of the keywords, it should return the HTML to be displayed
+as help and another string which is the location of the help. Usually a path to a file
+that will be used in the title of the window.
+
+
+=cut
+
+
 # Copyright 2008-2009 The Padre development team as listed in Padre.pm.
 # LICENSE
 # This program is free software; you can redistribute it and/or
