d6c1681f290de0ae71e177a12f6d9eee7d12f315
szabgab <szabgab@gmail.com>
2009-12-21 22:26:48 +0000

    change the comments on the Debugger actions
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@9845 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- lib/Padre/Action/Debugger.pm
+++ lib/Padre/Action/Debugger.pm
@@ -38,7 +38,7 @@ sub new {
 		need_runable => 1,
 		need_file    => 1,
 		label        => Wx::gettext('Step In') . ' (&s) ',
-		comment      => Wx::gettext('Run the current document through the Debug::Client.'),
+		comment      => Wx::gettext('Execute the next statement, enter subroutine if needed. (Start debugging if it is not yet running)'),
 
 		#shortcut     => 'Shift-F5',
 		menu_event  => sub {
@@ -52,7 +52,7 @@ sub new {
 		need_runable => 1,
 		need_file    => 1,
 		label        => Wx::gettext('Step Over') . ' (&n) ',
-		comment      => Wx::gettext('Run the current document through the Debug::Client.'),
+		comment      => Wx::gettext('Execute the next statement, If it is a subroutine call, stop only after it returned. (Start debugging if it is not yet running)'),
 
 		#shortcut     => 'Shift-F5',
 		menu_event  => sub {
@@ -67,7 +67,7 @@ sub new {
 		need_runable => 1,
 		need_file    => 1,
 		label        => Wx::gettext('Step Out') . ' (&r) ',
-		comment      => Wx::gettext('Run the current document through the Debug::Client.'),
+		comment      => Wx::gettext('If withing a subroutine, run till return is called and then stop.'),
 
 		#shortcut     => 'Shift-F5',
 		menu_event  => sub {
@@ -95,7 +95,7 @@ sub new {
 		need_runable => 1,
 		need_file    => 1,
 		label        => Wx::gettext('Jump to current execution line'),
-		comment      => Wx::gettext('Run'),
+		comment      => Wx::gettext('Set focus to the line where the current statement is in the debugging process'),
 
 		#shortcut     => 'Shift-F5',
 		menu_event  => sub {
@@ -151,7 +151,7 @@ sub new {
 		need_runable => 1,
 		need_file    => 1,
 		label        => Wx::gettext('Run to cursor'),
-		comment      => Wx::gettext('Run'),
+		comment      => Wx::gettext('Set a breakpoint at the line where to cursor is and run till there'),
 
 		#shortcut     => 'Shift-F5',
 		menu_event  => sub {
@@ -166,7 +166,7 @@ sub new {
 		need_runable => 1,
 		need_file    => 1,
 		label        => Wx::gettext('Show Stack Trace') . ' (&T) ',
-		comment      => Wx::gettext('Run the current document through the Debug::Client.'),
+		comment      => Wx::gettext('When in a subroutine call show all the calls since the main of the program'),
 
 		#shortcut     => 'Shift-F5',
 		menu_event  => sub {
@@ -180,7 +180,7 @@ sub new {
 		need_runable => 1,
 		need_file    => 1,
 		label        => Wx::gettext('Display value'),
-		comment      => Wx::gettext('Display the current value of a variable'),
+		comment      => Wx::gettext('Display the current value of a variable in the right hand side debugger pane'),
 
 		#shortcut     => 'Shift-F5',
 		menu_event  => sub {
@@ -194,7 +194,7 @@ sub new {
 		need_runable => 1,
 		need_file    => 1,
 		label        => Wx::gettext('Show Value') . ' (&x) ',
-		comment      => Wx::gettext('Run the current document through the Debug::Client.'),
+		comment      => Wx::gettext('Show the value of a variable.'),
 
 		#shortcut     => 'Shift-F5',
 		menu_event  => sub {
@@ -208,7 +208,7 @@ sub new {
 		need_runable => 1,
 		need_file    => 1,
 		label        => Wx::gettext('Evaluate Expression'),
-		comment      => Wx::gettext('Run the current document through the Debug::Client.'),
+		comment      => Wx::gettext('Type in any expression and evaluate it in the debugged process'),
 
 		#shortcut     => 'Shift-F5',
 		menu_event  => sub {
@@ -222,7 +222,7 @@ sub new {
 		need_runable => 1,
 		need_file    => 1,
 		label        => Wx::gettext('Quit Debugger') . ' (&q) ',
-		comment      => Wx::gettext('Run the current document through the Debug::Client.'),
+		comment      => Wx::gettext('Quit the process being debugged'),
 
 		#shortcut     => 'Shift-F5',
 		menu_event  => sub {
