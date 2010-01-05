2151ac6cd3581f64eda8b1cafaec92abc88b0ba7
adamk <adamk@cpan.org>
2010-01-05 06:01:15 +0000

    Added missing copyright
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@10059 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- lib/Padre/Test.pm
+++ lib/Padre/Test.pm
@@ -22,3 +22,8 @@ our $VERSION = '0.53';
 $ENV{PADRE_NOSPLASH} = 1;
 
 1;
+
+# Copyright 2008-2010 The Padre development team as listed in Padre.pm.
+# LICENSE
+# This program is free software; you can redistribute it and/or
+# modify it under the same terms as Perl 5 itself.
