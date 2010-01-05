81e6d6d512707e248478f44c8b64d1f63510734d
adamk <adamk@cpan.org>
2010-01-05 04:44:33 +0000

    Adding Padre::Test module to indicate that Padre is running inside of a test suite. You should only need to use this to suppress things like visible Padre windows during testing. It isn't needed everywhere if you don't have issues.
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@10053 bc93166f-e056-4eb9-8c59-0a0840647e1b

---
+++ lib/Padre/Test.pm
@@ -0,0 +1,21 @@
+package Padre::Test;
+
+# This package should be loaded by your test script (or by -MPadre::Test)
+# to signal that Padre is running inside of a test script, and should
+# behave appropriately.
+#
+# To avoid problems with the test environment not matching the run-time
+# environment (and as a result missing bugs because the tests don't work
+# quite the same as the real thing) uses of this module should be limited
+# to highly user-impacting changes, like keeping the Padre window invisible
+# during tests.
+
+# In Padre code, the existance of $Padre::Test::VERSION is suitable for
+# assuming we are in the test suite.
+
+use strict;
+use warnings;
+
+our $VERSION = '0.53';
+
+1;
