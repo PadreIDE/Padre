bd99f810089299d99aa8dceae447bff3de5ea1e7
adamk <adamk@cpan.org>
2010-01-25 01:07:06 +0000

    Adding a new role
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@10272 bc93166f-e056-4eb9-8c59-0a0840647e1b

---
+++ lib/Padre/Wx/Role/View.pm
@@ -0,0 +1,41 @@
+package Padre::Wx::Role::View;
+
+=pod
+
+=head1 NAME
+
+Padre::Wx::Role::View - A role for GUI tools that live in panels
+
+=head1 DESCRIPTION
+
+This is a role that should be inherited from by GUI elements that
+live in the left, right or bottom panels of Padre.
+
+=cut
+
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = '0.55';
+
+1;
+
+=pod
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2008-2010 The Padre development team as listed in Padre.pm.
+
+This program is free software; you can redistribute
+it and/or modify it under the same terms as Perl itself.
+
+The full text of the license can be found in the
+LICENSE file included with this module.
+
+=cut
+
+# Copyright 2008-2010 The Padre development team as listed in Padre.pm.
+# LICENSE
+# This program is free software; you can redistribute it and/or
+# modify it under the same terms as Perl 5 itself.
