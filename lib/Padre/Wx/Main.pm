4dec0168ecf04408465e9d034808e639256c54ec
adamk <adamk@cpan.org>
2010-01-28 03:13:19 +0000

    Another small fix to use ORLite 1.38's ->set method instead of the raw array position
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@10325 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- lib/Padre/Wx/Main.pm
+++ lib/Padre/Wx/Main.pm
@@ -2510,7 +2510,7 @@ sub save_session {
 
 	my $transaction = $self->lock('DB');
 	foreach my $file (@session) {
-		$file->[4] = $session->id;
+		$file->set( session => $session->id );
 		$file->insert;
 	}
 
