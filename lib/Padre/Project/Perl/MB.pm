58cfff2b61bb786847ef2d8f1bf3b72c6a959fa4
waxhead <peter.lavender@gmail.com>
2010-01-31 03:18:19 +0000

    Added in Copyright and set EOL to UNIX.
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@10391 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- lib/Padre/Project/Perl/MB.pm
+++ lib/Padre/Project/Perl/MB.pm
@@ -17,3 +17,10 @@ use Class::XSAccessor {
 };
 
 1;
+
+__END__
+
+# Copyright 2008-2010 The Padre development team as listed in Padre.pm.
+# LICENSE
+# This program is free software; you can redistribute it and/or
+# modify it under the same terms as Perl 5 itself.
--- lib/Padre/Project/Perl/MI.pm
+++ lib/Padre/Project/Perl/MI.pm
@@ -11,3 +11,12 @@ our $VERSION = '0.55';
 our @ISA     = 'Padre::Project::Perl::EUMM';
 
 1;
+
+
+__END__
+
+# Copyright 2008-2010 The Padre development team as listed in Padre.pm.
+# LICENSE
+# This program is free software; you can redistribute it and/or
+# modify it under the same terms as Perl 5 itself.
+
