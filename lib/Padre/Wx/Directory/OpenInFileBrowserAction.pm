7f8fa4efd2532a256c70c686ba9d43e0dfd40315
azawawi <ahmad.zawawi@gmail.com>
2010-02-07 15:47:18 +0000

    No need to launch a command line to execute explorer.exe on win32
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@10596 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- lib/Padre/Wx/Directory/OpenInFileBrowserAction.pm
+++ lib/Padre/Wx/Directory/OpenInFileBrowserAction.pm
@@ -68,7 +68,7 @@ sub open_in_file_browser {
 
 		# In windows, simply execute: explorer.exe /select,"$filename"
 		$filename =~ s/\//\\/g;
-		$error = $self->_execute( 'cmd', '/c', 'explorer.exe', "/select,\"$filename\"" );
+		$error = $self->_execute( 'explorer.exe', "/select,\"$filename\"" );
 	} elsif (Padre::Constant::UNIX) {
 		my $parent_folder = File::Basename::dirname($filename);
 		if ( defined $ENV{KDE_FULL_SESSION} ) {
