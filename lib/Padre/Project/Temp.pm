3cfe753ceede3ff19e80a03f9060b1d6e91871e7
azawawi <ahmad.zawawi@gmail.com>
2010-02-08 09:01:36 +0000

    Added missing copyright notice to Padre::Project::Temp
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@10614 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- lib/Padre/Project/Temp.pm
+++ lib/Padre/Project/Temp.pm
@@ -33,3 +33,22 @@ sub new {
 }
 
 1;
+
+=pod
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2008-2010 The Padre development team as listed in Padre.pm.
+
+This program is free software; you can redistribute
+it and/or modify it under the same terms as Perl itself.
+
+The full text of the license can be found in the
+LICENSE file included with this module.
+
+=cut
+
+# Copyright 2008-2010 The Padre development team as listed in Padre.pm.
+# LICENSE
+# This program is free software; you can redistribute it and/or
+# modify it under the same terms as Perl 5 itself.
