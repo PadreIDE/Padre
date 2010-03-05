a5079c7d77a0786f0195dc419b28ee3674d9d64f
Patrick Donelan <pat@patspam.com>
2010-03-05 19:24:03 +0000

    Fixes "Open File In Existing Padre" for non-win32
    Moved the require line inside the conditional
    
    
    
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@10983 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- lib/Padre/Startup.pm
+++ lib/Padre/Startup.pm
@@ -89,8 +89,8 @@ sub startup {
 
 				# Got the single instance PID
 				$pid =~ s/\s+\s//;
-				require Win32::API;
 				if (Padre::Constant::WIN32) {
+					require Win32::API;
 					Win32::API->new(
 						user32 => AllowSetForeground,
 					)->Call($pid);
