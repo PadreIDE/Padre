e46ac4885d28536a7012406ff677e9d286ede463
reneeb <module@renee-baecker.de>
2010-03-07 18:30:40 +0000

    if you have opened a session and you want to save the session, the current session name should be shown in the drop down.
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@10987 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- lib/Padre/Wx/Dialog/SessionSave.pm
+++ lib/Padre/Wx/Dialog/SessionSave.pm
@@ -269,8 +269,10 @@ sub _refresh_combo {
 
 	# clear list & fill it again
 	my $combo = $self->_combo;
+	my $preselection = $combo->GetValue;
 	$combo->Clear;
 	$combo->Append($_) foreach @names;
+	$combo->SetStringSelection( $preselection );
 }
 
 1;
