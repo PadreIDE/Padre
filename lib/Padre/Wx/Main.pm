ac78cfc9609ddd7cfb03f989822270458276ed3b
waxhead <peter.lavender@gmail.com>
2010-03-13 09:05:46 +0000

    Rename the new method to a more correct named.  Added comment to explain why it was added.
    I'm not sure it belongs here.
    
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@11091 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- lib/Padre/Wx/Main.pm
+++ lib/Padre/Wx/Main.pm
@@ -1383,11 +1383,14 @@ sub refresh_recent {
 
 =head3 C<refresh_view_document_as>
 
-Specifically refresh the View/View Document As list
+Specifically refresh the View menu.
 
 =cut
 
-sub refresh_view_document_as {
+# This was added to make sure the view
+# menu updates the View Document As 
+# each time a document changes.
+sub refresh_view_menu {
 	my $self = shift;
 	return if $self->locked('REFRESH');
 	$self->menu->view->refresh;
