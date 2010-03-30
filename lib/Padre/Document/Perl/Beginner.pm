de28f58bd45c8d4202e861fca17ff09c79f8d9ed
szabgab <szabgab@gmail.com>
2010-03-30 19:48:27 +0000

    eliminate false positives when there is   # if  $x = $y   commented out in the code
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@11247 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- lib/Padre/Document/Perl/Beginner.pm
+++ lib/Padre/Document/Perl/Beginner.pm
@@ -227,7 +227,7 @@ to actually change the array via s///.
 
 =cut
 
-	if ( $config->begerror_ifsetvar and $text =~ /^([\x00-\xff]*?)if\b[\s\t\r\n]*(\(?[\$\s\t\r\n\w]+\=[\s\t\r\n\$\w])/ ) {
+	if ( $config->begerror_ifsetvar and $text =~ m/\A([\x00-\xff]*?)  ^[^#]* if\b  \s*  (  \(?  [\$\s\w]+ = [\s\$\w] )/xsm ) {
 		$self->_report("A single = in a if-condition is usually a typo, use == or eq to compare.", $1);
 		return;
 	}
