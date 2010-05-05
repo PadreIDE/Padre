a15b0472ec7369b2e207bae641bd2020626abb54
Zeno Gantner <zeno.gantner@gmail.com>
2010-05-05 17:27:55 +0000

    fixed #878 Tabs like 'Outline', 'Functions'... should have a close button, just like the documents tabs
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@11419 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- lib/Padre/Wx/Right.pm
+++ lib/Padre/Wx/Right.pm
@@ -26,7 +26,7 @@ sub new {
 		-1,
 		Wx::wxDefaultPosition,
 		Wx::Size->new( 160, 500 ), # Used when floating
-		Wx::wxAUI_NB_SCROLL_BUTTONS | Wx::wxAUI_NB_TOP | Wx::wxBORDER_NONE
+		Wx::wxAUI_NB_SCROLL_BUTTONS | Wx::wxAUI_NB_TOP | Wx::wxBORDER_NONE | Wx::wxAUI_NB_CLOSE_ON_ACTIVE_TAB
 	);
 
 	# Add ourself to the window manager
