2d0848ad01bf3952351f77eff0dfeffa53ec3967
Zeno Gantner <zeno.gantner@gmail.com>
2010-05-11 08:17:09 +0000

    updated German translation (one string), updated info on Brazilian and Italian translations in Changes
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@11440 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- lib/Padre/PluginManager.pm
+++ lib/Padre/PluginManager.pm
@@ -868,7 +868,7 @@ sub get_menu {
 	}
 	my ( $label, $menu ) = eval { $plugin->{object}->menu_plugins($main) };
 	if ($@) {
-		$plugin->errstr( Wx::gettext("Error when calling menu for plug-in") . "'$module': $@" );
+		$plugin->errstr( Wx::gettext('Error when calling menu for plug-in ') . "'$module': $@" );
 		$plugin->{status} = 'error';
 
 		# TO DO: make sure these error messages show up somewhere or it will drive
