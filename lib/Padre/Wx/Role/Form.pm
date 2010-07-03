6e7c9a58cd55f09692a1ca85df58f9d2f87f17a0
adamk <adamk@cpan.org>
2010-07-03 15:15:49 +0000

    Added Padre::Wx::Role::Form role, which encapsulates the data extraction part of Padre::Wx::Dialog. This is the part that we'll still need once we let FormBuilder generate the dialog code inside of the using the old first-generation rule-based layout
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@11764 bc93166f-e056-4eb9-8c59-0a0840647e1b

---
+++ lib/Padre/Wx/Role/Form.pm
@@ -0,0 +1,45 @@
+package Padre::Wx::Role::Form;
+
+use 5.008005;
+use strict;
+use warnings;
+use Padre::Wx ();
+
+our $VERSION = '0.66';
+
+sub form {
+	my $self = shift;
+
+	# The list of fields to read
+	my @names = @_;
+	unless ( @names ) {
+		@names = grep {
+			$_->isa('Wx::Control')
+			and not
+			$_->isa('Wx::Button')
+		} sort keys %$self;
+	}
+
+	# Read the values from the named controls
+	my %hash = ();
+	foreach my $name ( @names ) {
+		my $control = $self->{$name};
+		if ( $control->can('GetValue') ) {
+			$hash{$name} = $control->GetValue;
+		} elsif ( $control->can('GetPath') ) {
+			$hash{$name} = $control->GetPath;
+		} elsif ( $control->isa('Wx::Choice') ) {
+			$hash{$name} = $control->GetSelection;
+		} elsif ( $control->isa('Wx::ColourPickerControl') ) {
+			$hash{$name} = $control->GetColour->GetAsString( Wx::wxC2S_HTML_SYNTAX );
+		} elsif ( $control->isa('Wx::FontPickerControl') ) {
+			$hash{$name} = $control->GetSelectedFont->GetNativeFontInfoUserDesc;
+		} else {
+			die "Unknown or unsupported control class " . ref($control);
+		}
+	}
+
+	return \%hash;
+}
+
+1;
