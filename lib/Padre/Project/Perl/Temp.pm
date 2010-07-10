c7762117c18665498932142551f17ad59533a449
adamk <adamk@cpan.org>
2010-07-10 14:12:47 +0000

    Starting the work needed to allow executing scripts and compile-testing for syntax checking without saving files, to allow compile testing against MULTIPLE unsaved files, and various other neato tricks nobody else can do properly.
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@11848 bc93166f-e056-4eb9-8c59-0a0840647e1b

---
+++ lib/Padre/Project/Perl/Temp.pm
@@ -0,0 +1,69 @@
+package Padre::Project::Perl::Temp;
+
+use 5.008005;
+use strict;
+use warnings;
+use File::Path       ();
+use File::Spec       ();
+use File::Spec::Unix ();
+use File::Temp       ();
+
+our $VERSION = '0.66';
+
+sub new {
+	my $class = shift;
+	my $self  = bless { @_ }, $class;
+	if ( ref $self->{project} ) {
+		$self->{project} = $self->{project}->root;
+	}
+	if ( defined $self->{project} ) {
+		$self->{project} = File::Spec->rel2abs( $self->{project} );
+	}
+	unless ( $self->{files} ) {
+		$self->{files} = { };
+	}
+	return $self;
+}
+
+sub run {
+	my $self  = shift;
+	my $files = $self->{files};
+
+	# Write the unsaved files
+	foreach my $unix ( sort keys %$files ) {
+		# Determine where to write the file to
+		my ($v, $d, $f) = File::Spec::Unix->splitpath($unix);
+		my @p           = File::Spec::Unix->splitdir($d);
+		my $dir         = File::Spec->catdir( $self->temp, @p );
+		my $file        = File::Spec->catfile( $dir, $f );
+
+		# Create the directory the file will be written to
+		unless ( -d $dir ) {
+			File::Path::mkpath( $dir, { verbose => 0 } );
+		}
+
+		# Write the file content
+		open( my $fh, '>', $file ) or die "open($file): $!";
+		binmode( $fh, ':utf8' );
+		$fh->print( $files->{$unix} );
+		close( $fh ) or die "close($file): $!";
+	}
+
+	return 1;
+}
+
+sub temp {
+	$_[0]->{temp} or
+	$_[0]->{temp} = File::Temp::tempdir( CLEANUP => 1 );
+}
+
+sub include {
+	my $self    = shift;
+	my @include = File::Spec->catdir( $self->{temp}, 'lib' );
+	if ( $self->{project} ) {
+		push @include, File::Spec->catdir( $self->{project}, 'lib' );
+	}
+	return @include;
+}
+
+1;
---
+++ t/42-perl-project-temp.t
@@ -0,0 +1,63 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use Test::More tests => 11;
+use Padre::Project::Perl       ();
+use Padre::Project::Perl::Temp ();
+
+
+
+
+
+######################################################################
+# Simple Cases
+
+SCOPE: {
+	my $string = <<'END_PERL';
+package Foo;
+
+use strict;
+
+sub dummy { 2 }
+
+1;
+END_PERL
+
+	my $null   = new_ok( 'Padre::Project::Perl::Temp' );
+	my $simple = new_ok( 'Padre::Project::Perl::Temp' => [
+		files => {
+			'lib/Foo.pm' => $string,
+		},
+	] );
+	ok( $simple->run, '->run ok' );
+	ok( $simple->temp, '->{temp} ok' );
+	ok( -d $simple->temp, "->{temp} exists at $simple->{temp}" );
+	ok(
+		-f File::Spec->catfile(
+			$simple->temp, 'lib', 'Foo.pm',
+		),
+		'Created package Foo ok',
+	);
+}
+
+
+
+
+
+######################################################################
+# Project-Based Case
+
+SCOPE: {
+	my $root    = File::Spec->catdir( qw{ t collection Config-Tiny} );
+	my $project = new_ok( 'Padre::Project::Perl' => [
+		root => $root,
+	] );
+	my $temp = new_ok( 'Padre::Project::Perl::Temp' => [
+		project => $project,
+		files   => { },
+	] );
+	ok( -d $temp->temp, '->temp exists' );
+	ok( ! ref $temp->{project}, '->{project} is flattened' );
+	ok( -d $temp->{project}, '->{project} directory exists' );
+}
