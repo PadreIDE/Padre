240e8e9e7ba8065a9988815b04d558a83684f88c
waxhead <peter.lavender@gmail.com>
2010-07-23 00:53:33 +0000

    Slight change to the way the locale object is instantiated, removing the warning to the console about not being able to set the language.
    This might not be correct but hasn't caused problems with the testing so far.
    
    If this is OK, search for the TODO's and remove any code that doesn't need to be there.
    
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@11923 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- lib/Padre/Locale.pm
+++ lib/Padre/Locale.pm
@@ -521,12 +521,24 @@ sub object {
 	undef $langcode if ref($langcode);
 	my $id     = rfc4646($langcode);
 	my $lang   = $RFC4646{$id}->{wxid};
-	my $locale = Wx::Locale->new($lang);
+	
+	# TODO Remove this if this works and is accepted.
+	#my $locale = Wx::Locale->new($lang);
+	
+	# this prevents the warning; typically:
+	# 10:23:15: Error: Cannot set locale to 'de_DE'.
+	# from Wx that it can't load a language.
+	my $locale = Wx::Locale->new(undef);
 	$locale->AddCatalogLookupPathPrefix( Padre::Util::sharedir('locale') );
-	unless ( $locale->IsLoaded($id) ) {
-		my $file = Padre::Util::sharefile( 'locale', $id ) . '.mo';
-		$locale->AddCatalog($id) if -f $file;
-	}
+	my $file = Padre::Util::sharefile( 'locale', $id ) . '.mo';
+	$locale->AddCatalog($id) if -f $file;
+	
+	# TODO Remove this too.
+	# unless ( $locale->IsLoaded($id) ) {
+		# my $file = Padre::Util::sharefile( 'locale', $id ) . '.mo';
+		# $locale->AddCatalog($id) if -f $file;
+	# }
+	
 	return $locale;
 }
 
