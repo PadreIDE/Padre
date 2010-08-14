db5e11fe913ca44d4d182695853543673972721b
chorny <alexchorny@gmail.com>
2010-08-14 22:13:45 +0000

    force English locale for tests
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@12232 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- t/lib/Padre/Win32.pm
+++ t/lib/Padre/Win32.pm
@@ -16,7 +16,7 @@ sub setup {
 
 	# Find Perl (ideally the gui one)
 	my $perl = Padre::Perl::wxperl();
-	my $cmd  = "start $perl script\\padre";
+	my $cmd  = "start $perl script\\padre --locale=en";
 	#$t->diag($cmd);
 	system $cmd;
 	my $padre;
