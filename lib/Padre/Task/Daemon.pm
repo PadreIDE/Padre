e35cf0ee3b41175afdd28e1815259096f55254f5
adamk <adamk@cpan.org>
2010-08-16 14:54:06 +0000

    Adding a base class (maybe a role later) to hold the task-level logic needed to implement the parent to child half of the bidirectional communication needed for thread daemons.
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@12239 bc93166f-e056-4eb9-8c59-0a0840647e1b

---
+++ lib/Padre/Task/Daemon.pm
@@ -0,0 +1,37 @@
+package Padre::Task::Daemon;
+
+use 5.008005;
+use strict;
+use warnings;
+use Params::Util ();
+use Padre::Logger;
+
+our $VERSION = '0.68';
+our @ISA     = 'Padre::Task';
+
+sub dequeue {
+	TRACE( $_[0] ) if DEBUG;
+	my $self   = shift;
+	my $handle = $self->handle  or return 0;
+	my $queue  = $handle->queue or return 0;
+
+	# Check the message for valid structure
+	my $message = $queue->dequeue_nb or return 0;
+	unless ( Params::Util::_ARRAY($message) ) {
+		TRACE('Non-ARRAY message received by a worker thread') if DEBUG;
+		return 0;
+	}
+	unless ( _IDENTIFIER($message->[0]) ) {
+		TRACE('Non-method message received by worker thread') if DEBUG;
+		return 0;
+	}
+
+	return $message;
+}
+
+1;
+
+# Copyright 2008-2010 The Padre development team as listed in Padre.pm.
+# LICENSE
+# This program is free software; you can redistribute it and/or
+# modify it under the same terms as Perl 5 itself.
