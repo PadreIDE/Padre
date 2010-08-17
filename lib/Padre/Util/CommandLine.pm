b510e6dc33bbff214eeca5b4c9465ee1aceb09e8
szabgab <szabgab@gmail.com>
2010-08-17 06:22:33 +0000

    start writing separate Padre::Util::CommandLine to handle TAB presses
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@12261 bc93166f-e056-4eb9-8c59-0a0840647e1b

---
+++ lib/Padre/Util/CommandLine.pm
@@ -0,0 +1,45 @@
+package Padre::Util::CommandLine;
+
+# Class to handle command line events
+# Currently Part of Padre but it shoule be able to stand on its own
+# and maybe moved to a separate package
+
+use 5.008;
+use strict;
+use warnings;
+use utf8;
+
+our $VERSION = '0.68';
+
+my $current_dir;
+my @current_list;
+my $current_index;
+my $last_suggest;
+
+sub tab {
+	my ($text) = @_;
+	if ($text =~ m/^:e\s*$/) {
+		require Cwd;
+		my $cwd = Cwd::cwd();
+		opendir my $dh, $cwd or die "Could not open $cwd $!";
+		@current_list = map {-d "$cwd/$_" ? "$_/" : $_} grep {$_ ne '.' and $_ ne '..'} sort readdir $dh;
+		if (not @current_list) {
+			# TODO how to handle empty dir?
+		}
+		$current_index = 0;
+		$last_suggest = ":e $current_list[$current_index]";
+		return $last_suggest;
+		
+	} elsif ($text eq $last_suggest) {
+		$current_index++;
+		$last_suggest = ":e $current_list[$current_index]";
+		return $last_suggest;
+	}
+}
+
+1;
+
+# Copyright 2008-2010 The Padre development team as listed in Padre.pm.
+# LICENSE
+# This program is free software; you can redistribute it and/or
+# modify it under the same terms as Perl 5 itself.
---
+++ t/85-commandline.t
@@ -0,0 +1,24 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use Test::More tests => 2+1;
+use FindBin qw($Bin);
+use File::Spec ();
+use File::Temp ();
+use Data::Dumper qw(Dumper);
+use Test::NoWarnings;
+
+# Testing the stand-alone Padre::Utile::CommandLine
+
+use Padre::Util::CommandLine;
+use Cwd qw(abs_path);
+my $files_dir = File::Spec->catdir(abs_path(File::Basename::dirname($0)), 'files');
+{
+	no warnings;
+	sub Cwd::cwd {	$files_dir }
+}
+
+
+is Padre::Util::CommandLine::tab(':e'), ':e Debugger.pm', 'TAB 1';
+is Padre::Util::CommandLine::tab(':e Debugger.pm'), ':e beginner/', 'TAB 2';
