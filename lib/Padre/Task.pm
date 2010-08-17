6bfbf29bd72cf5763d6412da696d745b36ec2466
adamk <adamk@cpan.org>
2010-08-17 03:58:03 +0000

    Partial documentation for the task class
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@12247 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- lib/Padre/Task.pm
+++ lib/Padre/Task.pm
@@ -1,5 +1,91 @@
 package Padre::Task;
 
+=pod
+
+=head1 NAME
+
+Padre::Task - Padre Task API 2.0
+
+=head1 DESCRIPTION
+
+The Padre Task API implements support for background and parallel
+execution of code in the L<Padre> IDE, and is based on the CPAN
+L<Process> API.
+
+A B<Task Class> is a class that completely encapsulates a single unit of
+work, describing not only the work to be done, but also how the unit of
+work is created, how is serialised for transport, and any initialisation
+or cleanup work needs to be done.
+
+A B<Task> is a single self-contained unit of work, and is implemented as
+a single instance of a particular Task Class.
+
+=head2 The lifecycle of a Task object
+
+From the perspective of a task author, the execution of a task will occur
+in four distinct phases.
+
+=head3 Construction
+
+The creation of a task is always done completely independantly of its
+execution. Typically this is done via the C<new> method, or something
+that calls it.
+
+This separate construction step allows validation of parameters in
+advance, as well as allowing bulk task pre-generation and advanced task
+management functionality such as prioritisation, queueing, throttling and
+load-balancing of tasks.
+
+=head3 Preparation
+
+Once a task has been constructed, an arbitrarily long time may pass before
+the code is actually run (if it is ever run at all).
+
+If the actual execution of the task will result in certain work being done
+in the parent thread, this work cannot be done in the constructor. And once
+created as an object, no futher task code will be called until the task is
+ready for execution.
+
+To give the author a chance to allow for any problems that may occur as a
+result of this delay, the Task API provides a preparation phase for the
+task via the C<prepare> method.
+
+This preparation code is run in the parent thread once the task has been
+prioritised, has a worker allocated to it, and has been encapsulated in its
+L<Padre::TaskHandle>, but before the object is serialised for transport
+into the thread.
+
+A task can use this preparation phase to detach from non-serialisable
+resources in the object such as database handles, to copy any interesting
+parent state late rather than early, or decide on a last-second self-abort.
+
+Once the preparation phase is completed the task will be serialised,
+transported into assigned worker thread and then executed B<immediately>.
+
+Because it will execute in the parent thead, the rest of the Padre instance
+is available for use if needed, but the preparation code should run quickly
+and must not block.
+
+=head3 Execution
+
+The main phase of the task is where the CPU-intensive or blocking code can
+be safely run. It is run inside a worker thread in the background, without
+impacting on the performance of the parent thread.
+
+However, the task execution phase must be entirely self-contained.
+
+The worker threads not only do not have access to the Padre IDE variable
+structure, but most Padre classes (including heavily used modules such as
+L<Padre::Current>) will not be loaded at all in the worker thread.
+
+Any output that needs to be transported back to the parent should be stored
+in the object somewhere. When the cleanup phase is run, these values will
+be available automatically in the parent.
+
+TO BE COMPLETED
+
+=cut
+
 use 5.008005;
 use strict;
 use warnings;
@@ -47,6 +133,31 @@ sub owner {
 
 sub callback {
 	$_[0]->{callback} || 'task_response';
+}
+
+
+
+
+
+######################################################################
+# Serialization - Based on Process::Serializable and Process::Storable
+
+# my $string = $task->as_string;
+sub as_string {
+	Storable::nfreeze( $_[0] );
+}
+
+# my $task = Class::Name->from_string($string);
+sub from_string {
+	my $class = shift;
+	my $self  = Storable::thaw( $_[0] );
+	unless ( Scalar::Util::blessed($self) eq $class ) {
+
+		# Because this is an internal API we can be brutally
+		# unforgiving is we aren't use the right way.
+		die("Task unexpectedly did not deserialize as a $class");
+	}
+	return $self;
 }
 
 
@@ -105,34 +216,22 @@ sub finish {
 	return 1;
 }
 
-
-
-
-
-######################################################################
-# Serialization - Based on Process::Serializable and Process::Storable
-
-# my $string = $task->as_string;
-sub as_string {
-	Storable::nfreeze( $_[0] );
-}
-
-# my $task = Class::Name->from_string($string);
-sub from_string {
-	my $class = shift;
-	my $self  = Storable::thaw( $_[0] );
-	unless ( Scalar::Util::blessed($self) eq $class ) {
-
-		# Because this is an internal API we can be brutally
-		# unforgiving is we aren't use the right way.
-		die("Task unexpectedly did not deserialize as a $class");
-	}
-	return $self;
-}
-
 1;
 
-# Copyright 2008-2010 The Padre development team as listed in Padre.pm.
-# LICENSE
-# This program is free software; you can redistribute it and/or
-# modify it under the same terms as Perl 5 itself.
+=pod
+
+=head1 SEE ALSO
+
+L<Padre>, L<Process>
+
+=head1 COPYRIGHT
+
+Copyright 2008-2010 The Padre development team as listed in Padre.pm.
+
+This program is free software; you can redistribute
+it and/or modify it under the same terms as Perl itself.
+
+The full text of the license can be found in the
+LICENSE file included with this module.
+
+=cut
