655167927ec4500ffe78c5448e12b0bfb63f12b8
adamk <adamk@cpan.org>
2010-08-18 09:07:10 +0000

    Dev, test and prod instances of Padre use different single instance ports, so they don't accidentally call the wrong Padre.
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@12282 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- lib/Padre/Constant.pm
+++ lib/Padre/Constant.pm
@@ -141,8 +141,19 @@ BEGIN {
 # system-installed Padre while running the test suite.
 # NOTE: The only reason this is here is that it is needed both during
 # main configuration, and also during Padre::Startup.
-use constant DEFAULT_SINGLEINSTANCE => ( WIN32 and not( $ENV{HARNESS_ACTIVE} or $^P ) ) ? 1 : 0;
-use constant DEFAULT_SINGLEINSTANCE_PORT => 4444;
+use constant DEFAULT_SINGLEINSTANCE => (
+	WIN32 and not( $ENV{HARNESS_ACTIVE} or $^P )
+) ? 1 : 0;
+
+# It would be better if we had fully dynamic collision awareness support,
+# so that Padre could automatically port hop.
+# In the mean time, just make sure that dev.pl, test, and production versions
+# of Padre use different ports, so they don't collide with each other.
+use constant DEFAULT_SINGLEINSTANCE_PORT => (
+	$ENV{PADRE_DEV}      ? 4446 :
+	$ENV{HARNESS_ACTIVE} ? 4445 :
+	4444
+);
 
 1;
 
