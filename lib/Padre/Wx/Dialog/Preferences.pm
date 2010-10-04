3904b3008cb3a1c0df6d83c3137628f3e55ff00d
szabgab <szabgab@gmail.com>
2010-10-04 20:27:23 +0000

    display preference option autocomplete_brackets in the main preference window
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@12594 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- lib/Padre/Wx/Dialog/Preferences.pm
+++ lib/Padre/Wx/Dialog/Preferences.pm
@@ -355,10 +355,18 @@ sub _behaviour_panel {
 
 		# Will be moved to a own AutoComp-panel as soon as there are enough options for this (and I get the spare time to do it):
 		[   [   'Wx::CheckBox',
+				'autocomplete_brackets',
+				( $config->autocomplete_brackets ? 1 : 0 ),
+				Wx::gettext(
+					"Autocomplete brackets")
+			],
+			[]
+		],
+		[   [   'Wx::CheckBox',
 				'autocomplete_multiclosebracket',
 				( $config->autocomplete_multiclosebracket ? 1 : 0 ),
 				Wx::gettext(
-					"Add another closing bracket if there is already one (and the auto-bracket-function is enabled)")
+					"Add another closing bracket if there is already one (and the 'Autocomplete brackets' above is enabled)")
 			],
 			[]
 		],
@@ -1184,6 +1192,10 @@ sub run {
 	$config->set(
 		'update_file_from_disk_interval',
 		$data->{update_file_from_disk_interval}
+	);
+	$config->set(
+		'autocomplete_brackets',
+		$data->{autocomplete_brackets} ? 1 : 0
 	);
 	$config->set(
 		'autocomplete_multiclosebracket',
