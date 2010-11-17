35edfb2fc18a3fb999160a5bf983d528827e0ae8
azawawi <ahmad.zawawi@gmail.com>
2010-11-17 07:36:18 +0000

    The first wizard library and wizard object... Nothing is really functional at the moment but it is a start :)
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@13119 bc93166f-e056-4eb9-8c59-0a0840647e1b

---
+++ lib/Padre/Wx/Wizard.pm
@@ -0,0 +1,104 @@
+package Padre::Wx::Wizard;
+
+use 5.008;
+use strict;
+use warnings;
+use Padre::Config   ();
+use Padre::Constant ();
+use Padre::Wx       ();
+
+our $VERSION = '0.75';
+
+# Generate faster accessors
+use Class::XSAccessor {
+	getters => {
+		id            => 'id',
+		name          => 'name',
+		label         => 'label',
+		category      => 'category',
+		comment       => 'comment',
+	},
+	accessors => {
+		shortcut => 'shortcut',
+	},
+};
+
+
+
+
+
+#####################################################################
+# Functions
+
+# This sub calls all the other files which actually create the actions
+sub create {
+	my $main = shift;
+}
+
+
+
+#####################################################################
+# Constructor
+
+sub new {
+	my $class    = shift;
+	my $ide      = Padre->ide;
+	my $wizards  = $ide->wizards;
+	my $self     = bless { id => -1, @_ }, $class;
+
+	# Save the action
+	$actions->{$name} = $self;
+
+	return $self;
+}
+
+# Translate on the fly when requested
+sub label {
+	Wx::gettext( $_[0]->{label} );
+}
+
+1;
+
+# Copyright 2008-2010 The Padre development team as listed in Padre.pm.
+# LICENSE
+# This program is free software; you can redistribute it and/or
+# modify it under the same terms as Perl 5 itself.
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Padre::Wx::Wizard - Padre Action Object
+
+=head1 SYNOPSIS
+
+  my $wizard = Padre::Wx::Wizard->new(
+	name        => 'perl5.script',
+	label       => Wx::gettext('Script'),
+	category    => Wx::gettext('Perl 5'),
+	comment     => Wx::gettext('Opens the Perl 5 script wizard'),
+  );
+
+=head1 DESCRIPTION
+
+This is the base class for the Padre Wizard API.
+
+=head1 METHODS
+
+=head2 new
+
+A default constructor for wizard objects.
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2008-2010 The Padre development team as listed in Padre.pm.
+
+This program is free software; you can redistribute
+it and/or modify it under the same terms as Perl itself.
+
+The full text of the license can be found in the
+LICENSE file included with this module.
+
+=cut
---
+++ lib/Padre/Wx/WizardLibrary.pm
@@ -0,0 +1,33 @@
+package Padre::Wx::WizardLibrary;
+
+# Defines all the core wizards for Padre.
+
+use 5.008005;
+use strict;
+use warnings;
+use Padre::Wx            ();
+use Padre::Wx::Wizard    ();
+
+our $VERSION = '0.75';
+
+######################################################################
+# Wizard Database
+
+sub init_wizards {
+
+	Padre::Wx::Wizard->new(
+		name        => 'perl5.script',
+		label       => Wx::gettext('Script'),
+		category    => Wx::gettext('Perl 5'),
+		comment     => Wx::gettext('Opens the Perl 5 script wizard'),
+	);
+
+	return 1;
+}
+
+1;
+
+# Copyright 2008-2010 The Padre development team as listed in Padre.pm.
+# LICENSE
+# This program is free software; you can redistribute it and/or
+# modify it under the same terms as Perl 5 itself.
