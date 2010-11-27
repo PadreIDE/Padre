ed0085c9cd6ea1879dd65c78efb5405d6051ad00
waxhead <peter.lavender@gmail.com>
2010-11-27 02:56:24 +0000

    Add the requisite copyright text at the bottom of the file so xt/copytight.t passes.
    Yes, even though it's in the POD right above it.. :)
    
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@13257 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- lib/Padre/Wx/Dialog/WindowList.pm
+++ lib/Padre/Wx/Dialog/WindowList.pm
@@ -478,3 +478,9 @@ under the same terms as Perl 5 itself.
 
 
 =cut
+
+# Copyright 2008-2010 The Padre development team as listed in Padre.pm.
+# LICENSE
+# This program is free software; you can redistribute it and/or
+# modify it under the same terms as Perl 5 itself.
+
