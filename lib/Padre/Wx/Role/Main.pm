98c5f00b292c3c5a32702519ea277c8f17d5c27b
Zeno Gantner <zeno.gantner@gmail.com>
2010-11-30 15:30:20 +0000

    fix (c) statements
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@13288 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- lib/Padre/Wx/Role/Main.pm
+++ lib/Padre/Wx/Role/Main.pm
@@ -111,14 +111,9 @@ sub current {
 Copyright 2008-2010 The Padre development team as listed in Padre.pm.
 
 This program is free software; you can redistribute
-it and/or modify it under the same terms as Perl itself.
+it and/or modify it under the same terms as Perl 5 itself.
 
 The full text of the license can be found in the
 LICENSE file included with this module.
 
 =cut
-
-# Copyright 2008-2010 The Padre development team as listed in Padre.pm.
-# LICENSE
-# This program is free software; you can redistribute it and/or
-# modify it under the same terms as Perl 5 itself.
