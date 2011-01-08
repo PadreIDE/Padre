cbb631d272adc841e5ffa10f8a8121557e990af0
szabgab <szabgab@gmail.com>
2011-01-08 19:19:45 +0000

    add example with simple read-only editor
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@13446 bc93166f-e056-4eb9-8c59-0a0840647e1b

---
+++ share/examples/wx/24_simple_editor_window.pl
@@ -0,0 +1,87 @@
+#!/usr/bin/perl 
+use strict;
+use warnings;
+
+
+#############################################################################
+##
+##
+## Copyright:   (c) The Padre development team
+## Licence:     This program is free software; you can redistribute it and/or
+##              modify it under the same terms as Perl itself
+#############################################################################
+
+my $app = Demo::ListView->new;
+$app->MainLoop;
+
+
+#####################
+package Demo::ListView;
+use strict;
+use warnings;
+
+use base 'Wx::App';
+
+sub OnInit {
+	my $frame = Demo::Frame->new;
+	$frame->Show(1);
+}
+
+#####################
+package Demo::Frame;
+use strict;
+use warnings;
+
+
+use Wx ':everything';
+use Wx::Event ':everything';
+
+use base 'Wx::Frame';
+
+sub new {
+	my ($class) = @_;
+
+	my $self = $class->SUPER::new(
+		undef,
+		-1,
+		'Wx::TextCtrl',
+		[ -1,  -1 ],
+		[ 750, 700 ],
+	);
+
+	my $box = Wx::BoxSizer->new( wxVERTICAL );
+
+	my $editor = Wx::TextCtrl->new(
+		$self, -1, '', Wx::wxDefaultPosition, Wx::wxDefaultSize,
+		wxTE_MULTILINE | wxNO_FULL_REPAINT_ON_RESIZE | wxTE_READONLY
+	);
+	# http://docs.wxwidgets.org/2.8.10/wx_wxtextctrl.html
+
+	my $content = '';
+	if ( open my $in, '<', $0 ) {
+		local $/ = undef;
+		$content = <$in>;
+	}
+	$editor->SetValue($content);
+
+
+	$box->Add($editor , 1, wxGROW);
+
+	my $close = Wx::Button->new( $self, -1, '&Close');
+	$box->Add($close, 0, wxALIGN_CENTER_HORIZONTAL); # http://docs.wxwidgets.org/2.8.10/wx_sizeroverview.html
+	EVT_BUTTON($self, $close, \&on_exit);
+	
+	$self->SetAutoLayout( 1 );
+	$self->SetSizer( $box );
+	# $box->Fit( $self );
+	# $box->SetSizeHints( $self );
+
+	return $self;
+}
+
+sub on_exit {
+	my ($self) = @_;
+	$self->Close;
+}
+
+
