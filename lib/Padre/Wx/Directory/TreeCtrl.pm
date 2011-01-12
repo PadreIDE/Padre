92514932315b9d082651b808cc757dbfd4fb7a03
adamk <adamk@cpan.org>
2011-01-12 02:56:19 +0000

    Enable wxCLIP_CHILDREN for some of the trees, which will reduce (but not eliminate) flickering. Someone needs to learn how to use double-buffering to fix that problem once and for all.
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@13472 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- lib/Padre/Wx/Directory/TreeCtrl.pm
+++ lib/Padre/Wx/Directory/TreeCtrl.pm
@@ -32,7 +32,7 @@ sub new {
 		Wx::wxDefaultPosition,
 		Wx::wxDefaultSize,
 		Wx::wxTR_HIDE_ROOT | Wx::wxTR_SINGLE | Wx::wxTR_FULL_ROW_HIGHLIGHT | Wx::wxTR_HAS_BUTTONS
-			| Wx::wxTR_LINES_AT_ROOT | Wx::wxBORDER_NONE
+			| Wx::wxTR_LINES_AT_ROOT | Wx::wxBORDER_NONE | Wx::wxCLIP_CHILDREN
 	);
 
 	# Create the image list
