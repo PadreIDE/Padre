e470e6f57389d5a6c26e924ea4ad553973530683
adamk <adamk@cpan.org>
2011-01-13 07:02:51 +0000

    Hating excess whitespace :)
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@13480 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- lib/Padre/Wx/TreeCtrl/ScrollLock.pm
+++ lib/Padre/Wx/TreeCtrl/ScrollLock.pm
@@ -83,6 +83,8 @@ sub DESTROY {
 	# hash cleanup after this method completes.
 }
 
+1;
+
 =pod
 
 =head1 TODO
@@ -102,9 +104,6 @@ This program is free software; you can redistribute it and/or
 modify it under the same terms as Perl 5 itself.
 
 =cut
-
-
-1;
 
 # Copyright 2008-2010 The Padre development team as listed in Padre.pm.
 # LICENSE
