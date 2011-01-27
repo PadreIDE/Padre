60d2439fa54328477d5edb933b3e81dbe80ef795
adamk <adamk@cpan.org>
2011-01-27 04:23:26 +0000

    Improved selection of which background worker to use. The new mechanism attempts to maximise specialisation of background to prevent them having to load modules wherever possible (and might reduce memory paging a bit as well).
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@13574 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- lib/Padre/TaskManager.pm
+++ lib/Padre/TaskManager.pm
@@ -141,21 +141,42 @@ sub best_thread {
 	TRACE( $_[0] ) if DEBUG;
 	my $self    = shift;
 	my $handle  = shift;
+	my $task    = $handle->class;
 	my $workers = $self->{workers};
 	my @unused  = grep { not $_->handle } @$workers;
+	my @seen    = grep { $_->{seen}->{$task} } @unused;
 
-	# First try to find a specialist.
-	# Any of them will do at this point, no futher work needed.
-	foreach my $worker (@unused) {
-		next unless $worker->{seen}->{ $handle->class };
-		return $worker;
+	# Our basic strategy is to reuse an existing worker that
+	# has done this task before to prevent loading more modules.
+	if ( @seen ) {
+		# Try to concentrate reuse as much as possible.
+		# Pick the worker that has done the least other things.
+		# Break a tie by awarding the task to the worker that has
+		# done this type of task the most often to prevent flipping
+		# between multiple with similar %seen diversity.
+		@seen = sort {
+			scalar(keys %{$a->{seen}}) <=> scalar(keys %{$b->{seen}})
+			or
+			$b->{seen}->{$task} <=> $a->{seen}->{$task}
+		} @seen;
+		return $seen[0];
 	}
 
-	# Bias towards maximum reuse of a smaller number of threads.
-	# This will (hopefully) allow the most stale threads to swap
-	# better, and will simplify decisions on when to clean up
-	# excessive threads.
-	if ( defined $unused[0] ) {
+	### TODO: In future, we could also try to check for workers which
+	# have seen the superclasses of our class so something which has
+	# seen another LWP-based task will also be chosen for a new
+	# LWP-based task.
+
+	# If nothing has seen this task before, bias towards the least
+	# specialised thread. The idea here is to try and create one
+	# big generalist worker, which will maximise the likelyhood that
+	# the other threads will specialise and minimise memory load by
+	# having all the rare stuff in one big thread where they will
+	# hopefully have shared dependencies.
+	if ( @unused ) {
+		@unused = sort {
+			scalar(keys %{$b->{seen}}) <=> scalar(keys %{$a->{seen}})
+		} @unused;
 		return $unused[0];
 	}
 
@@ -164,6 +185,7 @@ sub best_thread {
 		return $self->start_thread( scalar @$workers );
 	}
 
+	# This task will have to wait for another worker to become free
 	return undef;
 }
 
