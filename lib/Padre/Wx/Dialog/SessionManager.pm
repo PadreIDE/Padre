73d24e3adc62127c39051f89e20890bbe6f53216
szabgab <szabgab@gmail.com>
2011-02-04 11:22:16 +0000

    Show the date and time on windows as well, (by Mark Dootson)
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@13626 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- lib/Padre/Wx/Dialog/SessionManager.pm
+++ lib/Padre/Wx/Dialog/SessionManager.pm
@@ -352,7 +352,7 @@ sub _refresh_list {
 	foreach my $session ( reverse @sessions ) {
 		my $name   = $session->name;
 		my $descr  = $session->description;
-		my $update = strftime( '%F %T', localtime( $session->last_update ) );
+		my $update = strftime( '%Y-%m-%d %H:%M:%S', localtime( $session->last_update ) );
 
 		# inserting the session in the list
 		my $item = Wx::ListItem->new;
