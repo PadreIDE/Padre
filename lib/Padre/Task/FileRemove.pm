3bf566405cb154c13cc6c16844e3206cdf3e2ebc
adamk <adamk@cpan.org>
2011-02-21 05:53:05 +0000

    - Added Padre::Task::FileRemove to allow non-blocking deletion of temporary       directories in the background once they are of no further use (ADAMK)
    Did a little cleaning up of English and accuracy in Changes while doing so.
    
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@13815 bc93166f-e056-4eb9-8c59-0a0840647e1b

---
+++ lib/Padre/Task/FileRemove.pm
@@ -0,0 +1,139 @@
+package Padre::Task::FileRemove;
+
+=pod
+
+=head1 NAME
+
+Padre::Task::FileRemove - Delete files and folders in a background thread
+
+=head1 SYNOPSIS
+
+  # Fire and forget background deletion of a temporary directory
+  Padre::Task::FileRemove->new(
+    path => 'C:\foo\bar\baz',
+  )->schedule;
+
+=head1 DESCRIPTION
+
+The L<File::Remove> CPAN module is a specialised package for deleting files
+or recursively deleting directories.
+
+As well as providing the basic support for recursive deletion, it adds
+several other important features such as removing readonly limits on the fly,
+taking ownership of files if permitted, and moving the current working
+directory out of the deletion path so that directory cursors won't block the
+deletion (a particular problem on Windows).
+
+The task takes the name of a single file or directory to delete (for now), and
+proceeds to attempt a recursive deletion of the file or directory via the
+L<File::Remove> C<remove> method.
+
+=head1 METHODS
+
+=cut
+
+use 5.008;
+use strict;
+use warnings;
+use File::Spec  ();
+use Padre::Task ();
+
+our $VERSION = '0.83';
+our @ISA     = 'Padre::Task';
+
+
+
+
+
+######################################################################
+# Constructor
+
+=pod
+
+=head2 new
+
+  my $task = Padre::Task::FileRemove->new(
+      path => '/foo/bar/baz',
+  );
+
+Creates a new deletion task.
+
+Takes a single parameter C<path> which B<must> be an absolute path to the
+file to delete (as the "current directory" may change between the time the
+removal task is created and when it is executed).
+
+=cut
+
+sub new {
+	my $self = shift->SUPER::new(@_);
+
+	# Check the path to remove
+	unless ( defined $self->path ) {
+		die "Missing or invalid path";
+	}
+	unless ( File::Spec->file_name_is_absolute($self->path) ) {
+		die "File path is not absolute";
+	}
+
+	return $self;
+}
+
+=pod
+
+=head2 path
+
+The C<path> accessor returns the absolute path of the file or directory the
+task will try to delete (or tried to delete in the case of completed tasks).
+
+=cut
+
+sub path {
+	$_[0]->{path};
+}
+
+
+
+
+
+######################################################################
+# Padre::Task Methods
+
+sub run {
+	my $self = shift;
+
+	# Do not check for the path existing at prepare time as this involves
+	# a blocking stat call. Better to just pass it through and do the file
+	# existance check and any resulting shortcuts in the background.
+	my $path = $self->path;
+	unless ( -e $path ) {
+		return 1;
+	}
+
+	# Hand off to the specialist module
+	require File::Remove;
+	$self->{removed} = [
+		File::Remove::remove( \1, $path );
+	];
+
+	return 1;
+}
+
+1;
+
+=pod
+
+=head1 SEE ALSO
+
+L<Padre>, L<Padre::Task>, L<File::Remove>
+
+=head1 COPYRIGHT
+
+Copyright 2008-2011 The Padre development team as listed in Padre.pm.
+
+This program is free software; you can redistribute
+it and/or modify it under the same terms as Perl 5 itself.
+
+The full text of the license can be found in the
+LICENSE file included with this module.
+
+=cut
