89b19f5ae26e2ffac17ca8d5006c1b0b785fb1b0
adamk <adamk@cpan.org>
2011-03-15 15:32:07 +0000

    First shot at a method to load the configuration state into the dialog
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@14065 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- lib/Padre/Wx/Dialog/Preferences2.pm
+++ lib/Padre/Wx/Dialog/Preferences2.pm
@@ -8,6 +8,51 @@ use Padre::Wx::FBP::Preferences2 ();
 our $VERSION = '0.85';
 our @ISA     = 'Padre::Wx::FBP::Preferences2';
 
+
+
+
+
+######################################################################
+# Constructor and Accessors
+
+
+
+
+
+#####################################################################
+# Load and Save
+
+sub load {
+	my $self   = shift;
+	my $config = shift;
+
+	# Iterate over the configuration entries and apply the
+	# configuration state to the dialog.
+	foreach my $name ( $config->settings ) {
+		next unless $self->can($name);
+
+		# Get the Wx element for this option
+		my $value  = $config->$name();
+		my $widget = $self->$name();
+
+		# Apply this one setting to this one widget
+		if ( $widget->isa('Wx::Checkbox') ) {
+			$widget->SetValue( $value ? 1 : 0 );
+		} else {
+			next;
+		}
+	}
+
+	return 1;
+}
+
+sub save {
+	my $self   = shift;
+	my $config = shift;
+
+	die "CODE INCOMPLETE";
+}
+
 1;
 
 # Copyright 2008-2011 The Padre development team as listed in Padre.pm.
