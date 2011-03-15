039a43f2ba1f8606fc3ce33ee4a4060dcd41a6f8
adamk <adamk@cpan.org>
2011-03-15 15:02:33 +0000

    Adding concrete class
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@14063 bc93166f-e056-4eb9-8c59-0a0840647e1b

---
+++ lib/Padre/Wx/Dialog/Preferences2.pm
@@ -0,0 +1,17 @@
+package Padre::Wx::Dialog::Preferences2;
+
+use 5.008;
+use strict;
+use warnings;
+use Padre::Wx::FBP::Preferences2 ();
+
+our $VERSION = '0.85';
+our @ISA     = 'Padre::Wx::FBP::Preferences2';
+
+1;
+
+# Copyright 2008-2011 The Padre development team as listed in Padre.pm.
+# LICENSE
+# This program is free software; you can redistribute it and/or
+# modify it under the same terms as Perl 5 itself.
+
--- lib/Padre/Wx/FBP/Preferences2.pm
+++ lib/Padre/Wx/FBP/Preferences2.pm
@@ -10,7 +10,7 @@ use warnings;
 use Padre::Wx ();
 use Padre::Wx::Role::Main ();
 
-our $VERSION = '0.01';
+our $VERSION = '0.85';
 our @ISA     = qw{
 	Padre::Wx::Role::Main
 	Wx::Dialog
