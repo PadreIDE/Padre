9c4dbb0fbc26407678ebb203c0f56da9fd2addc9
szabgab <szabgab@gmail.com>
2011-03-31 19:53:15 +0000

    add a test file for #1203
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@14210 bc93166f-e056-4eb9-8c59-0a0840647e1b

---
+++ t/files/hiding_errors.pl
@@ -0,0 +1,11 @@
+use strict;
+use warnings;
+#use CGI::Carp qw(fatalsToBrowser);
+
+my no_such_sub
+
+
+# see ticket #1203
+# if the # removed from line 3 te syntax checker of Padre shows
+# that there are errors but does not show the actual errors
+
