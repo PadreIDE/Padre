35fa2ee469828d0259d768848272c50ea5a15baf
adamk <adamk@cpan.org>
2011-05-02 07:27:15 +0000

    Completing replacement for Padre::Wx::Dialog::Text
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@14269 bc93166f-e056-4eb9-8c59-0a0840647e1b

---
+++ lib/Padre/Wx/Dialog/Text2.pm
@@ -0,0 +1,34 @@
+package Padre::Wx::Dialog::Text2;
+
+use 5.008;
+use strict;
+use warnings;
+use Padre::Wx::FBP::Text;
+
+our $VERSION = '0.85';
+our @ISA     = 'Padre::Wx::FBP::Text';
+
+
+
+
+
+######################################################################
+# Original API Emulation
+
+sub show {
+	my $class = shift;
+	my $main  = shift;
+	my $title = shift || '';
+	my $text  = shift || '';
+
+	# Create the dialog
+	my $self  = $class->new($main);
+	$self->SetTitle($title);
+	$self->display->SetText($text);
+
+	# Display the dialog
+	$self->CentreOnParent;
+	$self->ShowModal;
+}
+
+1;
