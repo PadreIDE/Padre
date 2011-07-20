9cb24c2981824a56bd74965f5d11f9ce9fe29fe8
adamk <adamk@cpan.org>
2011-07-20 06:46:05 +0000

    Moving dialog for config sync from Sync2 to Sync
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@14992 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- lib/Padre/Wx/ActionLibrary.pm
+++ lib/Padre/Wx/ActionLibrary.pm
@@ -2297,8 +2297,8 @@ sub init {
 		label      => _T('Preferences Sync'),
 		comment    => _T('Share your preferences between multiple computers'),
 		menu_event => sub {
-			require Padre::Wx::Dialog::Sync2;
-			Padre::Wx::Dialog::Sync2->new( $_[0] )->ShowModal;
+			require Padre::Wx::Dialog::Sync;
+			Padre::Wx::Dialog::Sync->new( $_[0] )->ShowModal;
 		},
 	);
 
