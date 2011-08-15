d2c6aa9b22194a79cc14e2cde8e2b91d105974ce
adamk <adamk@cpan.org>
2011-08-15 02:53:30 +0000

    Adding skeleton for the new expression evaluator (part of the developer plugin)
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@15511 bc93166f-e056-4eb9-8c59-0a0840647e1b

---
+++ lib/Padre/Wx/Dialog/Expression.pm
@@ -0,0 +1,11 @@
+package Padre::Wx::Dialog::Expression;
+
+use 5.008;
+use strict;
+use warnings;
+use Padre::Wx::FBP::Expression ();
+
+our $VERSION = '0.89';
+our @ISA     = 'Padre::Wx::FBP::Expression';
+
+1;
