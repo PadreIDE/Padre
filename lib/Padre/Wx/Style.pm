ea2f3e4baf0dcde411981f622d2e6366bdc556e7
azawawi <ahmad.zawawi@gmail.com>
2011-09-13 12:45:14 +0000

    Added copyright to Padre::Wx::Style 2.0
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@16194 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- lib/Padre/Wx/Style.pm
+++ lib/Padre/Wx/Style.pm
@@ -278,3 +278,8 @@ sub apply {
 }
 
 1;
+
+# Copyright 2008-2011 The Padre development team as listed in Padre.pm.
+# LICENSE
+# This program is free software; you can redistribute it and/or
+# modify it under the same terms as Perl 5 itself.
