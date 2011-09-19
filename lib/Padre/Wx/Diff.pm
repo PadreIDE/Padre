410898aa0a7a7dafcca42764bc0f15da7561537f
azawawi <ahmad.zawawi@gmail.com>
2011-09-19 08:34:44 +0000

    Simplify diff box positioning code
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@16352 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- lib/Padre/Wx/Diff.pm
+++ lib/Padre/Wx/Diff.pm
@@ -256,9 +256,8 @@ sub show_diff_box {
 		require Padre::Wx::Dialog::Diff;
 		$self->{dialog} = Padre::Wx::Dialog::Diff->new($editor);
 	}
-	my $pt = $editor->PointFromPosition( $editor->PositionFromLine( $line + 1 ) );
-	$self->{dialog}
-		->show( $editor, $diff->{message}, $diff->{original_text}, $diff->{type}, $editor->ClientToScreenPoint($pt) );
+	$self->{dialog}->show( $editor, $diff->{message}, $diff->{original_text}, $diff->{type},
+		$editor->PointFromPosition( $editor->PositionFromLine( $line + 1 ) ) );
 }
 
 1;
