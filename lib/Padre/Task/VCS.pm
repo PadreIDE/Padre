34ec4502e25c79ebe8c79a36f8ac316c6d505551
azawawi <ahmad.zawawi@gmail.com>
2011-09-21 10:53:02 +0000

    Added missing Padre::Task::VCS to execute vcs command in the background without hassle :)
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@16417 bc93166f-e056-4eb9-8c59-0a0840647e1b

---
+++ lib/Padre/Task/VCS.pm
@@ -0,0 +1,72 @@
+package Padre::Task::VCS;
+
+use 5.008005;
+use strict;
+use warnings;
+use Padre::Task     ();
+use Padre::Util     ();
+use Padre::Logger;
+
+our $VERSION = '0.91';
+our @ISA     = 'Padre::Task';
+
+######################################################################
+# Constructor
+
+sub new {
+	my $self = shift->SUPER::new(@_);
+
+	# Just convert the document to text for now.
+	# Later, we'll suck in more data from the project and
+	# other related documents to do VCS operations more awesomely.
+	unless ( Params::Util::_INSTANCE( $self->{document}, 'Padre::Document' ) ) {
+		die "Failed to provide a document to the VCS task\n";
+	}
+
+	# Remove the document entirely as we do this,
+	# as it won't be able to survive serialisation.
+	my $document = delete $self->{document};
+
+	# Obtain document full filename
+	my $file = $document->{file};
+	$self->{filename} = $file ? $file->filename : undef;
+
+	# Obtain project's Version Control System (VCS)
+	$self->{vcs} = $document->project->vcs;
+
+	# Obtain document project dir
+	$self->{project_dir} = $document->project_dir;
+
+	return $self;
+}
+
+
+
+
+
+######################################################################
+# Padre::Task Methods
+
+sub run {
+	my $self = shift;
+
+	# Pull the text off the task so we won't need to serialize
+	# it back up to the parent Wx thread at the end of the task.
+	my $text        = delete $self->{text}        if $self->{text};
+	my $vcs         = delete $self->{vcs}         if ( $self->{vcs} );
+	my $filename    = delete $self->{filename}    if $self->{filename};
+	my $project_dir = delete $self->{project_dir} if $self->{project_dir};
+
+	# TODO implement run
+
+	$self->{data} = [];
+
+	return 1;
+}
+
+1;
+
+# Copyright 2008-2011 The Padre development team as listed in Padre.pm.
+# LICENSE
+# This program is free software; you can redistribute it and/or
+# modify it under the same terms as Perl 5 itself.
