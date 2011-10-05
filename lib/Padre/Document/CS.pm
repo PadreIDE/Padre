4c5acef52658f9f6e3986a2913fecbaf17dd175a
azawawi <ahmad.zawawi@gmail.com>
2011-10-05 11:38:56 +0000

    Added Padre::Document::CS for C#
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@16743 bc93166f-e056-4eb9-8c59-0a0840647e1b

---
+++ lib/Padre/Document/CS.pm
@@ -0,0 +1,34 @@
+package Padre::Document::CS;
+
+use 5.008;
+use strict;
+use warnings;
+use Padre::Document::DoubleSlashComment ();
+
+our $VERSION = '0.91';
+our @ISA     = qw{
+	Padre::Document::DoubleSlashComment
+};
+
+# C# Keywords
+# The list is obtained from src/scite/src/cpp.properties
+sub lexer_keywords {
+	return [
+		[   qw(abstract as ascending base bool break by byte case catch char checked
+class const continue decimal default delegate descending do double else enum
+equals event explicit extern false finally fixed float for foreach from goto group if
+implicit in int interface internal into is join lock let long namespace new null
+object on operator orderby out override params private protected public
+readonly ref return sbyte sealed select short sizeof stackalloc static
+string struct switch this throw true try typeof uint ulong
+unchecked unsafe ushort using var virtual void where while)
+		],
+	];
+}
+
+1;
+
+# Copyright 2008-2011 The Padre development team as listed in Padre.pm.
+# LICENSE
+# This program is free software; you can redistribute it and/or
+# modify it under the same terms as Perl 5 itself.
