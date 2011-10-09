044b043425f11daafe6bd21852f307921de12004
Olivier Mengué <dolmen@cpan.org>
2011-10-09 09:19:53 +0000

    "About" dialog refactoring
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@16864 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- lib/Padre/Wx/Dialog/About.pm
+++ lib/Padre/Wx/Dialog/About.pm
@@ -133,23 +133,23 @@ sub _translation {
 #######
 sub _system_info {
 	my $self = shift;
-	
-	$self->{off_set} = 24;
+
+	my $offset = 24;
 
 	$self->{output}->AppendText("\n");
 
-	$self->{output}->AppendText( sprintf "%$self->{off_set}s %s\n", 'Padre', $VERSION );
+	$self->{output}->AppendText( sprintf "%*s %s\n", $offset, 'Padre', $VERSION );
 
-	$self->_core_info();
-	$self->_wx_info();
+	$self->_core_info($offset);
+	$self->_wx_info($offset);
 
 	$self->{output}->AppendText("Other...\n");
 
-	$self->{output}->AppendText( sprintf "%$self->{off_set}s %s\n", 'PPI', $PPI::VERSION );
+	$self->{output}->AppendText( sprintf "%*s %s\n", $offset, 'PPI', $PPI::VERSION );
 
 	my $config_dir_txt = Wx::gettext('Config:');
 	my $config_dir     = Padre::Constant::CONFIG_DIR;
-	$self->{output}->AppendText( sprintf "%$self->{off_set}s %s\n", $config_dir_txt, $config_dir );
+	$self->{output}->AppendText( sprintf "%*s %s\n", $offset, $config_dir_txt, $config_dir );
 	
 	return;
 }
@@ -158,22 +158,22 @@ sub _system_info {
 # Composed Method _core_info
 #######
 sub _core_info {
-	my $self = shift;
+	my ($self, $offset) = @_;
 
 	$self->{output}->AppendText("Core...\n");
 
-	$self->{output}->AppendText( sprintf "%$self->{off_set}s %s\n", $Config{osname}, $Config{archname} );
+	$self->{output}->AppendText( sprintf "%*s %s\n", $offset, $Config{osname}, $Config{archname} );
 
 	if ( $Config{osname} eq 'linux' ) {
 
 		my $kernel = qx{uname -r};
 		chomp($kernel);
-		$self->{output}->AppendText( sprintf "%$self->{off_set}s %s\n", 'kernel', $kernel );
+		$self->{output}->AppendText( sprintf "%*s %s\n", $offset, 'kernel', $kernel );
 
 		my $description = qx{cat /etc/issue};
 		chomp($description);
 		$description =~ s/\\n \\l//;
-		$self->{output}->AppendText( sprintf "%$self->{off_set}s %s", 'description', $description );
+		$self->{output}->AppendText( sprintf "%*s %s\n", $offset, 'description', $description );
 	}
 
 	# Yes, THIS variable should have this upper case char :-)
@@ -181,18 +181,18 @@ sub _core_info {
 
 	# $perl_version = "$perl_version";
 	$perl_version =~ s/^v//;
-	$self->{output}->AppendText( sprintf "%$self->{off_set}s %s\n", 'Perl', $perl_version );
+	$self->{output}->AppendText( sprintf "%*s %s\n", $offset, 'Perl', $perl_version );
 
 	# How many threads are running
 	my $threads_text = Wx::gettext('Threads:');
 	my $threads = $INC{'threads.pm'} ? scalar( threads->list ) : Wx::gettext('(disabled)');
-	$self->{output}->AppendText( sprintf "%$self->{off_set}s %s\n", 'Threads', $threads );
+	$self->{output}->AppendText( sprintf "%*s %s\n", $offset, 'Threads', $threads );
 
 	# Calculate the current memory in use across all threads
 	my $RAM_text = Wx::gettext('RAM:');
 	my $ram = Padre::Util::humanbytes( Padre::Util::process_memory() ) || '0';
 	$ram = Wx::gettext('(unsupported)') if $ram eq '0';
-	$self->{output}->AppendText( sprintf "%$self->{off_set}s %s\n", 'Ram', $ram );
+	$self->{output}->AppendText( sprintf "%*s %s\n", $offset, 'Ram', $ram );
 
 	return;
 }
@@ -201,29 +201,29 @@ sub _core_info {
 # Composed Method _wx_info
 #######
 sub _wx_info {
-	my $self = shift;
+	my ($self, $offset) = @_;
 
 	$self->{output}->AppendText("Wx...\n");
 
-	$self->{output}->AppendText( sprintf "%$self->{off_set}s %s\n", 'Wx', $Wx::VERSION );
+	$self->{output}->AppendText( sprintf "%*s %s\n", $offset, 'Wx', $Wx::VERSION );
 
 
 	# Reformat the native wxWidgets version string slightly
 	my $wx_widgets = Wx::wxVERSION_STRING();
 	$wx_widgets =~ s/^wx\w+\s+//;
-	$self->{output}->AppendText( sprintf "%$self->{off_set}s %s\n", 'WxWidgets', $wx_widgets );
+	$self->{output}->AppendText( sprintf "%*s %s\n", $offset, 'WxWidgets', $wx_widgets );
 
-	$self->{output}->AppendText( sprintf "%$self->{off_set}s %s\n", 'unicode', Wx::wxUNICODE() );
+	$self->{output}->AppendText( sprintf "%*s %s\n", $offset, 'unicode', Wx::wxUNICODE() );
 
 	require Alien::wxWidgets;
 	my $alien = $Alien::wxWidgets::VERSION;
 
-	$self->{output}->AppendText( sprintf "%$self->{off_set}s %s\n", 'Alien::wxWidgets', $alien );
+	$self->{output}->AppendText( sprintf "%*s %s\n", $offset, 'Alien::wxWidgets', $alien );
 
 	$self->{output}
-		->AppendText( sprintf "%$self->{off_set}s %s\n", 'Wx::Perl::ProcessStream', $Wx::Perl::ProcessStream::VERSION );
+		->AppendText( sprintf "%*s %s\n", $offset, 'Wx::Perl::ProcessStream', $Wx::Perl::ProcessStream::VERSION );
 
-	$self->{output}->AppendText( sprintf "%$self->{off_set}s %s\n", 'Wx::Scintilla', $Wx::Scintilla::VERSION );
+	$self->{output}->AppendText( sprintf "%*s %s\n", $offset, 'Wx::Scintilla', $Wx::Scintilla::VERSION );
 
 	return;
 }
