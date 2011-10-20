50543449df5fa88e145c5febe6da934efe201f48
azawawi <ahmad.zawawi@gmail.com>
2011-10-20 21:28:28 +0000

    Removed the need to do HTTPS metacpan requests and removed the LWP::Protocol::https. Padre now uses ElasticSearch REST POST JSON Requests. Big words for such a simple thing. It is way fast now on win32 and ubuntu :)
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@17116 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- lib/Padre/Task/CPAN2.pm
+++ lib/Padre/Task/CPAN2.pm
@@ -45,27 +45,110 @@ sub run {
 	my $query = delete $self->{query};
 
 	if ( $command eq CPAN_SEARCH ) {
-		require LWP::UserAgent;
-		my $ua = LWP::UserAgent->new;
-		$ua->timeout(10);
-		$ua->env_proxy;
-		my $url      = "https://metacpan.org/search/autocomplete?q=$query";
-		my $response = $ua->get($url);
-		unless ( $response->is_success ) {
-			TRACE( sprintf( "Got '%s for %s", $response->status_line, $url ) )
-				if DEBUG;
-			return;
-		}
-		require JSON::XS;
-		$self->{model} = JSON::XS::decode_json( $response->decoded_content );
-	} elsif ( $command eq CPAN_INSTALL ) {
+		$self->{model} = $self->metacpan_autocomplete( $query, 10 );
+	}
+	elsif ( $command eq CPAN_INSTALL ) {
 
 		#TODO run cpanm module!
-	} else {
+	}
+	else {
 		TRACE("Unimplemented $command. Please fix!") if DEBUG;
 	}
 
 	return 1;
+}
+
+#
+# Adopted from https://github.com/CPAN-API/metacpan-web
+#
+sub metacpan_autocomplete {
+	my ( $self, $query, $size ) = @_;
+
+	# Convert :: to spaces so we dont crash request :)
+	$query =~ s/::/ /g;
+
+	# Create an array of query keywords that are separated by spaces
+	my @query = split( /\s+/, $query );
+
+	# The documentation Module-Name that should be analyzed
+	my $should = [
+		map {
+			{ field   => { 'documentation.analyzed'  => "$_*" } },
+			  { field => { 'documentation.camelcase' => "$_*" } }
+		  } grep { $_ } @query
+	];
+
+	# The distribution we do not want in our search
+	my @ROGUE_DISTRIBUTIONS =
+	  qw(kurila perl_debug perl-5.005_02+apache1.3.3+modperl pod2texi perlbench spodcxx);
+
+	# The ElasticSearch query in Perl
+	my %payload = (
+		query => {
+			filtered => {
+				query => {
+					custom_score => {
+						query => { bool => { should => $should } },
+						script =>
+"_score - doc['documentation'].stringValue.length()/100"
+					},
+				},
+				filter => {
+					and => [
+						{
+							not => {
+								filter => {
+									or => [
+										map {
+											{ term =>
+												  { 'file.distribution' => $_ }
+											}
+										  } @ROGUE_DISTRIBUTIONS
+									]
+								}
+							}
+						},
+						{ exists => { field          => 'documentation' } },
+						{ term   => { 'file.indexed' => \1 } },
+						{ term   => { 'file.status'  => 'latest' } },
+						{
+							not => {
+								filter =>
+								  { term => { 'file.authorized' => \0 } }
+							}
+						}
+					]
+				}
+			}
+		},
+		fields => [qw(documentation release author distribution)],
+		size   => $size,
+	);
+
+	# Convert ElasticSearch Perl query to a JSON request
+	require JSON::XS;
+	my $json_request = JSON::XS::encode_json( \%payload );
+
+	# POST the json request to api.metacpan.org
+	require LWP::UserAgent;
+	my $ua = LWP::UserAgent->new( agent => "Padre/$VERSION" );
+	$ua->timeout(10);
+	$ua->env_proxy;
+	my $response = $ua->post( 'http://api.metacpan.org/v0/file/_search',
+		Content => $json_request, );
+
+	unless ( $response->is_success ) {
+		TRACE( sprintf( "Got '%s' from metacpan.org", $response->status_line ) )
+		  if DEBUG;
+		return [];
+	}
+
+	# Decode json response then cleverly map it for the average joe :)
+	my $data = JSON::XS::decode_json( $response->decoded_content );
+	my @results = map { $_->{fields} } @{ $data->{hits}->{hits} || [] };
+
+	# And return its reference
+	return \@results;
 }
 
 1;
