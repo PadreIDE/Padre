7011bfcfed191002b66039541e21a455c9a0a8f9
adamk <adamk@cpan.org>
2011-11-11 00:43:18 +0000

    Adding wxFormBuilder version of the Replace dialog
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@17330 bc93166f-e056-4eb9-8c59-0a0840647e1b

---
+++ lib/Padre/Wx/Dialog/Replace.pm
@@ -0,0 +1,11 @@
+package Padre::Wx::Dialog::Replace;
+
+use 5.008;
+use strict;
+use warnings;
+use Padre::Wx::FBP::Replace ();
+
+our $VERSION = '0.91';
+our @ISA     = 'Padre::Wx::FBP::Replace';
+
+1;
