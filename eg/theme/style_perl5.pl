751504bf9e46e425bcd776409f5cf152594b69fe
kevindawson <bowtie@cpan.org>
2011-11-14 13:54:08 +0000

    updated to new prefixes
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@17374 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- eg/theme/style_perl5.pl
+++ eg/theme/style_perl5.pl
@@ -6,6 +6,8 @@ use warnings;
 
 # Turn on $OUTPUT_AUTOFLUSH
 $| = 1;
+
+use feature 'unicode_strings';
 use diagnostics;
 use Data::Printer { caller_info => 1, colored => 1, };
 
@@ -13,15 +15,15 @@ my $STYLE_DEFAULT = text;
 my $STYLE_BRACELIGHT;
 my $STYLE_BRACEBAD;
 
-my $PL_COMMENTLINE; #comment
-my $PL_POD;
+my $SCE_PL_COMMENTLINE; #comment
+my $SCE_PL_POD;
 =pod
 
 =head1 heading-1
 
 =cut
 
-my $PL_POD_VERB;
+my $SCE_PL_POD_VERB;
 =pod
 
 =over
@@ -29,68 +31,67 @@ my $PL_POD_VERB;
 =item * This is a bulleted list.
 
 	#code is a pod verb
-	my $PL_POD_VERB;
+	my $SCE_PL_POD_VERB;
 
 =back
 
 =cut
 
-my $PL_NUMBER = 1;
+my $SCE_PL_NUMBER = 1;
 
-my $PL_WORD;
-# use my sub for while else print return if chomp shift
+my $SCE_PL_WORD; # use my sub for while else print return if chomp shift
 say 'sample text';
 print "sample text\n";
 sub function {
 	return;
 }
 
-my $PL_STRING = "string";
-my $PL_CHARACTER = 'c';
+my $SCE_PL_STRING = "string";
+my $SCE_PL_CHARACTER = 'c';
 
-my $PL_PUNCTUATION; # () [] {} 
-my $PL_PREPROCESSOR; # not emitted by LexPerl, can we recycle it?
-my $PL_OPERATOR; # + - * % ** . =~ x , ++ -- ||= != <=
-my $PL_IDENTIFIER; #struct $variable @array %hash 
+my $SCE_PL_PUNCTUATION; # () [] {} 
+my $SCE_PL_PREPROCESSOR; # not emitted by LexPerl, can we recycle it?
+my $SCE_PL_OPERATOR; # + - * % ** . =~ x , ++ -- ||= != <=
+my $SCE_PL_IDENTIFIER; #struct $variable @array %hash 
 
-my $PL_SCALAR;
-my @PL_ARRAY;
-$PL_ARRAY[100]; # indexed
-my %PL_HASH;
-$PL_HASH{keyname};
+my $SCE_PL_SCALAR;
+my @SCE_PL_ARRAY;
+$SCE_PL_ARRAY[100]; # indexed
+my %SCE_PL_HASH;
+$SCE_PL_HASH{keyname};
 
 
-my $PL_SYMBOLTABLE;
+my $SCE_PL_SYMBOLTABLE;
 *Package::Foo::variable = 'blah';
 
-my $PL_XLAT = tr/abc/xyz/;
+my $SCE_PL_XLAT = tr/abc/xyz/;
 
-my $PL_REGEX =~ m/ <:name>(pattern) /p;
-my $PL_REGSUBST = s/^\s{1}//a;
+my $SCE_PL_REGEX =~ m/ <:name>(pattern) /p;
+my $SCE_PL_REGSUBST = s/^\s{1}//a;
 
-my $PL_LONGQUOTE; # what?
-my $PL_BACKTICKS = `back ticks`;
+my $SCE_PL_LONGQUOTE; # what?
+my $SCE_PL_BACKTICKS = `back ticks`;
 
-my $PL_DATASECTION; # see below
+my $SCE_PL_DATASECTION; # see below
 
-my $PL_HERE_DELIM = <<FOO;
+my $SCE_PL_HERE_DELIM = <<FOO;
 sample text
 FOO
-my $PL_HERE_Q = <<'FOO';
+my $SCE_PL_HERE_Q = <<'FOO';
 sample text
 FOO
-my $PL_HERE_QQ = <<"FOO";
+my $SCE_PL_HERE_QQ = <<"FOO";
 sample text
 FOO
-my $PL_HERE_QX = <<`FOO`;
+my $SCE_PL_HERE_QX = <<`FOO`;
 sample text
 FOO
 
-my $PL_STRING_Q  = q( single quoted string literal );
-my $PL_STRING_QQ = qq( double quoted string literal );
-my $PL_STRING_QX = qx{ command };
-my $PL_STRING_QR = qr/ sample text /;
-my @PL_STRING_QW = qw( word list );
+my $SCE_PL_STRING_Q  = q( single quoted string literal );
+my $SCE_PL_STRING_QQ = qq( double quoted string literal );
+my $SCE_PL_STRING_QX = qx{ command };
+my $SCE_PL_STRING_QR = qr/ sample text /;
+my @SCE_PL_STRING_QW = qw( word list );
 
 my $escaped = "Hello World\n";
 
