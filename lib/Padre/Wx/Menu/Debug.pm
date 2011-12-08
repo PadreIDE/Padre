90c74598eb1210211df73ea6ea6c8cc266d25c3a
kevindawson <bowtie@cpan.org>
2011-12-08 18:13:42 +0000

    Tidy up Debug menu so that it only contains new items
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@17637 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- lib/Padre/Wx/Menu/Debug.pm
+++ lib/Padre/Wx/Menu/Debug.pm
@@ -13,9 +13,6 @@ our $VERSION = '0.93';
 our @ISA     = 'Padre::Wx::Menu';
 
 
-
-
-
 #####################################################################
 # Padre::Wx::Menu Methods
 
@@ -41,7 +38,7 @@ sub new {
 		'debug.panel_debugger',
 	);
 
-	$self->AppendSeparator if $main->config->feature_debug2;
+	$self->AppendSeparator;
 
 	$self->{launch} = $self->add_menu_action(
 		'debug.launch',
@@ -51,79 +48,14 @@ sub new {
 		'debug.set_breakpoints',
 	);
 
-	# $self->AppendSeparator if $main->config->feature_debug2;
-
 	$self->{quit2} = $self->add_menu_action(
 		'debug.quit2',
 	);
 
-	$self->AppendSeparator if $main->config->feature_debug2;
+	$self->AppendSeparator;
 
 	$self->{visit_debug_wiki} = $self->add_menu_action(
 		'debug.visit_debug_wiki',
-	);
-
-	$self->{step_in} = $self->add_menu_action(
-		'debug.step_in',
-	);
-	$self->{step_over} = $self->add_menu_action(
-		'debug.step_over',
-	);
-
-	$self->{step_out} = $self->add_menu_action(
-		'debug.step_out',
-	);
-
-	$self->{run} = $self->add_menu_action(
-		'debug.run',
-	);
-
-	$self->AppendSeparator if !$main->config->feature_debug2;
-
-	$self->{jump_to} = $self->add_menu_action(
-		'debug.jump_to',
-	);
-
-	$self->AppendSeparator if !$main->config->feature_debug2;
-
-
-	$self->{set_breakpoint} = $self->add_menu_action(
-		'debug.set_breakpoint',
-	);
-
-	$self->{remove_breakpoint} = $self->add_menu_action(
-		'debug.remove_breakpoint',
-	);
-
-	$self->{list_breakpoints} = $self->add_menu_action(
-		'debug.list_breakpoints',
-	);
-
-	$self->AppendSeparator if !$main->config->feature_debug2;
-
-	$self->{show_stack_trace} = $self->add_menu_action(
-		'debug.show_stack_trace',
-	);
-
-	$self->{display_value} = $self->add_menu_action(
-		'debug.display_value',
-	);
-
-	$self->AppendSeparator if !$main->config->feature_debug2;
-
-	$self->{show_value} = $self->add_menu_action(
-		'debug.show_value',
-	);
-
-	$self->{evaluate_expression} = $self->add_menu_action(
-		'debug.evaluate_expression',
-	);
-
-	$self->AppendSeparator if !$main->config->feature_debug2;
-
-
-	$self->{quit} = $self->add_menu_action(
-		'debug.quit',
 	);
 
 	return $self;
@@ -141,29 +73,15 @@ sub refresh {
 	my $document = Padre::Current::_CURRENT(@_)->document;
 	my $hasdoc   = $document ? 1 : 0;
 
-	$self->{panel_breakpoints}->Check( $config->main_panel_breakpoints )   if $main->config->feature_debug2;
-	$self->{panel_debug_output}->Check( $config->main_panel_debug_output ) if $main->config->feature_debug2;
-	$self->{panel_debugger}->Check( $config->main_panel_debugger )         if $main->config->feature_debug2;
+	$self->{panel_breakpoints}->Check( $config->main_panel_breakpoints );
+	$self->{panel_debug_output}->Check( $config->main_panel_debug_output );
+	$self->{panel_debugger}->Check( $config->main_panel_debugger );
 
-	$self->{launch}->Enable(1)           if $main->config->feature_debug2;
-	$self->{set_breakpoints}->Enable(1) if $main->config->feature_debug2;
-	$self->{quit2}->Enable(1)            if $main->config->feature_debug2;
-	$self->{visit_debug_wiki}->Enable(1) if $main->config->feature_debug2;
+	$self->{launch}->Enable(1);
+	$self->{set_breakpoints}->Enable(1);
+	$self->{quit2}->Enable(1);
 
-	if ( !$main->config->feature_debug2 ) {
-		$self->{step_in}->Enable($hasdoc);
-		$self->{step_over}->Enable($hasdoc);
-		$self->{step_out}->Enable($hasdoc);
-		$self->{run}->Enable($hasdoc);
-		$self->{jump_to}->Enable($hasdoc);
-		$self->{set_breakpoint}->Enable($hasdoc);
-		$self->{remove_breakpoint}->Enable($hasdoc);
-		$self->{list_breakpoints}->Enable($hasdoc);
-		$self->{show_stack_trace}->Enable($hasdoc);
-		$self->{display_value}->Enable($hasdoc);
-		$self->{show_value}->Enable($hasdoc);
-		$self->{evaluate_expression}->Enable($hasdoc);
-	}
+	$self->{visit_debug_wiki}->Enable(1);
 
 	return 1;
 }
