cc93b112ef20b321f9f68567eb420cf31c3b1728
adamk <adamk@cpan.org>
2011-12-15 06:00:36 +0000

    Fleshing out the wxFormBuilder FindFast panel a bit more
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@17694 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- lib/Padre/Wx/Panel/FindFast.pm
+++ lib/Padre/Wx/Panel/FindFast.pm
@@ -8,6 +8,71 @@ use Padre::Wx::FBP::FindFast ();
 our $VERSION = '0.93';
 our @ISA     = 'Padre::Wx::FBP::FindFast';
 
+
+
+
+
+######################################################################
+# Constructor
+
+sub new {
+	my $class = shift;
+	my $self  = $class->SUPER::new(@_);
+
+	# Immediately hide the panel to prevent display glitching
+	$self->Hide;
+
+	# Create a private pane in AUI
+	$self->main->aui->AddPane(
+		$self,
+		Padre::Wx->aui_pane_info(
+			Name           => 'footer',
+			CaptionVisible => 0,
+			Layer          => 1,
+			PaneBorder     => 0,
+		)->Bottom->Fixed->Hide,
+	);
+
+	return $self;
+}
+
+
+
+
+
+######################################################################
+# Main Methods
+
+sub show {
+	my $self = shift;
+
+	# Reset the content of the panel
+	$self->{find_term}->SetValue('');
+	$self->{find_term}->SetFocus;
+
+	# When showing always give focus to the term
+	$self->{find_term}->SetFocus;
+
+	# Show the AUI pane
+	my $aui  = $self->main->aui;
+	$aui->Find('footer')->Show;
+	$aui->Update;
+
+	return 1;
+}
+
+sub hide {
+	my $self = shift;
+
+	# Hide the AUI pane
+	# Show the panel
+	my $aui  = $self->main->aui;
+	$aui->Find('footer')->Hide;
+	$aui->Update;
+
+	return 1;
+}
+
 1;
 
 # Copyright 2008-2011 The Padre development team as listed in Padre.pm.
