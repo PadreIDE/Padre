a92d6386156405a3d4d4e085ca24d7858b3f6a4e
adamk <adamk@cpan.org>
2011-12-21 12:59:59 +0000

    Finished basic parser for svn prop files
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@17748 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- lib/Padre/SVN.pm
+++ lib/Padre/SVN.pm
@@ -5,8 +5,27 @@ package Padre::SVN;
 use 5.008;
 use strict;
 use warnings;
+use File::Spec ();
 
 our $VERSION = '0.93';
+
+# Find and parse the properties file
+sub file_props {
+	my $file = shift;
+	my $base = find_props($file);
+	return parse_props($base);
+}
+
+# Find the props-base for a file
+sub find_props {
+	my $file = shift;
+	my ( $v, $d, $f ) = File::Spec->splitpath($file);
+	return File::Spec->catpath(
+		$v,
+		File::Spec->catdir( $d, '.svn', 'prop-base' ),
+		$f . '.svn-base',
+	);
+}
 
 # Parse a property file
 sub parse_props {
@@ -14,16 +33,64 @@ sub parse_props {
 	open( FILE, '<', $file ) or die "Failed to open '$file'";
 
 	# Simple state parser
-	my %props = ();
+	my %hash   = ();
+	my $kbytes = 0;
+	my $vbytes = 0;
+	my $key    = undef;
+	my $value  = undef;
 	while ( my $line = <FILE> ) {
+		if ( $vbytes ) {
+			my $l = length $line;
+			if ( $l == $vbytes + 1 ) {
+				# Perfect content length
+				chomp($line);
+				$hash{$key} = $value . $line;
+				$vbytes = 0;
+				$key    = undef;
+				$value  = undef;
+				next;
+			}
+			if ( $l > $vbytes ) {
+				$value .= $line;
+				$vbytes -= $l;
+				next;
+			}
+			die "Found value longer than specified length";
+		}
+
+		if ( $kbytes ) {
+			my $l = length $line;
+			if ( $l == $kbytes + 1 ) {
+				# Perfect content length
+				chomp($line);
+				$key .= $line;
+				$kbytes = 0;
+				next;
+			}
+			if ( $l > $kbytes ) {
+				$key .= $line;
+				$kbytes -= $l;
+				next;
+			}
+			die "Found key longer than specified length";
+		}
+
+		if ( defined $key ) {
+			$line =~ /^V\s(\d+)/ or die "Failed to find expected V line";
+			$vbytes = $1;
+			$value  = '';
+			next;
+		}
+
 		last if $line =~ /^END/;
 
 		# We should have a K line indicating key size
 		$line =~ /^K\s(\d+)/ or die "Failed to find expected K line";
-		my $bytes = $1;
-
-		# Fetch 
+		$kbytes = $1;
+		$key    = '';
 	}
+
+	return \%hash;
 }
 
 1;
--- t/12_svn.t
+++ t/12_svn.t
@@ -7,15 +7,18 @@ use warnings;
 use Test::More;
 BEGIN {
 	if ( -d '.svn' ) {
-		plan( tests => 2 );
+		plan tests => 4;
 	} else {
-		plan( skip_all => 'Not in an SVN checkout' );
+		plan skip_all => 'Not in an SVN checkout';
 	}
 }
 use Test::NoWarnings;
 use File::Spec::Functions ':ALL';
 use t::lib::Padre;
 use Padre::SVN;
+
+my $t = catfile( 't', '12_svn.t' );
+ok( -f $t, "Found file $t" );
 
 
 
@@ -24,7 +27,14 @@ use Padre::SVN;
 ######################################################################
 # Basic checks 
 
-my $file = File::Spec->catfile( qw{
-	t .svn props-base File.pm.svn-base
-} );
-ok( -f $file, 'Found property file' );
+# Find the property file
+my $file = Padre::SVN::find_props($t);
+ok( -f $file, "Found property file $file" );
+
+# Parse the property file
+my $hash = Padre::SVN::parse_props($file);
+is_deeply(
+	$hash,
+	{ 'svn:eol-style' => 'LF' },
+	'Found expected properties',
+);
