6ebda36633a1885e0651b8d24988c302163ab0f4
adamk <adamk@cpan.org>
2012-01-10 06:44:52 +0000

    Added documentation for Padre::Wx::TreeCtrl
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@17858 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- lib/Padre/Wx/TreeCtrl.pm
+++ lib/Padre/Wx/TreeCtrl.pm
@@ -1,6 +1,20 @@
 package Padre::Wx::TreeCtrl;
 
-# A general use TreeCtrl that adds a variety of convenience methods
+=pod
+
+=head1 NAME
+
+Padre::Wx::TreeCtrl - A Wx::TreeCtrl with various extra convenience methods
+
+=head1 DESCRIPTION
+
+B<Padre::Wx::TreeCtrl> is a direct subclass of L<Wx::TreeCtrl> with a
+handful of additional methods that make life easier when writing GUI
+components for Padre that use trees.
+
+=head1 METHODS
+
+=cut
 
 use 5.008;
 use strict;
@@ -18,6 +32,35 @@ our @ISA     = 'Wx::TreeCtrl';
 ######################################################################
 # ScrollLock Integration
 
+=pod
+
+=head2 lock_scroll
+
+  SCOPE: {
+      my $lock = $tree->lock_scroll;
+  
+      # Apply changes to the tree
+  }
+
+When making changes to L<Wx::TreeCtrl> objects, many changes to the tree
+structure also include an implicit movement of the scroll position to
+focus on the node that was changed.
+
+When generating changes to trees that are not the immediate focus of the
+user this can be extremely flickery and disconcerting, especially when
+generating entire large trees.
+
+The C<lock_scroll> method creates a guard object which combines an update
+lock with a record of the scroll position of the tree.
+
+When the object is destroyed, the scroll position of the tree is returned
+to the original position immediately before the update lock is released.
+
+The effect is that the tree has changed silently, with the scroll position
+remaining unchanged.
+
+=cut
+
 sub lock_scroll {
 	Padre::Wx::ScrollLock->new( $_[0] );
 }
@@ -29,8 +72,20 @@ sub lock_scroll {
 ######################################################################
 # Expanded Wx-like Methods
 
-# Iterate through the children to find one with specific text.
-# Return undef if no child with that text exists.
+=pod
+
+=head2 GetChildByText
+
+  my $item = $tree->GetChildByText("Foo");
+
+The C<GetChildByText> method is a convenience method for searching through
+a tree to find a specific item based on the item text of the child.
+
+It returns the item ID of the first node containing the search text or
+C<undef> if no element in the tree contains the search text.
+
+=cut
+
 sub GetChildByText {
 	my $self = shift;
 	my $item = shift;
@@ -54,8 +109,19 @@ sub GetChildByText {
 	return undef;
 }
 
-# Fetch a list of all Perl data elements for all nodes
-# in depth-first top-to-bottom order.
+=pod
+
+=head2 GetChildrenPlData
+
+  my @data = $tree->GetChildrenPlData;
+
+The C<GetChildrenPlData> method fetches a list of Perl data elements
+(via C<GetPlData>) for B<all> nodes in the tree.
+
+The list is returned based on a depth-first top-down node order.
+
+=cut
+
 sub GetChildrenPlData {
 	my $self  = shift;
 	my @queue = $self->GetRootItem;
@@ -77,8 +143,18 @@ sub GetChildrenPlData {
 	return \@data;
 }
 
-# Fetch a list of the Perl data elements for expanded nodes
-# in depth-first top to bottom order.
+=pod
+
+=head2 GetExpandedPlData
+
+  my @data = $tree->GetExpandedPlData;
+
+The C<GetExpandedPlData> method is a variation of the C<GetChildrenPlData>
+method. It returns a list of Perl data elements in depth-first top-down node
+order, but only for nodes which are expanded (via C<IsExpanded>).
+
+=cut
+
 sub GetExpandedPlData {
 	my $self  = shift;
 	my @queue = $self->GetRootItem;
@@ -104,7 +180,16 @@ sub GetExpandedPlData {
 
 1;
 
-# Copyright 2008-2012 The Padre development team as listed in Padre.pm.
-# LICENSE
-# This program is free software; you can redistribute it and/or
-# modify it under the same terms as Perl 5 itself.
+=pod
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2008-2012 The Padre development team as listed in Padre.pm.
+
+This program is free software; you can redistribute
+it and/or modify it under the same terms as Perl 5 itself.
+
+The full text of the license can be found in the
+LICENSE file included with this module.
+
+=cut
