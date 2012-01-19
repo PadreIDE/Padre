f59267aba14e92a1fd590c1c088cdf3b2e19c837
adamk <adamk@cpan.org>
2012-01-19 22:34:18 +0000

    Fixing some minor test complaints
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@17992 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- lib/Padre/Wx/ListView.pm
+++ lib/Padre/Wx/ListView.pm
@@ -5,7 +5,7 @@ package Padre::Wx::ListView;
 use 5.008;
 use strict;
 use warnings;
-use Padre::Wx             ();
+use Padre::Wx ();
 
 our $VERSION = '0.93';
 our @ISA     = 'Wx::ListView';
@@ -78,3 +78,8 @@ sub tidy_content {
 }
 
 1;
+
+# Copyright 2008-2012 The Padre development team as listed in Padre.pm.
+# LICENSE
+# This program is free software; you can redistribute it and/or
+# modify it under the same terms as Perl 5 itself.
