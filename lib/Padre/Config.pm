28ec1c04d203e53b3c41d3d9d6cae4a0f50f6e96
adamk <adamk@cpan.org>
2012-01-23 14:08:37 +0000

    Whitespace
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@18052 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- lib/Padre/Config.pm
+++ lib/Padre/Config.pm
@@ -839,9 +839,9 @@ setting(
 
 # Directory Tree Settings
 setting(
-	name  => 'default_projects_directory',
-	type  => Padre::Constant::PATH,
-	store => Padre::Constant::HOST,
+	name    => 'default_projects_directory',
+	type    => Padre::Constant::PATH,
+	store   => Padre::Constant::HOST,
 	default => File::HomeDir->my_documents || '',
 );
 
@@ -849,9 +849,9 @@ setting(
 
 # The default editor font should be Consolas 10pt on Vista and Windows 7
 setting(
-	name  => 'editor_font',
-	type  => Padre::Constant::ASCII,
-	store => Padre::Constant::HUMAN,
+	name    => 'editor_font',
+	type    => Padre::Constant::ASCII,
+	store   => Padre::Constant::HUMAN,
 	default => Padre::Util::DISTRO =~ /^WIN(?:VISTA|7)$/ ? 'consolas 10' : '',
 );
 setting(
