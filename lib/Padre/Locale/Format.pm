95456b0bcb6f471b1cc7b7f5598e478783aed5da
azawawi <ahmad.zawawi@gmail.com>
2012-03-20 13:43:21 +0000

    Fix xt/copyright.t under RELEASE_TESTING=1
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@18877 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- lib/Padre/Locale/Format.pm
+++ lib/Padre/Locale/Format.pm
@@ -101,3 +101,8 @@ sub bytes {
 }
 
 1;
+
+# Copyright 2008-2012 The Padre development team as listed in Padre.pm.
+# LICENSE
+# This program is free software; you can redistribute it and/or
+# modify it under the same terms as Perl 5 itself.
