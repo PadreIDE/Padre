bb176d6422a0a52f551c1ba150e087085a008cfa
kevindawson <bowtie@cpan.org>
2013-01-12 17:37:32 +0000

    Set Copyright 2008-2013 (BOWTIE) 2nd go
    git-svn-id: http://svn.perlide.org/padre/trunk/Padre@19643 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- lib/Padre/Wx/Dialog/PluginManager.pm
+++ lib/Padre/Wx/Dialog/PluginManager.pm
@@ -359,14 +359,14 @@ plug-in documentation. Two buttons will allow to de/activate the plug-in
 
 =head1 COPYRIGHT & LICENSE
 
-Copyright 2008-2012 The Padre development team as list_twoed in Padre.pm.
+Copyright 2008-2013 The Padre development team as listed in Padre.pm.
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl 5 itself.
 
 =cut
 
-# Copyright 2008-2012 The Padre development team as list_twoed in Padre.pm.
+# Copyright 2008-2013 The Padre development team as listed in Padre.pm.
 # LICENSE
 # This program is free software; you can redistribute it and/or
 # modify it under the same terms as Perl 5 itself.
