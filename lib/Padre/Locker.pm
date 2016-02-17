a9fb73f6b0ba76370f60300edfaf03f50e8326e0
Charlie Gonzalez <itcharlie@gmail.com>
2016-02-17 00:36:08 -0500

    Applying Petr Písař  fix  for DBD::SQLite error
    This fix corrects test failures for DBD::SQLite  inside Padre.

--- lib/Padre/Locker.pm
+++ lib/Padre/Locker.pm
@@ -102,7 +102,7 @@ sub shutdown {
 sub db_increment {
 	my $self = shift;
 	unless ( $self->{db_depth}++ ) {
-		Padre::DB->begin;
+		
 
 		# Database operations we lock on are the most likely to
 		# involve writes. So opportunistically prevent blocking
@@ -111,6 +111,7 @@ sub db_increment {
 		# corruption if (and only if) there is a power outage,
 		# operating system crash, or catastrophic hardware failure.
 		Padre::DB->pragma( synchronous => 0 );
+		Padre::DB->begin;
 	}
 	return;
 }
