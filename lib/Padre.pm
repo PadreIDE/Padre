8a7fc2cbff49c46d2dc3e1b025837a479d9ac629
Charlie Gonzalez <itcharlie@gmail.com>
2016-02-17 22:16:20 -0500

    1 more copyright update
    updated copyright on Padre.pm

--- lib/Padre.pm
+++ lib/Padre.pm
@@ -845,7 +845,7 @@ Tatsuhiko Miyagawa for consulting on our I18N and L10N support.
 
 =cut
 
-# Copyright 2008-2014 The Padre development team as listed in Padre.pm.
+# Copyright 2008-2016 The Padre development team as listed in Padre.pm.
 # LICENSE
 # This program is free software; you can redistribute it and/or
 # modify it under the same terms as Perl 5 itself.
