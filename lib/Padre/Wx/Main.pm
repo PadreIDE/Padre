a3df4d7b8a765f1d0303410cdc686d4bf8adabdc
Tim Lapawa <git@lapawa.de>
2018-03-05 11:24:36 +0100

    The tiniest patch you can get to acquire perl 5.26 compatibility. It just replaces a malformed UTF8 character.

--- lib/Padre/Wx/Main.pm
+++ lib/Padre/Wx/Main.pm
@@ -5561,7 +5561,7 @@ sub on_prev_pane {
 
     $main->on_join_lines;
 
-Join current line with next one (à la B<vi> with C<Ctrl+J>). No return value.
+Join current line with next one (a la B<vi> with C<Ctrl+J>). No return value.
 
 =cut
 
